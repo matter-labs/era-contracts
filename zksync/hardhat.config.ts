40c7aa47c24cc0c3e375f5f9133af2973add594d
Ramon Canales <emaildoramon@gmail.com>
2023-10-09 10:11:15 +0100

    chore(compiler): updates zksolc version

--- zksync/hardhat.config.ts
+++ zksync/hardhat.config.ts
@@ -11,7 +11,7 @@ if (!process.env.CHAIN_ETH_NETWORK) {
 
 export default {
     zksolc: {
-        version: '1.3.11',
+        version: '1.3.14',
         compilerSource: 'binary',
         settings: {
             isSystem: true
