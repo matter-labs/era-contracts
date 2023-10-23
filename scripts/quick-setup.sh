13664fc51aa8b60f3bb7c1a6056a58cf71e86a7f
Stanislav Breadless <stanislavbezkor@gmail.com>
2023-10-23 11:06:51 +0200

    use latest vm version

--- scripts/quick-setup.sh
+++ scripts/quick-setup.sh
@@ -6,7 +6,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 rustup toolchain install nightly
 
 # install era-test-node
-cargo +nightly install --git https://github.com/matter-labs/era-test-node.git --locked --branch boojum-integration
+cargo +nightly install --git https://github.com/matter-labs/era-test-node.git --locked --branch boojum-integration-vm-1.4.1
 
 yarn
 yarn build
