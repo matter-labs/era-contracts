e3513cbedf52f92a8dad0c981b2c39c45892884a
Bence Haromi <bence.haromi@gmail.com>
2023-10-30 14:23:46 +0000

    style(zksync): prettier:fix

--- zksync/hardhat.config.ts
+++ zksync/hardhat.config.ts
@@ -1,48 +1,48 @@
-import '@matterlabs/hardhat-zksync-solc';
-import '@matterlabs/hardhat-zksync-verify';
-import '@nomicfoundation/hardhat-chai-matchers';
-import '@nomiclabs/hardhat-ethers';
-import '@nomiclabs/hardhat-solpp';
-import 'hardhat-typechain';
+import "@matterlabs/hardhat-zksync-solc";
+import "@matterlabs/hardhat-zksync-verify";
+import "@nomicfoundation/hardhat-chai-matchers";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-solpp";
+import "hardhat-typechain";
 
 // If no network is specified, use the default config
 if (!process.env.CHAIN_ETH_NETWORK) {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    require('dotenv').config();
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  require("dotenv").config();
 }
 
 export default {
-    zksolc: {
-        version: '1.3.11',
-        compilerSource: 'binary',
-        settings: {
-            isSystem: true
-        }
+  zksolc: {
+    version: "1.3.11",
+    compilerSource: "binary",
+    settings: {
+      isSystem: true,
     },
-    solidity: {
-        version: '0.8.19'
+  },
+  solidity: {
+    version: "0.8.19",
+  },
+  defaultNetwork: "localhost",
+  networks: {
+    localhost: {
+      // era-test-node default url
+      url: "http://127.0.0.1:8011",
+      ethNetwork: null,
+      zksync: true,
     },
-    defaultNetwork: 'localhost',
-    networks: {
-        localhost: {
-            // era-test-node default url
-            url: 'http://127.0.0.1:8011',
-            ethNetwork: null,
-            zksync: true
-        },
-        zkSyncTestnet: {
-            url: 'https://zksync2-testnet.zksync.dev',
-            ethNetwork: 'goerli',
-            zksync: true,
-            // contract verification endpoint
-            verifyURL: 'https://zksync2-testnet-explorer.zksync.dev/contract_verification'
-        },
-        zksyncMainnet: {
-            url: 'https://mainnet.era.zksync.io',
-            ethNetwork: 'mainnet',
-            zksync: true,
-            // contract verification endpoint
-            verifyURL: 'https://zksync2-mainnet-explorer.zksync.io/contract_verification'
-        }
-    }
+    zkSyncTestnet: {
+      url: "https://zksync2-testnet.zksync.dev",
+      ethNetwork: "goerli",
+      zksync: true,
+      // contract verification endpoint
+      verifyURL: "https://zksync2-testnet-explorer.zksync.dev/contract_verification",
+    },
+    zksyncMainnet: {
+      url: "https://mainnet.era.zksync.io",
+      ethNetwork: "mainnet",
+      zksync: true,
+      // contract verification endpoint
+      verifyURL: "https://zksync2-mainnet-explorer.zksync.io/contract_verification",
+    },
+  },
 };
--- zksync/src/verify.ts
+++ zksync/src/verify.ts
@@ -1,42 +1,42 @@
-import * as hardhat from 'hardhat';
+import * as hardhat from "hardhat";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function verifyPromise(address: string, constructorArguments?: Array<any>, libraries?: object): Promise<any> {
-    return new Promise((resolve, reject) => {
-        hardhat
-            .run('verify:verify', { address, constructorArguments, libraries })
-            .then(() => resolve(`Successfully verified ${address}`))
-            .catch((e) => reject(`Failed to verify ${address}\nError: ${e.message}`));
-    });
+  return new Promise((resolve, reject) => {
+    hardhat
+      .run("verify:verify", { address, constructorArguments, libraries })
+      .then(() => resolve(`Successfully verified ${address}`))
+      .catch((e) => reject(`Failed to verify ${address}\nError: ${e.message}`));
+  });
 }
 
 async function main() {
-    if (process.env.CHAIN_ETH_NETWORK == 'localhost') {
-        console.log('Skip contract verification on localhost');
-        return;
-    }
+  if (process.env.CHAIN_ETH_NETWORK == "localhost") {
+    console.log("Skip contract verification on localhost");
+    return;
+  }
 
-    const promises = [];
+  const promises = [];
 
-    // Contracts without constructor parameters
-    for (const address of [
-        process.env.CONTRACTS_L2_WETH_TOKEN_IMPL_ADDR,
-        process.env.CONTRACTS_L2_ERC20_BRIDGE_IMPL_ADDR,
-        process.env.CONTRACTS_L2_ERC20_BRIDGE_TOKEN_IMPL_ADDR
-    ]) {
-        const promise = verifyPromise(address);
-        promises.push(promise);
-    }
+  // Contracts without constructor parameters
+  for (const address of [
+    process.env.CONTRACTS_L2_WETH_TOKEN_IMPL_ADDR,
+    process.env.CONTRACTS_L2_ERC20_BRIDGE_IMPL_ADDR,
+    process.env.CONTRACTS_L2_ERC20_BRIDGE_TOKEN_IMPL_ADDR,
+  ]) {
+    const promise = verifyPromise(address);
+    promises.push(promise);
+  }
 
-    const messages = await Promise.allSettled(promises);
-    for (const message of messages) {
-        console.log(message.status == 'fulfilled' ? message.value : message.reason);
-    }
+  const messages = await Promise.allSettled(promises);
+  for (const message of messages) {
+    console.log(message.status == "fulfilled" ? message.value : message.reason);
+  }
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch((err) => {
-        console.error('Error:', err.message || err);
-        process.exit(1);
-    });
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.error("Error:", err.message || err);
+    process.exit(1);
+  });
