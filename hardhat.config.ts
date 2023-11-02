27d550a1cc44020583a6456b8868b59413127590
Bence Haromi <56651250+benceharomi@users.noreply.github.com>
2023-11-02 13:56:00 +0000

    chore: synchronise linting rules of repositories (#49)
    * chore: command name changes
    
    * lint(calculate-hashes): fix
    
    * fix: lint:md command
    
    * chore: package.json commands alphabetical order
    
    * lint: using @matterlabs/eslint-config-typescript and "@matterlabs/prettier-config
    
    * style: prettier:fix
    
    * lint: lint:fix
    
    * Revert "lint: lint:fix"
    
    This reverts commit 15993b2d2ddfce0d876966d170e781645ff66cf9.
    
    * lint: eslint rules turned off
    
    * lint: lint:fix with new rules
    
    * chore: .eslintignore removed
    
    * chore: create githooks to check formatting and linting  (#56)
    
    * chore: pre-commit and pre-push hooks added
    
    * docs: removed yarn lint from PR template
    
    * Revert "chore: package.json commands alphabetical order"
    
    This reverts commit e39a52c0b764a6ef40cfdc0fded9e068cceba1ce.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,51 +1,51 @@
-import '@matterlabs/hardhat-zksync-chai-matchers';
-import '@matterlabs/hardhat-zksync-solc';
-import '@nomiclabs/hardhat-ethers';
-import '@nomiclabs/hardhat-solpp';
-import '@typechain/hardhat';
+import "@matterlabs/hardhat-zksync-chai-matchers";
+import "@matterlabs/hardhat-zksync-solc";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-solpp";
+import "@typechain/hardhat";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const systemConfig = require('./SystemConfig.json');
+const systemConfig = require("./SystemConfig.json");
 
 export default {
-    zksolc: {
-        version: '1.3.14',
-        compilerSource: 'binary',
-        settings: {
-            isSystem: true
-        }
+  zksolc: {
+    version: "1.3.14",
+    compilerSource: "binary",
+    settings: {
+      isSystem: true,
     },
-    zkSyncDeploy: {
-        zkSyncNetwork: 'http://localhost:3050',
-        ethNetwork: 'http://localhost:8545'
+  },
+  zkSyncDeploy: {
+    zkSyncNetwork: "http://localhost:3050",
+    ethNetwork: "http://localhost:8545",
+  },
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      viaIR: true,
     },
-    solidity: {
-        version: '0.8.17',
-        settings: {
-            optimizer: {
-                enabled: true,
-                runs: 200
-            },
-            viaIR: true
-        }
+  },
+  solpp: {
+    defs: (() => {
+      return {
+        ECRECOVER_COST_GAS: systemConfig.ECRECOVER_COST_GAS,
+        KECCAK_ROUND_COST_GAS: systemConfig.KECCAK_ROUND_COST_GAS,
+        SHA256_ROUND_COST_GAS: systemConfig.SHA256_ROUND_COST_GAS,
+      };
+    })(),
+  },
+  networks: {
+    hardhat: {
+      zksync: true,
     },
-    solpp: {
-        defs: (() => {
-            return {
-                ECRECOVER_COST_GAS: systemConfig.ECRECOVER_COST_GAS,
-                KECCAK_ROUND_COST_GAS: systemConfig.KECCAK_ROUND_COST_GAS,
-                SHA256_ROUND_COST_GAS: systemConfig.SHA256_ROUND_COST_GAS
-            };
-        })()
+    zkSyncTestNode: {
+      url: "http://127.0.0.1:8011",
+      ethNetwork: "",
+      zksync: true,
     },
-    networks: {
-        hardhat: {
-            zksync: true
-        },
-        zkSyncTestNode: {
-            url: 'http://127.0.0.1:8011',
-            ethNetwork: '',
-            zksync: true
-        }
-    }
+  },
 };
