46e6f178e0675b2c1280058adb76d06e89412bb6
koloz <zach.kolodny@gmail.com>
2023-11-09 08:46:50 -0500

    changed modifier to onlyGovernor and added tests

---
+++ ethereum/test/foundry/unit/concrete/Admin/Authorization.t.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.20;
+
+import {AdminTest} from "./_Admin_Shared.t.sol";
+
+contract AuthorizationTest is AdminTest {
+    function test_SetPendingAdmin_RevertWhen() public {
+        address newAdmin = address(0x1337);
+        vm.prank(owner);
+        vm.expectRevert(bytes.concat("1g"));
+        proxyAsAdmin.setPendingAdmin(newAdmin);
+    }
+
+    function test_SetPendingAdmin_RevertWhen_AdminNotGovernanceOwner() public {
+        address newAdmin = address(0x1337);
+        vm.prank(owner);
+        vm.expectRevert(bytes.concat("1g"));
+        proxyAsAdmin.setPendingAdmin(newAdmin);
+    }
+}
