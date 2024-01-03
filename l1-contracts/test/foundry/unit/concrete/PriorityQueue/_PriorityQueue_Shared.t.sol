5eafb16b9368241888515fa4a0973f869fe988c2
mm-zk <mm@matterlabs.dev>
2024-01-03 10:52:45 +0100

    prettier fix

--- l1-contracts/test/foundry/unit/concrete/PriorityQueue/_PriorityQueue_Shared.t.sol
+++ l1-contracts/test/foundry/unit/concrete/PriorityQueue/_PriorityQueue_Shared.t.sol
@@ -4,7 +4,6 @@ pragma solidity 0.8.20;
 import {Test} from "forge-std/Test.sol";
 import {PriorityQueueTest, PriorityOperation} from "solpp/dev-contracts/test/PriorityQueueTest.sol";
 
-
 contract PriorityQueueSharedTest is Test {
     PriorityQueueTest internal priorityQueue;
 
