859a60d6b2cd7075522b6b625dd2ae7a11d3d8d1
koloz <zach.kolodny@gmail.com>
2024-01-12 13:51:42 -0500

    added comment

--- system-contracts/contracts/Constants.sol
+++ system-contracts/contracts/Constants.sol
@@ -136,6 +136,7 @@ uint256 constant STATE_DIFF_FINAL_VALUE_OFFSET = 124;
 
 /// @dev Total number of bytes in a blob. Blob = 4096 field elements * 31 bytes per field element
 /// @dev 4844 defines it as 131_072 but we use 4096 * 31 within our circuits to always fit within a field element
+/// @dev Our circuits will prove that a 4844 blob and our internal blob are the same.
 uint256 constant BLOB_SIZE_BYTES = 126_976;
 
 /// @dev The hash of an empty blob, equal to keccak(new bytes(BLOB_SIZE_BYTES))
