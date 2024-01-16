8070ab990e7b72f1513f834ee010773e16d97f54
kelemeno <kl@matterlabs.dev>
2024-01-16 14:05:10 +0000

    updated based on defaultUpgrade contract

--- l1-contracts/contracts/upgrades/GenesisUpgrade.sol
+++ l1-contracts/contracts/upgrades/GenesisUpgrade.sol
@@ -9,39 +9,10 @@ import "./IDefaultUpgrade.sol";
 /// @author Matter Labs
 /// @custom:security-contact security@matterlabs.dev
 contract GenesisUpgrade is BaseZkSyncUpgradeGenesis {
-    /// @notice Placeholder function for custom logic for upgrading L1 contract.
-    /// Typically this function will never be used.
-    /// @param _customCallDataForUpgrade Custom data for an upgrade, which may be interpreted differently for each
-    /// upgrade.
-    function _upgradeL1Contract(bytes calldata _customCallDataForUpgrade) internal override {}
-
-    /// @notice placeholder function for custom logic for post-upgrade logic.
-    /// Typically this function will never be used.
-    /// @param _customCallDataForUpgrade Custom data for an upgrade, which may be interpreted differently for each
-    /// upgrade.
-    function _postUpgrade(bytes calldata _customCallDataForUpgrade) internal override {}
-
     /// @notice The main function that will be called by the upgrade proxy.
     /// @param _proposedUpgrade The upgrade to be executed.
     function upgrade(ProposedUpgrade calldata _proposedUpgrade) public override returns (bytes32) {
         super.upgrade(_proposedUpgrade);
-
-        _setNewProtocolVersion(_proposedUpgrade.newProtocolVersion);
-        _upgradeL1Contract(_proposedUpgrade.l1ContractsUpgradeCalldata);
-        _upgradeVerifier(_proposedUpgrade.verifier, _proposedUpgrade.verifierParams);
-        _setBaseSystemContracts(_proposedUpgrade.bootloaderHash, _proposedUpgrade.defaultAccountHash);
-
-        bytes32 txHash;
-        txHash = _setL2SystemContractUpgrade(
-            _proposedUpgrade.l2ProtocolUpgradeTx,
-            _proposedUpgrade.factoryDeps,
-            _proposedUpgrade.newProtocolVersion
-        );
-
-        _postUpgrade(_proposedUpgrade.postUpgradeCalldata);
-
-        emit UpgradeComplete(_proposedUpgrade.newProtocolVersion, txHash, _proposedUpgrade);
-
         return Diamond.DIAMOND_INIT_SUCCESS_RETURN_VALUE;
     }
 }
