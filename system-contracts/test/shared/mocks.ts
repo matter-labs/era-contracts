5f2dc1fd63a61c6a03cf4cc5a8394aafdf925470
Neo <128649481+neotheprogramist@users.noreply.github.com>
2024-01-23 13:39:54 +0100

    L1Messenger Tests (#150)
    Co-authored-by: Uacias <filgum0326@gmail.com>

--- system-contracts/test/shared/mocks.ts
+++ system-contracts/test/shared/mocks.ts
@@ -10,6 +10,8 @@ import {
   TEST_L1_MESSENGER_SYSTEM_CONTRACT_ADDRESS,
   TEST_MSG_VALUE_SYSTEM_CONTRACT_ADDRESS,
   TEST_NONCE_HOLDER_SYSTEM_CONTRACT_ADDRESS,
+  TEST_SYSTEM_CONTEXT_CONTRACT_ADDRESS,
+  TEST_COMPRESSOR_CONTRACT_ADDRESS,
 } from "./constants";
 import { deployContractOnAddress, getWallets, loadArtifact } from "./utils";
 
@@ -21,6 +23,8 @@ type CallResult = {
 // Currently listed only contracts, that actually need to be mocked in the tests.
 // But other contracts can be added if needed.
 const TEST_SYSTEM_CONTRACTS_MOCKS = {
+  Compressor: TEST_COMPRESSOR_CONTRACT_ADDRESS,
+  SystemContext: TEST_SYSTEM_CONTEXT_CONTRACT_ADDRESS,
   NonceHolder: TEST_NONCE_HOLDER_SYSTEM_CONTRACT_ADDRESS,
   L1Messenger: TEST_L1_MESSENGER_SYSTEM_CONTRACT_ADDRESS,
   KnownCodesStorage: TEST_KNOWN_CODE_STORAGE_CONTRACT_ADDRESS,
