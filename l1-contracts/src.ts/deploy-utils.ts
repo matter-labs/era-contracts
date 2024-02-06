d5fc47ac0ee070b06f011a9118b97d2dca2391c3
koloz <zach.kolodny@gmail.com>
2024-02-06 13:44:59 -0500

    reduce duplicate code

--- l1-contracts/src.ts/deploy-utils.ts
+++ l1-contracts/src.ts/deploy-utils.ts
@@ -17,43 +17,21 @@ export async function deployViaCreate2(
 ): Promise<[string, string]> {
   // [address, txHash]
 
-  const log = (msg: string) => {
-    if (verbose) {
-      console.log(msg);
-    }
-  };
-  log(`Deploying ${contractName}`);
-
-  const create2Factory = SingletonFactoryFactory.connect(create2FactoryAddress, deployWallet);
   const contractFactory = await hardhat.ethers.getContractFactory(contractName, {
     signer: deployWallet,
     libraries,
   });
   const bytecode = contractFactory.getDeployTransaction(...[...args, ethTxOptions]).data;
-  const expectedAddress = ethers.utils.getCreate2Address(
-    create2Factory.address,
+
+  return await deployBytecodeViaCreate2(
+    deployWallet,
+    contractName,
+    bytecode,
     create2Salt,
-    ethers.utils.keccak256(bytecode)
+    ethTxOptions,
+    create2FactoryAddress,
+    verbose
   );
-
-  const deployedBytecodeBefore = await deployWallet.provider.getCode(expectedAddress);
-  if (ethers.utils.hexDataLength(deployedBytecodeBefore) > 0) {
-    log(`Contract ${contractName} already deployed`);
-    return [expectedAddress, ethers.constants.HashZero];
-  }
-
-  const tx = await create2Factory.deploy(bytecode, create2Salt, ethTxOptions);
-  const receipt = await tx.wait(2);
-
-  const gasUsed = receipt.gasUsed;
-  log(`${contractName} deployed, gasUsed: ${gasUsed.toString()}`);
-
-  const deployedBytecodeAfter = await deployWallet.provider.getCode(expectedAddress);
-  if (ethers.utils.hexDataLength(deployedBytecodeAfter) == 0) {
-    throw new Error("Failed to deploy bytecode via create2 factory");
-  }
-
-  return [expectedAddress, tx.hash];
 }
 
 export async function deployBytecodeViaCreate2(
