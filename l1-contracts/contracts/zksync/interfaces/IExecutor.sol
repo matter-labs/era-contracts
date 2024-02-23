df3cc42d210ce7969ca2e34f134eb7900e43d56d
koloz193 <zach.kolodny@gmail.com>
2024-02-23 07:10:24 -0700

    feat(executor): add blob linear hash and commitment to calldata (#221)

--- l1-contracts/contracts/zksync/interfaces/IExecutor.sol
+++ l1-contracts/contracts/zksync/interfaces/IExecutor.sol
@@ -99,8 +99,11 @@ interface IExecutor is IBase {
     /// @param pubdataCommitments Packed pubdata commitments/data.
     /// @dev pubdataCommitments format: This will always start with a 1 byte pubdataSource flag. Current allowed values are 0 (calldata) or 1 (blobs)
     ///                             kzg: list of: opening point (16 bytes) || claimed value (32 bytes) || commitment (48 bytes) || proof (48 bytes) = 144 bytes
-    ///                             calldata: pubdataCommitments.length - 1 bytes of pubdata
+    ///                             calldata: pubdataCommitments.length - 1 - 32 bytes of pubdata
+    ///                                       and 32 bytes appended to serve as the blob commitment part for the aux output part of the batch commitment
     /// @dev For 2 blobs we will be sending 288 bytes of calldata instead of the full amount for pubdata.
+    /// @dev When using calldata, we only need to send one blob commitment since the max number of bytes in calldata fits in a single blob and we can pull the
+    ///     linear hash from the system logs
     struct CommitBatchInfo {
         uint64 batchNumber;
         uint64 timestamp;
