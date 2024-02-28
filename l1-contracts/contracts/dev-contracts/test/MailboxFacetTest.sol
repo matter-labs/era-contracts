16ba7f3f1653d153911a6500141a3d669f077780
toni-calvin <antonio.calvin@lambdaclass.com>
2024-02-28 16:08:33 +0100

    remove unused method

--- l1-contracts/contracts/dev-contracts/test/MailboxFacetTest.sol
+++ l1-contracts/contracts/dev-contracts/test/MailboxFacetTest.sol
@@ -17,8 +17,4 @@ contract MailboxFacetTest is MailboxFacet {
     function getL2GasPrice(uint256 _l1GasPrice) external view returns (uint256) {
         return _deriveL2GasPrice(_l1GasPrice, REQUIRED_L2_GAS_PRICE_PER_PUBDATA);
     }
-
-    function getPubdataPricingMode() external view returns (PubdataPricingMode) {
-        return s.feeParams.pubdataPricingMode;
-    }
 }
