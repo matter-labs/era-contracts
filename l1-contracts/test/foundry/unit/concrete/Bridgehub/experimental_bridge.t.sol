05c2c78a1390fd7675ec0619f55773b283f991fe
saxenism <rahulsaxena.pro@gmail.com>
2024-02-29 14:34:08 +0530

    failing tests

--- l1-contracts/test/foundry/unit/concrete/Bridgehub/experimental_bridge.t.sol
+++ l1-contracts/test/foundry/unit/concrete/Bridgehub/experimental_bridge.t.sol
@@ -4,22 +4,24 @@ pragma solidity 0.8.20;
 
 import {Test} from "forge-std/Test.sol";
 
+
+import {Diamond} from "solpp/state-transition/libraries/Diamond.sol";
 import {TestnetERC20Token} from "solpp/dev-contracts/TestnetERC20Token.sol";
 import {IBridgehub, Bridgehub} from "solpp/bridgehub/Bridgehub.sol";
-import {DummyStateTransitionManager} from "solpp/dev-contracts/test/DummyStateTransitionManager.sol";
+import {DummyStateTransitionManagerWBH} from "solpp/dev-contracts/test/DummyStateTransitionManagerWithBridgeHubAddress.sol";
 import {IL1SharedBridge} from "solpp/bridge/interfaces/IL1SharedBridge.sol";
 
 contract ExperimentalBridgeTest is Test {
 
     Bridgehub bridgeHub;
     address public bridgeOwner;
-    DummyStateTransitionManager mockSTM;
+    DummyStateTransitionManagerWBH mockSTM;
     TestnetERC20Token testToken;
 
     function setUp() public {
         bridgeHub = new Bridgehub();
         bridgeOwner = makeAddr("BRIDGE_OWNER");
-        mockSTM = new DummyStateTransitionManager();
+        mockSTM = new DummyStateTransitionManagerWBH(address(bridgeHub));
         testToken = new TestnetERC20Token("ZKSTT", "ZkSync Test Token", 18);
 
         // test if the ownership of the bridgeHub is set correctly or not
@@ -132,23 +134,6 @@ contract ExperimentalBridgeTest is Test {
         bridgeHub.removeStateTransitionManager(randomAddressWithoutTheCorrectInterface);
     }
 
-    /**
-        /// @notice token can be any contract with the appropriate interface/functionality
-    function addToken(address _token) external onlyOwner {
-        require(!tokenIsRegistered[_token], "Bridgehub: token already registered");
-        tokenIsRegistered[_token] = true;
-    }
-
-    /// @notice To set shared bridge, only Owner. Not done in initialize, as
-    /// the order of deployment is Bridgehub, Shared bridge, and then we call this
-    function setSharedBridge(address _sharedBridge) external onlyOwner {
-        sharedBridge = IL1SharedBridge(_sharedBridge);
-    }
-
-
-    
-     */
-
     function test_addToken(address randomCaller, address randomAddress) public {
         if(randomCaller != bridgeOwner) {
             vm.prank(randomCaller);
@@ -183,5 +168,94 @@ contract ExperimentalBridgeTest is Test {
         bridgeHub.setSharedBridge(randomAddress);
 
         assertTrue(bridgeHub.sharedBridge() == IL1SharedBridge(randomAddress), "after call from the bridgeowner, this randomAddress should be the registered sharedBridge");
+    }
+
+
+    uint256 newChainId;
+    address admin;
+    function test_createNewChain(
+        address randomCaller, 
+        uint256 chainId,
+        bool isFreezable, 
+        bytes4[] memory mockSelectors, 
+        address mockInitAddress, 
+        bytes memory mockInitCalldata
+    ) public {
+        address deployerAddress = makeAddr("DEPLOYER_ADDRESS");
+        address mockSharedBridge = makeAddr("MOCK_SHARED_BRIDGE");
+        admin = makeAddr("NEW_CHAIN_ADMIN");
+        Diamond.DiamondCutData memory dcData;
+
+        vm.startPrank(bridgeOwner);
+        bridgeHub.setDeployer(deployerAddress);
+        bridgeHub.addStateTransitionManager(address(mockSTM));
+        bridgeHub.addToken(address(testToken));
+        bridgeHub.setSharedBridge(mockSharedBridge);
+        vm.stopPrank();
+
+        if(randomCaller != deployerAddress && randomCaller != bridgeOwner) {
+            vm.prank(randomCaller);
+            vm.expectRevert(bytes("Bridgehub: not owner or deployer"));
+            bridgeHub.createNewChain(
+                chainId,
+                address(mockSTM),
+                address(testToken),
+                uint256(123),
+                admin,
+                bytes("")
+            );
+        }
+
+        chainId = bound(chainId, 1, type(uint48).max);
+        bytes memory _newChainInitData = _createNewChainInitData(isFreezable, mockSelectors, mockInitAddress, mockInitCalldata);
+
+        emit log_named_bytes32("ICH", mockSTM.initialCutHash());
+
+        vm.startPrank(deployerAddress);
+        vm.mockCall(
+            address(mockSTM),
+            abi.encodeWithSelector(mockSTM.createNewChain.selector, chainId, address(mockSTM), address(testToken), uint256(chainId * 2), admin, _newChainInitData),
+            bytes('')
+        );
+        newChainId = bridgeHub.createNewChain(
+            chainId,
+            address(mockSTM),
+            address(testToken),
+            uint256(chainId * 2),
+            admin,
+            _newChainInitData
+        );
+        vm.stopPrank();
+
+        assertTrue(bridgeHub.stateTransitionManager(newChainId) == address(mockSTM));
+        assertTrue(bridgeHub.baseToken(newChainId) == address(testToken));
+    }
+
+    function _createNewChainInitData(bool isFreezable, bytes4[] memory mockSelectors, address mockInitAddress, bytes memory mockInitCalldata) internal returns (bytes memory) {
+        bytes4[] memory singleSelector = new bytes4[](1);
+        singleSelector[0] = bytes4(0xabcdef12);
+        
+        Diamond.FacetCut memory facetCut;
+        Diamond.DiamondCutData memory diamondCutData;
+
+
+        facetCut.facet = address(this); // for a random address, it will fail the check of _facet.code.length > 0
+        facetCut.action = Diamond.Action.Add;
+        facetCut.isFreezable = isFreezable;
+        if(mockSelectors.length == 0) {
+            mockSelectors = singleSelector;
+        }
+        facetCut.selectors = mockSelectors;
+
+        Diamond.FacetCut[] memory facetCuts = new Diamond.FacetCut[](1);
+        facetCuts[0] = facetCut;
+
+        diamondCutData.facetCuts = facetCuts;
+        diamondCutData.initAddress = address(0);
+        diamondCutData.initCalldata = "";
+
+        mockSTM.setInitialCutHash(diamondCutData);
+
+        return abi.encode(diamondCutData);
     }
 }
