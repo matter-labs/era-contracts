7b80c5dfe90da9c07bfd7cfe9652d108a1a77056
Ivan Schasny <ivan.schasny@gmail.com>
2024-03-11 20:53:04 +0000

    fix: l1 to l2 address conversion

--- l2-contracts/src/utils.ts
+++ l2-contracts/src/utils.ts
@@ -19,13 +19,17 @@ const L1_TO_L2_ALIAS_OFFSET = "0x1111000000000000000000000000000000001111";
 const ADDRESS_MODULO = ethers.BigNumber.from(2).pow(160);
 
 export function applyL1ToL2Alias(address: string): string {
-  return ethers.utils.hexlify(ethers.BigNumber.from(address).add(L1_TO_L2_ALIAS_OFFSET).mod(ADDRESS_MODULO));
+  return ethers.utils.hexZeroPad(
+    ethers.utils.hexlify(ethers.BigNumber.from(address).add(L1_TO_L2_ALIAS_OFFSET).mod(ADDRESS_MODULO)),
+    40
+  );
 }
 
 export function unapplyL1ToL2Alias(address: string): string {
   // We still add ADDRESS_MODULO to avoid negative numbers
-  return ethers.utils.hexlify(
-    ethers.BigNumber.from(address).sub(L1_TO_L2_ALIAS_OFFSET).add(ADDRESS_MODULO).mod(ADDRESS_MODULO)
+  return ethers.utils.hexZeroPad(
+    ethers.utils.hexlify(ethers.BigNumber.from(address).sub(L1_TO_L2_ALIAS_OFFSET).add(ADDRESS_MODULO).mod(ADDRESS_MODULO)),
+    40
   );
 }
 
