5706e22a7ccd9bd188844b531d2556430907aed6
Danil <deniallugo@gmail.com>
2024-03-11 18:31:07 +0100

    Deploy contracts with correct nonce
    Signed-off-by: Danil <deniallugo@gmail.com>

--- l1-contracts/scripts/deploy-erc20.ts
+++ l1-contracts/scripts/deploy-erc20.ts
@@ -27,22 +27,27 @@ type TokenDescription = Token & {
   implementation?: string;
 };
 
-async function deployToken(token: TokenDescription, wallet: Wallet): Promise<Token> {
+async function deployToken(token: TokenDescription, wallet: Wallet, nonce?: number): Promise<[Token, number]> {
+  if (nonce === undefined) {
+    nonce = await wallet.getTransactionCount();
+  }
   token.implementation = token.implementation || DEFAULT_ERC20;
   const tokenFactory = await hardhat.ethers.getContractFactory(token.implementation, wallet);
   const args = token.implementation !== "WETH9" ? [token.name, token.symbol, token.decimals] : [];
-  const erc20 = await tokenFactory.deploy(...args, { gasLimit: 5000000 });
+  const erc20 = await tokenFactory.deploy(...args, { gasLimit: 5000000, nonce: nonce });
   await erc20.deployTransaction.wait();
 
   if (token.implementation !== "WETH9") {
-    await erc20.mint(wallet.address, parseEther("3000000000"));
+    nonce += 1;
+    await erc20.mint(wallet.address, parseEther("3000000000"), { gasLimit: 5000000, nonce: nonce });
   }
   for (let i = 0; i < 10; ++i) {
     const testWallet = Wallet.fromMnemonic(ethTestConfig.test_mnemonic as string, "m/44'/60'/0'/0/" + i).connect(
       provider
     );
     if (token.implementation !== "WETH9") {
-      await erc20.mint(testWallet.address, parseEther("3000000000"));
+      nonce += 1;
+      await erc20.mint(testWallet.address, parseEther("3000000000"), { gasLimit: 5000000, nonce: nonce });
     }
   }
 
@@ -53,7 +58,7 @@ async function deployToken(token: TokenDescription, wallet: Wallet): Promise<Tok
     delete token.implementation;
   }
 
-  return token;
+  return [token, nonce + 1];
 }
 
 async function main() {
@@ -96,8 +101,11 @@ async function main() {
         ? new Wallet(cmd.privateKey, provider)
         : Wallet.fromMnemonic(ethTestConfig.mnemonic, "m/44'/60'/0'/0/1").connect(provider);
 
+      let nonce = await wallet.getTransactionCount();
       for (const token of tokens) {
-        result.push(await deployToken(token, wallet));
+        let token_result;
+        [token_result, nonce] = await deployToken(token, wallet, nonce);
+        result.push(token_result);
       }
 
       console.log(JSON.stringify(result, null, 2));
