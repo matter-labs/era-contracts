57e7f626695f089b1157772e3640cdc05ff6b72f
Stanislav Breadless <stanislavbezkor@gmail.com>
2024-03-12 10:22:35 +0100

    make compile

--- system-contracts/contracts/PubdataChunkPublisher.sol
+++ system-contracts/contracts/PubdataChunkPublisher.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
+pragma solidity ^0.8.20;
 
 import {IPubdataChunkPublisher} from "./interfaces/IPubdataChunkPublisher.sol";
 import {ISystemContract} from "./interfaces/ISystemContract.sol";
