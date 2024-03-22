cfc96e7c2a9dc617ef817ccdf0c498a33c184871
Dima Zhornyk <55756184+dimazhornyk@users.noreply.github.com>
2024-03-22 12:57:41 +0100

    feat: add transaction filtering (#272)

---
+++ l1-contracts/contracts/dev-contracts/test/DummyTransactionFiltererFalse.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.20;
+
+import {ITransactionFilterer} from "../../state-transition/chain-interfaces/ITransactionFilterer.sol";
+
+contract TransactionFiltererFalse is ITransactionFilterer {
+    // add this to be excluded from coverage report
+    function test() internal virtual {}
+
+    function isTransactionAllowed(
+        address,
+        address,
+        uint256,
+        uint256,
+        bytes memory,
+        address
+    ) external view returns (bool) {
+        return false;
+    }
+}
