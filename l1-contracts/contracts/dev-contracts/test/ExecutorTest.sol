814ea34cdcc1cdcdb84cdeb0fdaa89792aac91f2
koloz <zach.kolodny@gmail.com>
2024-03-24 08:49:03 -0400

    updated test and hardhat json

--- l1-contracts/contracts/dev-contracts/test/ExecutorTest.sol
+++ l1-contracts/contracts/dev-contracts/test/ExecutorTest.sol
@@ -5,9 +5,7 @@ import {ExecutorFacet} from "../../state-transition/chain-deps/facets/Executor.s
 pragma solidity 0.8.24;
 
 contract TestExecutor is ExecutorFacet {
-    /// @dev Since we don't have access to the new BLOBHASH opecode we need to leverage a static call to a yul contract
-    /// that calls the opcode via a verbatim call. This should be swapped out once there is solidity support for the
-    /// new opcode.
+    /// @dev Since we want to test the blob functionality we want mock the calls to the blobhash opcode.
     function _getBlobVersionedHash(uint256 _index) internal view virtual override returns (bytes32 versionedHash) {
         (bool success, bytes memory data) = s.blobVersionedHashRetriever.staticcall(abi.encode(_index));
         require(success, "vc");
