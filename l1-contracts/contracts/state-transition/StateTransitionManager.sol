c88671da04d906dcd781e28c0344db19c83a454a
kelemeno <kl@matterlabs.dev>
2024-04-01 19:31:53 +0200

    new events

--- l1-contracts/contracts/state-transition/StateTransitionManager.sol
+++ l1-contracts/contracts/state-transition/StateTransitionManager.sol
@@ -130,12 +130,16 @@ contract StateTransitionManager is IStateTransitionManager, ReentrancyGuard, Own
 
     /// @dev set validatorTimelock. Cannot do it an initialization, as validatorTimelock is deployed after STM
     function setValidatorTimelock(address _validatorTimelock) external onlyOwnerOrAdmin {
+        address oldValidatorTimelock = validatorTimelock;
         validatorTimelock = _validatorTimelock;
+        emit NewValidatorTimelock(oldValidatorTimelock, _validatorTimelock);
     }
 
     /// @dev set initial cutHash
     function setInitialCutHash(Diamond.DiamondCutData calldata _diamondCut) external onlyOwner {
+        bytes32 oldInitialCutHash = initialCutHash;
         initialCutHash = keccak256(abi.encode(_diamondCut));
+        emit NewInitialCutHash(oldInitialCutHash, initialCutHash);
     }
 
     /// @dev set New Version with upgrade from old version
@@ -144,8 +148,12 @@ contract StateTransitionManager is IStateTransitionManager, ReentrancyGuard, Own
         uint256 _oldProtocolVersion,
         uint256 _newProtocolVersion
     ) external onlyOwner {
-        upgradeCutHash[_oldProtocolVersion] = keccak256(abi.encode(_cutData));
+        bytes32 newCutHash = keccak256(abi.encode(_cutData));
+        upgradeCutHash[_oldProtocolVersion] = newCutHash;
+        uint256 previousProtocolVersion = protocolVersion;
         protocolVersion = _newProtocolVersion;
+        emit NewProtocolVersion(previousProtocolVersion, _newProtocolVersion);
+        emit NewUpgradeCutHash(_oldProtocolVersion, newCutHash);
     }
 
     /// @dev set upgrade for some protocolVersion
@@ -154,6 +162,7 @@ contract StateTransitionManager is IStateTransitionManager, ReentrancyGuard, Own
         uint256 _oldProtocolVersion
     ) external onlyOwner {
         upgradeCutHash[_oldProtocolVersion] = keccak256(abi.encode(_cutData));
+        emit NewUpgradeCutHash(_oldProtocolVersion, upgradeCutHash[_oldProtocolVersion]);
     }
 
     /// @dev freezes the specified chain
