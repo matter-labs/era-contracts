fe844c65719ae7757bdef5c14094e0a49e83a274
kelemeno <34402761+kelemeno@users.noreply.github.com>
2024-04-08 09:17:21 +0100

    OZ fix M02 "changeFeeParams" function (#292)
    Co-authored-by: vladbochok <vladbochok1@gmail.com>
    Co-authored-by: Vlad Bochok <41153528+vladbochok@users.noreply.github.com>

--- l1-contracts/contracts/state-transition/chain-deps/facets/Admin.sol
+++ l1-contracts/contracts/state-transition/chain-deps/facets/Admin.sol
@@ -70,6 +70,9 @@ contract AdminFacet is ZkSyncStateTransitionBase, IAdmin {
         require(_newFeeParams.maxPubdataPerBatch >= _newFeeParams.priorityTxMaxPubdata, "n6");
 
         FeeParams memory oldFeeParams = s.feeParams;
+
+        require(_newFeeParams.pubdataPricingMode == oldFeeParams.pubdataPricingMode, "n7"); // we cannot change pubdata pricing mode
+
         s.feeParams = _newFeeParams;
 
         emit NewFeeParams(oldFeeParams, _newFeeParams);
@@ -89,7 +92,7 @@ contract AdminFacet is ZkSyncStateTransitionBase, IAdmin {
 
     /// @inheritdoc IAdmin
     function setPubdataPricingMode(PubdataPricingMode _pricingMode) external onlyAdmin {
-        require(s.totalBatchesCommitted == 0, "AdminFacet: set validium only after genesis"); // Validium mode can be set only before the first batch is committed
+        require(s.totalBatchesCommitted == 0, "AdminFacet: set validium only after genesis"); // Validium mode can be set only before the first batch is processed
         s.feeParams.pubdataPricingMode = _pricingMode;
         emit ValidiumModeStatusUpdate(_pricingMode);
     }
