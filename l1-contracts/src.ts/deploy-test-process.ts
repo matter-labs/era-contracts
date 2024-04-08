27e3ba8c656cf7584c06f47b42eb275f442a5fb2
Bence Haromi <bence.haromi@gmail.com>
2024-04-08 17:18:43 +0100

    fix(deploy-test-process): CONTRACTS_ERA_CHAIN_ID set to 270

--- l1-contracts/src.ts/deploy-test-process.ts
+++ l1-contracts/src.ts/deploy-test-process.ts
@@ -41,7 +41,7 @@ export async function loadDefaultEnvVarsForTests(deployWallet: Wallet) {
   process.env.CONTRACTS_RECURSION_CIRCUITS_SET_VKS_HASH = ethers.constants.HashZero;
   // process.env.CONTRACTS_SHARED_BRIDGE_UPGRADE_STORAGE_SWITCH = "1";
   process.env.ETH_CLIENT_CHAIN_ID = (await deployWallet.getChainId()).toString();
-  process.env.CONTRACTS_ERA_CHAIN_ID = "9";
+  process.env.CONTRACTS_ERA_CHAIN_ID = "270";
   process.env.CONTRACTS_ERA_DIAMOND_PROXY_ADDR = CONTRACTS_ERA_DIAMOND_PROXY_ADDR;
   process.env.CONTRACTS_L2_SHARED_BRIDGE_ADDR = ADDRESS_ONE;
   process.env.CONTRACTS_BRIDGEHUB_PROXY_ADDR = ADDRESS_ONE;
