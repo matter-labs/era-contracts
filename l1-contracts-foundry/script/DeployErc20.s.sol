9921416926bc71c544318354729855ed881fc084
aon <21188659+aon@users.noreply.github.com>
2024-04-18 14:43:33 -0300

    fix: improve deploy erc20 configs

--- l1-contracts-foundry/script/DeployErc20.s.sol
+++ l1-contracts-foundry/script/DeployErc20.s.sol
@@ -38,18 +38,24 @@ contract RegisterHyperchainScript is Script {
     }
 
     function initializeConfig() internal {
-        // Grab config from output of l1 deployment
-        string memory root = vm.projectRoot();
-        string memory path = string.concat(root, "/script-config/config-deploy-erc20.toml");
-        string memory toml = vm.readFile(path);
-
         config.deployerAddress = msg.sender;
+
+        string memory root = vm.projectRoot();
+
+        // Grab config from output of l1 deployment
+        string memory path = string.concat(root, "/script-out/output-deploy-l1.toml");
+        string memory toml = vm.readFile(path);
 
         // Config file must be parsed key by key, otherwise values returned
         // are parsed alfabetically and not by key.
         // https://book.getfoundry.sh/cheatcodes/parse-toml
-        config.create2FactoryAddr = vm.parseTomlAddress(toml, "$.create2_factory_addr");
-        config.create2FactorySalt = vm.parseTomlBytes32(toml, "$.create2_factory_salt");
+        config.create2FactoryAddr = vm.parseTomlAddress(toml, "$.l1.create2_factory_addr");
+        config.create2FactorySalt = vm.parseTomlBytes32(toml, "$.l1.create2_factory_salt");
+
+        // Grab config from custom config file
+        path = string.concat(root, "/script-config/config-deploy-erc20.toml");
+        toml = vm.readFile(path);
+
         string[] memory tokens = vm.parseTomlKeys(toml, "$.tokens");
 
         for (uint256 i = 0; i < tokens.length; i++) {
