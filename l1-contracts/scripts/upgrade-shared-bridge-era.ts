06d4311dda56d0b0b7284bb045b2c80fa9b8f0aa
Lyova Potyomkin <lyova.potyomkin@gmail.com>
2024-04-22 14:19:24 +0300

    feat: allow using era's legacy interface for testing (#384)
    Co-authored-by: Stanislav Breadless <stanislavbezkor@gmail.com>

--- l1-contracts/scripts/upgrade-shared-bridge-era.ts
+++ l1-contracts/scripts/upgrade-shared-bridge-era.ts
@@ -7,7 +7,10 @@ import { Deployer } from "../src.ts/deploy";
 import { formatUnits, parseUnits, Interface } from "ethers/lib/utils";
 import { web3Provider, GAS_MULTIPLIER } from "./utils";
 import { deployedAddressesFromEnv } from "../src.ts/deploy-utils";
-import { ethTestConfig } from "../src.ts/utils";
+import { ethTestConfig, getAddressFromEnv } from "../src.ts/utils";
+import { hashL2Bytecode } from "../../l2-contracts/src/utils";
+import { Provider } from "zksync-web3";
+import beaconProxy = require("../../l2-contracts/artifacts-zk/@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol/BeaconProxy.json");
 
 const provider = web3Provider();
 
@@ -18,7 +21,6 @@ async function main() {
 
   program
     .option("--private-key <private-key>")
-    .option("--chain-id <chain-id>")
     .option("--gas-price <gas-price>")
     .option("--nonce <nonce>")
     .option("--owner-address <owner-address>")
@@ -57,12 +59,56 @@ async function main() {
       await deployer.deploySharedBridgeImplementation(create2Salt, { nonce });
 
       const proxyAdminInterface = new Interface(hardhat.artifacts.readArtifactSync("ProxyAdmin").abi);
-      const calldata = proxyAdminInterface.encodeFunctionData("upgrade(address,address)", [
+      let calldata = proxyAdminInterface.encodeFunctionData("upgrade(address,address)", [
         deployer.addresses.Bridges.SharedBridgeProxy,
         deployer.addresses.Bridges.SharedBridgeImplementation,
       ]);
 
       await deployer.executeUpgrade(deployer.addresses.TransparentProxyAdmin, 0, calldata);
+
+      // deploy a dummy erc20 bridge to set the storage values
+      await deployer.deployERC20BridgeImplementation(create2Salt, {}, true);
+
+      // upgrade to dummy bridge
+      calldata = proxyAdminInterface.encodeFunctionData("upgrade(address,address)", [
+        deployer.addresses.Bridges.ERC20BridgeProxy,
+        deployer.addresses.Bridges.ERC20BridgeImplementation,
+      ]);
+
+      await deployer.executeUpgrade(deployer.addresses.TransparentProxyAdmin, 0, calldata);
+      console.log("Upgraded ERC20Bridge to 'dummy' implementation");
+
+      const dummyBridgeAbi = hardhat.artifacts.readArtifactSync("DummyL1ERC20Bridge").abi;
+      const dummyBridge = new ethers.Contract(
+        deployer.addresses.Bridges.ERC20BridgeProxy,
+        dummyBridgeAbi,
+        deployWallet
+      );
+
+      const l2SharedBridgeAddress = getAddressFromEnv("CONTRACTS_L2_SHARED_BRIDGE_ADDR");
+      const l2TokenBytecodeHash = hashL2Bytecode(beaconProxy.bytecode);
+      const l2Provider = new Provider(process.env.API_WEB3_JSON_RPC_HTTP_URL);
+      // For the server to start up.
+      console.log("Waiting for server to start up");
+      await waitForServer(l2Provider);
+
+      const l2SharedBridge = new ethers.Contract(
+        l2SharedBridgeAddress,
+        ["function l2TokenBeacon() view returns (address)"],
+        l2Provider
+      );
+      const l2TokenBeacon = await l2SharedBridge.l2TokenBeacon();
+
+      console.log("Retrieved storage values for TestERC20Bridge:");
+      console.log("l2SharedBridgeAddress:", l2SharedBridgeAddress);
+      console.log("l2TokenBeacon:", l2TokenBeacon);
+      console.log("l2TokenBytecodeHash:", ethers.utils.hexlify(l2TokenBytecodeHash));
+
+      // set storage values
+      const tx = await dummyBridge.setValues(l2SharedBridgeAddress, l2TokenBeacon, l2TokenBytecodeHash);
+      await tx.wait();
+
+      console.log("Set storage values for TestERC20Bridge");
     });
 
   await program.parseAsync(process.argv);
@@ -74,3 +120,21 @@ main()
     console.error("Error:", err);
     process.exit(1);
   });
+
+async function waitForServer(provider: Provider) {
+  let iter = 0;
+  while (iter < 60) {
+    try {
+      await provider.getBlockNumber();
+      return;
+    } catch (_) {
+      await sleep(2);
+      iter += 1;
+    }
+  }
+  throw new Error("Server didn't start up in time. Exiting.");
+}
+
+async function sleep(seconds: number) {
+  return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+}
