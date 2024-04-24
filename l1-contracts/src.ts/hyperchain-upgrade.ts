cb047bd9ed8314b292b738a73f415f3d0a52c81e
Stanislav Breadless <stanislavbezkor@gmail.com>
2024-04-24 12:56:21 +0200

    hardcode gas for transfr

--- l1-contracts/src.ts/hyperchain-upgrade.ts
+++ l1-contracts/src.ts/hyperchain-upgrade.ts
@@ -411,10 +411,11 @@ async function migrateAssets(deployer: Deployer) {
     console.log("transferring Eth");
   }
   const sharedBridge = deployer.defaultSharedBridge(deployer.deployWallet);
-  const ethTransferData = sharedBridge.interface.encodeFunctionData("transferFundsFromLegacy", [
+  const ethTransferData = sharedBridge.interface.encodeFunctionData("safeTransferFundsFromLegacy", [
     ADDRESS_ONE,
     deployer.addresses.StateTransition.DiamondProxy,
     deployer.chainId,
+    300_000
   ]);
   await deployer.executeUpgrade(deployer.addresses.Bridges.SharedBridgeProxy, 0, ethTransferData);
 
@@ -433,10 +434,11 @@ async function migrateAssets(deployer: Deployer) {
   );
   await mintTx.wait();
 
-  const daiTransferData = sharedBridge.interface.encodeFunctionData("transferFundsFromLegacy", [
+  const daiTransferData = sharedBridge.interface.encodeFunctionData("safeTransferFundsFromLegacy", [
     altTokenAddress,
     deployer.addresses.Bridges.ERC20BridgeProxy,
     deployer.chainId,
+    300_000
   ]);
   // daiTransferData;
   await deployer.executeUpgrade(deployer.addresses.Bridges.SharedBridgeProxy, 0, daiTransferData);
