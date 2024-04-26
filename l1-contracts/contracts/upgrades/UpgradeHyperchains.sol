b8247267877d2d140de35fa85d1160a0f945d7eb
kelemeno <34402761+kelemeno@users.noreply.github.com>
2024-04-26 14:08:29 +0100

    fix: upgrade hyperchain add missing fields (#415)

--- l1-contracts/contracts/upgrades/UpgradeHyperchains.sol
+++ l1-contracts/contracts/upgrades/UpgradeHyperchains.sol
@@ -13,12 +13,20 @@ contract UpgradeHyperchains is BaseZkSyncUpgrade {
     /// @notice The main function that will be called by the upgrade proxy.
     /// @param _proposedUpgrade The upgrade to be executed.
     function upgrade(ProposedUpgrade calldata _proposedUpgrade) public override returns (bytes32) {
-        (uint256 chainId, address bridgehubAddress, address stateTransitionManager, address sharedBridgeAddress) = abi
-            .decode(_proposedUpgrade.postUpgradeCalldata, (uint256, address, address, address));
+        (
+            uint256 chainId,
+            address bridgehubAddress,
+            address stateTransitionManager,
+            address sharedBridgeAddress,
+            address chainAdmin,
+            address validatorTimelock
+        ) = abi.decode(_proposedUpgrade.postUpgradeCalldata, (uint256, address, address, address, address, address));
         require(chainId != 0, "UpgradeHyperchain: 1");
         require(bridgehubAddress != address(0), "UpgradeHyperchain: 2");
         require(stateTransitionManager != address(0), "UpgradeHyperchain: 3");
         require(sharedBridgeAddress != address(0), "UpgradeHyperchain: 4");
+        require(chainAdmin != address(0), "UpgradeHyperchains: 5");
+        require(validatorTimelock != address(0), "UpgradeHyperchains: 6");
 
         s.chainId = chainId;
         s.bridgehub = bridgehubAddress;
@@ -27,6 +35,8 @@ contract UpgradeHyperchains is BaseZkSyncUpgrade {
         s.baseToken = ETH_TOKEN_ADDRESS;
         s.baseTokenGasPriceMultiplierNominator = 1;
         s.baseTokenGasPriceMultiplierDenominator = 1;
+        s.admin = chainAdmin;
+        s.validators[validatorTimelock] = true;
 
         super.upgrade(_proposedUpgrade);
         return Diamond.DIAMOND_INIT_SUCCESS_RETURN_VALUE;
