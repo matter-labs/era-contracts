01dddf5738d2f4dd5f21ea0ded047beee9b77e3c
kelemeno <kl@matterlabs.dev>
2024-04-26 11:15:23 +0100

    linting

--- l1-contracts/contracts/upgrades/UpgradeHyperchains.sol
+++ l1-contracts/contracts/upgrades/UpgradeHyperchains.sol
@@ -13,8 +13,14 @@ contract UpgradeHyperchains is BaseZkSyncUpgrade {
     /// @notice The main function that will be called by the upgrade proxy.
     /// @param _proposedUpgrade The upgrade to be executed.
     function upgrade(ProposedUpgrade calldata _proposedUpgrade) public override returns (bytes32) {
-        (uint256 chainId, address bridgehubAddress, address stateTransitionManager, address sharedBridgeAddress, address chainAdmin, address validatorTimelock) = abi
-            .decode(_proposedUpgrade.postUpgradeCalldata, (uint256, address, address, address, address, address));
+        (
+            uint256 chainId,
+            address bridgehubAddress,
+            address stateTransitionManager,
+            address sharedBridgeAddress,
+            address chainAdmin,
+            address validatorTimelock
+        ) = abi.decode(_proposedUpgrade.postUpgradeCalldata, (uint256, address, address, address, address, address));
         require(chainId != 0, "UpgradeHyperchain: 1");
         require(bridgehubAddress != address(0), "UpgradeHyperchain: 2");
         require(stateTransitionManager != address(0), "UpgradeHyperchain: 3");
