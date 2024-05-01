519992d7d4bcca27dc16d78a61b8a5389ecde344
tejks <sarmatys.programmer@gmail.com>
2024-05-01 19:13:56 +0200

    feat: Code refactor

--- l1-contracts/test/foundry/integration/_Bridgehub_shared.t.sol
+++ l1-contracts/test/foundry/integration/_Bridgehub_shared.t.sol
@@ -2,6 +2,7 @@
 
 pragma solidity 0.8.24;
 
+import {StdStorage, stdStorage} from "forge-std/Test.sol";
 import {Test} from "forge-std/Test.sol";
 
 import {Diamond} from "contracts/state-transition/libraries/Diamond.sol";
@@ -26,17 +27,11 @@ import {StateTransitionManagerInitializeData} from "contracts/state-transition/I
 import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import {REQUIRED_L2_GAS_PRICE_PER_PUBDATA} from "contracts/common/Config.sol";
 import {IZkSyncHyperchain} from "contracts/state-transition/chain-interfaces/IZkSyncHyperchain.sol";
-
-contract StateTransitionManagerFactory is Test {
-    function getStateTransitionManagerAddress(address bridgeHubAddress) public returns (StateTransitionManager) {
-        return new StateTransitionManager(bridgeHubAddress, type(uint256).max);
-    }
-
-    // add this to be excluded from coverage report
-    function test() internal virtual {}
-}
+import {ETH_TOKEN_ADDRESS, REQUIRED_L2_GAS_PRICE_PER_PUBDATA, MAX_NEW_FACTORY_DEPS} from "contracts/common/Config.sol";
 
 contract BridgeHubIntegration is Test {
+    using stdStorage for StdStorage;
+
     address diamondAddress;
     address stmAddress;
     address bridgeHubOwner;
@@ -56,6 +51,66 @@ contract BridgeHubIntegration is Test {
     uint256 chainId;
     uint256 eraChainId;
 
+    constructor() {
+        bridgeHubOwner = makeAddr("bridgeHubOwner");
+        eraDiamondProxy = makeAddr("eraDiamondProxy");
+        admin = makeAddr("admin");
+        l1WethAddress = makeAddr("weth");
+        validator = makeAddr("validator");
+        l2SharedBridge = makeAddr("l2sharedBridge");
+
+        testnetVerifier = new TestnetVerifier();
+
+        chainId = 1;
+        eraChainId = 9;
+
+        bridgeHub = new Bridgehub();
+        bridgeHubAddress = address(bridgeHub);
+
+        vm.prank(bridgeHub.owner());
+        bridgeHub.transferOwnership(bridgeHubOwner);
+
+        vm.prank(bridgeHubOwner);
+        bridgeHub.acceptOwnership();
+
+        vm.prank(bridgeHubOwner);
+        bridgeHub.setPendingAdmin(admin);
+
+        vm.prank(admin);
+        bridgeHub.acceptAdmin();
+
+        setFacetCuts();
+        initializeStateTransitionManager();
+        registerStateTransitionManager(stmAddress);
+
+        token = new TestnetERC20Token("ERC20Base", "UWU", 18);
+        registerNewToken(address(token));
+        registerNewToken(ETH_TOKEN_ADDRESS);
+
+        sharedBridge = new L1SharedBridge({
+            _l1WethAddress: l1WethAddress,
+            _bridgehub: IBridgehub(address(bridgeHub)),
+            _eraChainId: eraChainId,
+            _eraDiamondProxy: eraDiamondProxy
+        });
+
+        // skipped intializing upgradeable proxy for now
+        vm.prank(sharedBridge.owner());
+        sharedBridge.transferOwnership(bridgeHubOwner);
+
+        vm.prank(bridgeHubOwner);
+        sharedBridge.acceptOwnership();
+
+        // mock l2 side of shared bridge
+        vm.startPrank(bridgeHubOwner);
+        sharedBridge.initializeChainGovernance(chainId, l2SharedBridge);
+        sharedBridge.initializeChainGovernance(eraChainId, l2SharedBridge);
+        vm.stopPrank();
+
+        vm.prank(bridgeHubOwner);
+        bridgeHub.setSharedBridge(address(sharedBridge));
+    }
+
     function registerStateTransitionManager(address _stmAddress) internal {
         vm.prank(bridgeHubOwner);
         bridgeHub.addStateTransitionManager(_stmAddress);
@@ -73,34 +128,7 @@ contract BridgeHubIntegration is Test {
         adminFacet.setTokenMultiplier(1, 1);
     }
 
-    function registerNewChain(uint256 _chainId) internal returns (uint256 chainId) {
-        Diamond.DiamondCutData memory diamondCutData = getDiamondCutData(diamondAddress);
-
-        vm.prank(bridgeHubOwner);
-
-        chainId = bridgeHub.createNewChain(
-            _chainId,
-            stmAddress,
-            address(token),
-            uint256(12),
-            admin,
-            abi.encode(diamondCutData)
-        );
-    }
-
-    function getDiamondCutData(address _diamondInit) internal returns (Diamond.DiamondCutData memory) {
-        InitializeDataNewChain memory initializeData = Utils.makeInitializeDataForNewChain(address(testnetVerifier));
-
-        bytes memory initCalldata = abi.encode(initializeData);
-
-        return Diamond.DiamondCutData({facetCuts: facetCuts, initAddress: _diamondInit, initCalldata: initCalldata});
-    }
-
-    function getSTM() internal returns (StateTransitionManager) {
-        return StateTransitionManager(stmAddress);
-    }
-
-    function initializeSTM() internal {
+    function initializeStateTransitionManager() private {
         StateTransitionManager stm = new StateTransitionManager(bridgeHubAddress, type(uint256).max);
         GenesisUpgrade genesisUpgradeContract = new GenesisUpgrade();
         DiamondInit diamondInit = new DiamondInit();
@@ -127,34 +155,7 @@ contract BridgeHubIntegration is Test {
         stmAddress = address(transparentUpgradeableProxy);
     }
 
-    constructor() {
-        bridgeHubOwner = makeAddr("bridgeHubOwner");
-        eraDiamondProxy = makeAddr("eraDiamondProxy");
-        admin = makeAddr("admin");
-        l1WethAddress = makeAddr("weth");
-        validator = makeAddr("validator");
-        l2SharedBridge = makeAddr("l2sharedBridge");
-
-        testnetVerifier = new TestnetVerifier();
-        chainId = 1;
-        eraChainId = 9;
-
-        bridgeHub = new Bridgehub();
-        bridgeHubAddress = address(bridgeHub);
-
-        // skipped intializing upgradeable proxy for now
-        vm.prank(bridgeHub.owner());
-        bridgeHub.transferOwnership(bridgeHubOwner);
-
-        vm.prank(bridgeHubOwner);
-        bridgeHub.acceptOwnership();
-
-        vm.prank(bridgeHubOwner);
-        bridgeHub.setPendingAdmin(admin);
-
-        vm.prank(admin);
-        bridgeHub.acceptAdmin();
-
+    function setFacetCuts() private {
         facetCuts.push(
             Diamond.FacetCut({
                 facet: address(new UtilsFacet()),
@@ -163,6 +164,7 @@ contract BridgeHubIntegration is Test {
                 selectors: Utils.getUtilsFacetSelectors()
             })
         );
+
         facetCuts.push(
             Diamond.FacetCut({
                 facet: address(new AdminFacet()),
@@ -171,6 +173,7 @@ contract BridgeHubIntegration is Test {
                 selectors: Utils.getAdminSelectors()
             })
         );
+
         facetCuts.push(
             Diamond.FacetCut({
                 facet: address(new ExecutorFacet()),
@@ -179,6 +182,7 @@ contract BridgeHubIntegration is Test {
                 selectors: Utils.getExecutorSelectors()
             })
         );
+
         facetCuts.push(
             Diamond.FacetCut({
                 facet: address(new GettersFacet()),
@@ -196,48 +200,91 @@ contract BridgeHubIntegration is Test {
                 selectors: Utils.getMailboxSelectors()
             })
         );
-
-        initializeSTM();
-        registerStateTransitionManager(stmAddress);
-
-        token = new TestnetERC20Token("ERC20Base", "UWU", 18);
-        registerNewToken(address(token));
-
-        sharedBridge = new L1SharedBridge({
-            _l1WethAddress: l1WethAddress,
-            _bridgehub: IBridgehub(address(bridgeHub)),
-            _eraChainId: eraChainId,
-            _eraDiamondProxy: eraDiamondProxy
-        });
-
-        // skipped intializing upgradeable proxy for now
-        vm.prank(sharedBridge.owner());
-        sharedBridge.transferOwnership(bridgeHubOwner);
-
-        vm.prank(bridgeHubOwner);
-        sharedBridge.acceptOwnership();
-
-        // mock l2 side of shared bridge
-        vm.startPrank(bridgeHubOwner);
-        sharedBridge.initializeChainGovernance(chainId, l2SharedBridge);
-        sharedBridge.initializeChainGovernance(eraChainId, l2SharedBridge);
-        vm.stopPrank();
-
-        vm.prank(bridgeHubOwner);
-        bridgeHub.setSharedBridge(address(sharedBridge));
     }
 
-    // add this to be excluded from coverage report
-    function test() internal virtual {}
+    function registerNewChain(uint256 _chainId, address _baseToken) internal returns (uint256 chainId) {
+        Diamond.DiamondCutData memory diamondCutData = getDiamondCutData(diamondAddress);
+
+        vm.prank(bridgeHubOwner);
+        chainId = bridgeHub.createNewChain(
+            _chainId,
+            stmAddress,
+            _baseToken,
+            uint256(12),
+            admin,
+            abi.encode(diamondCutData)
+        );
+    }
+
+    function getDiamondCutData(address _diamondInit) internal returns (Diamond.DiamondCutData memory) {
+        InitializeDataNewChain memory initializeData = Utils.makeInitializeDataForNewChain(address(testnetVerifier));
+
+        bytes memory initCalldata = abi.encode(initializeData);
+
+        return Diamond.DiamondCutData({facetCuts: facetCuts, initAddress: _diamondInit, initCalldata: initCalldata});
+    }
+
+    function setSharedBridgeChainBalance(uint256 _chainId, address _token, uint256 _value) internal {
+        stdstore
+            .target(address(sharedBridge))
+            .sig(sharedBridge.chainBalance.selector)
+            .with_key(_chainId)
+            .with_key(_token)
+            .checked_write(_value);
+    }
 }
 
-contract IntegrationTests is BridgeHubIntegration {
-    address alice;
-    address bob;
+contract HyperchainFactory is BridgeHubIntegration {
+    uint256 minHyperchainId = 10;
+    uint256[] hyperchainIds;
+
+    function spawnHyperchain() public {
+        hyperchainIds.push(registerNewChain(minHyperchainId + hyperchainIds.length, ETH_TOKEN_ADDRESS));
+    }
+
+    function spawnHyperchain(address _token) public {
+        hyperchainIds.push(registerNewChain(minHyperchainId + hyperchainIds.length, _token));
+    }
+
+    function spawnMultipleHyperchains(uint256 _numHyperchains) public {
+        for (uint i = 0; i < _numHyperchains; i++) {
+            spawnHyperchain();
+        }
+    }
+
+    function spawnMultipleHyperchainsWithToken(uint256 _numHyperchains, address _token) public {
+        for (uint i = 0; i < _numHyperchains; i++) {
+            spawnHyperchain(_token);
+        }
+    }
+
+    function getHyperchainAddress(uint256 _chainId) public view returns (address) {
+        return bridgeHub.getHyperchain(_chainId);
+    }
+
+    function getHyperchainBaseToken(uint256 _chainId) public view returns (address) {
+        return bridgeHub.baseToken(_chainId);
+    }
+
+    function clearSharedBridgeBalances() public {
+        for (uint i = 0; i < hyperchainIds.length; i++) {
+            setSharedBridgeChainBalance(hyperchainIds[i], ETH_TOKEN_ADDRESS, 0);
+            setSharedBridgeChainBalance(hyperchainIds[i], address(token), 0);
+        }
+    }
+
+    function test_creationOfHyperchains() public {
+        for (uint i = minHyperchainId; i < hyperchainIds.length; i++) {
+            address newHyperchain = getHyperchainAddress(i);
+            assert(newHyperchain != address(0));
+        }
+    }
+}
+
+contract L2TxMocker is Test {
     address mockRefundRecipient;
     address mockL2Contract;
 
-    uint256 mockMintValue = 1 ether;
     uint256 mockL2Value = 1000;
     uint256 mockL2GasLimit = 10000000;
     uint256 mockL2GasPerPubdataByteLimit = REQUIRED_L2_GAS_PRICE_PER_PUBDATA;
@@ -245,34 +292,23 @@ contract IntegrationTests is BridgeHubIntegration {
     bytes mockL2Calldata;
     bytes[] mockFactoryDeps;
 
-    uint256 startChainId = 10;
-    uint256[] chainIds;
-
-    function addNewChain() internal {
-        chainIds.push(registerNewChain(startChainId + chainIds.length));
-    }
-
     constructor() {
+        mockRefundRecipient = makeAddr("refundrecipient");
+        mockL2Contract = makeAddr("mockl2contract");
+
         mockL2Calldata = "";
         mockFactoryDeps = new bytes[](1);
         mockFactoryDeps[0] = "11111111111111111111111111111111";
-        mockL2Contract = makeAddr("mockl2contract");
-        alice = makeAddr("alice");
-        bob = makeAddr("bob");
-        mockRefundRecipient = makeAddr("refundrecipient");
-
-        addNewChain();
-        addNewChain();
-        addNewChain();
     }
 
     function createMockL2TransactionRequestDirect(
-        uint256 chainId
+        uint256 chainId,
+        uint256 mintValue
     ) internal returns (L2TransactionRequestDirect memory) {
         L2TransactionRequestDirect memory l2TxnReqDirect;
 
         l2TxnReqDirect.chainId = chainId;
-        l2TxnReqDirect.mintValue = mockMintValue;
+        l2TxnReqDirect.mintValue = mintValue;
         l2TxnReqDirect.l2Contract = mockL2Contract;
         l2TxnReqDirect.l2Value = mockL2Value;
         l2TxnReqDirect.l2Calldata = mockL2Calldata;
@@ -283,15 +319,25 @@ contract IntegrationTests is BridgeHubIntegration {
 
         return l2TxnReqDirect;
     }
+}
 
-    function test_creationOfHyperchains() public {
-        for (uint i = startChainId; i < chainIds.length; i++) {
-            address newHyperchain = bridgeHub.getHyperchain(i);
-            assert(newHyperchain != address(0));
-        }
+contract IntegrationTests is BridgeHubIntegration, HyperchainFactory, L2TxMocker {
+    address alice;
+    address bob;
+
+    function setUp() public {
+        spawnMultipleHyperchains(2);
+        spawnMultipleHyperchainsWithToken(2, address(token));
+
+        alice = makeAddr("alice");
+        bob = makeAddr("bob");
     }
 
-    function test_bridgeHubChangesSharedBridge() public {
+    function test_bridgeHubChangesSharedBridge_NonEth() public {
+        clearSharedBridgeBalances();
+
+        uint256 mockMintValue = 1 ether;
+
         vm.txGasPrice(0.05 ether);
         vm.deal(alice, 1 ether);
         vm.deal(bob, 1 ether);
@@ -302,17 +348,19 @@ contract IntegrationTests is BridgeHubIntegration {
         assertEq(token.balanceOf(alice), mockMintValue);
         assertEq(token.balanceOf(bob), mockMintValue);
 
+        uint256 firstChainId = hyperchainIds[2];
+        uint256 secondChainId = hyperchainIds[3];
 
-        uint256 firstChainId = chainIds[0];
-        uint256 secondChainId = chainIds[1];
+        assertTrue(getHyperchainBaseToken(firstChainId) == address(token));
+        assertTrue(getHyperchainBaseToken(secondChainId) == address(token));
 
-        L2TransactionRequestDirect memory aliceRequest = createMockL2TransactionRequestDirect(firstChainId);
-        L2TransactionRequestDirect memory bobRequest = createMockL2TransactionRequestDirect(secondChainId);
+        L2TransactionRequestDirect memory aliceRequest = createMockL2TransactionRequestDirect(firstChainId, 1 ether);
+        L2TransactionRequestDirect memory bobRequest = createMockL2TransactionRequestDirect(secondChainId, 1 ether);
 
         bytes32 canonicalHash = keccak256(abi.encode("CANONICAL_TX_HASH"));
-        address firstHyperChainAddress = bridgeHub.getHyperchain(firstChainId);
-        address secondHyperChainAddress = bridgeHub.getHyperchain(secondChainId);
-    
+        address firstHyperChainAddress = getHyperchainAddress(firstChainId);
+        address secondHyperChainAddress = getHyperchainAddress(secondChainId);
+
         vm.startPrank(alice);
         assertEq(token.balanceOf(alice), mockMintValue);
         token.approve(address(sharedBridge), mockMintValue);
@@ -343,6 +391,14 @@ contract IntegrationTests is BridgeHubIntegration {
         bytes32 resultantHash2 = bridgeHub.requestL2TransactionDirect(bobRequest);
         assertEq(canonicalHash, resultantHash2);
 
+        // check if the balances of alice and bob are 0
+        assertEq(token.balanceOf(alice), 0);
+        assertEq(token.balanceOf(bob), 0);
+
+        // check if the shared bridge has the correct balances
+        assertEq(token.balanceOf(address(sharedBridge)), 2 ether);
+
+        // check if the shared bridge has the correct balances for each chain
         assertEq(sharedBridge.chainBalance(firstChainId, address(token)), mockMintValue);
         assertEq(sharedBridge.chainBalance(secondChainId, address(token)), mockMintValue);
     }
