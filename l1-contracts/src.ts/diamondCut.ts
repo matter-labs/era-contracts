2679109b64191c4f48d64bf40519064ebe747bf1
Zach Kolodny <zach.kolodny@gmail.com>
2024-05-07 09:12:58 -0400

    chore(scripts): remove unconditionally removed facets

--- l1-contracts/src.ts/diamondCut.ts
+++ l1-contracts/src.ts/diamondCut.ts
@@ -6,9 +6,6 @@ import { ethers } from "ethers";
 import { IZkSyncHyperchainFactory } from "../typechain/IZkSyncHyperchainFactory";
 import { IZkSyncHyperchainBaseFactory } from "../typechain/IZkSyncHyperchainBaseFactory";
 
-// Some of the facets are to be removed with the upcoming upgrade.
-const UNCONDITIONALLY_REMOVED_FACETS = ["DiamondCutFacet", "GovernanceFacet"];
-
 export enum Action {
   Add = 0,
   Replace = 1,
@@ -132,7 +129,6 @@ export async function getFacetCutsForUpgrade(
 ) {
   const newFacetCuts = await getCurrentFacetCutsForAdd(adminAddress, gettersAddress, mailboxAddress, executorAddress);
   namesOfFacetsToBeRemoved = namesOfFacetsToBeRemoved || [
-    ...UNCONDITIONALLY_REMOVED_FACETS,
     ...Object.keys(newFacetCuts),
   ];
   const oldFacetCuts = await getDeployedFacetCutsForRemove(wallet, zkSyncAddress, namesOfFacetsToBeRemoved);
