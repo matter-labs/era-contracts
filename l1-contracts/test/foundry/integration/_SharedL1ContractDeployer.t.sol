b8780edc29bd1bf6a7258519e1e8c0948c8c9255
tejks <sarmatys.programmer@gmail.com>
2024-05-09 10:28:42 +0200

    feat: Add SharedBridge balance setter

--- l1-contracts/test/foundry/integration/_SharedL1ContractDeployer.t.sol
+++ l1-contracts/test/foundry/integration/_SharedL1ContractDeployer.t.sol
@@ -2,11 +2,15 @@
 pragma solidity 0.8.24;
 
 import {Test} from "forge-std/Test.sol";
-import {DeployL1Script} from "./deploy-scripts/script/DeployL1.s.sol";
+import {StdStorage, stdStorage} from "forge-std/Test.sol";
+
+import {DeployL1Script} from "../../../scripts-rs/script/DeployL1.s.sol";
 import {Bridgehub} from "contracts/bridgehub/Bridgehub.sol";
 import {L1SharedBridge} from "contracts/bridge/L1SharedBridge.sol";
 
 contract L1ContractDeployer is Test {
+    using stdStorage for StdStorage;
+
     address bridgehubProxyAddress;
     address bridgehubOwnerAddress;
     Bridgehub bridgeHub;
@@ -42,5 +46,14 @@ contract L1ContractDeployer is Test {
     function registerL2SharedBridge(uint256 _chainId, address _l2SharedBridge) internal {
         vm.prank(bridgehubOwnerAddress);
         sharedBridge.initializeChainGovernance(_chainId, _l2SharedBridge);
+    }
+
+    function _setSharedBridgeChainBalance(uint256 _chainId, address _token, uint256 _value) internal {
+        stdstore
+            .target(address(sharedBridge))
+            .sig(sharedBridge.chainBalance.selector)
+            .with_key(_chainId)
+            .with_key(_token)
+            .checked_write(_value);
     }
 }
