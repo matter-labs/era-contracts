8854ae8230b395680fabb74d758b1d00211d7ddc
Danil <deniallugo@gmail.com>
2024-05-14 17:07:36 +0200

    Fix admins
    Signed-off-by: Danil <deniallugo@gmail.com>

--- l1-contracts-foundry/script/AcceptAdmin.s.sol
+++ l1-contracts-foundry/script/AcceptAdmin.s.sol
@@ -5,25 +5,51 @@ import {Script} from "forge-std/Script.sol";
 import {stdToml} from "forge-std/StdToml.sol";
 
 import {Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
+import {IZkSyncHyperchain} from "contracts/state-transition/chain-interfaces/IZkSyncHyperchain.sol";
 import {Utils} from "./Utils.sol";
 
 contract AcceptAdmin is Script {
     using stdToml for string;
 
-    // This function should be called by the owner to accept the admin role
-    function run() public {
+    struct Config {
+        address admin;
+        address governor;
+    }
+
+    Config config;
+
+    function initConfig() public {
         string memory root = vm.projectRoot();
         string memory path = string.concat(root, "/script-config/config-accept-admin.toml");
         string memory toml = vm.readFile(path);
-        address admin = toml.readAddress("$.target_addr");
-        address governor = toml.readAddress("$.governor");
-        Ownable2Step adminContract = Ownable2Step(admin);
+        config.admin = toml.readAddress("$.target_addr");
+        config.governor = toml.readAddress("$.governor");
+    }
 
+    // This function should be called by the owner to accept the admin role
+    function acceptOwner() public {
+        initConfig();
+
+        Ownable2Step adminContract = Ownable2Step(config.admin);
         Utils.executeUpgrade({
-            _governor: governor,
+            _governor: config.governor,
             _salt: bytes32(0),
-            _target: admin,
+            _target: config.admin,
             _data: abi.encodeCall(adminContract.acceptOwnership, ()),
+            _value: 0,
+            _delay: 0
+        });
+    }
+
+    // This function should be called by the owner to accept the admin role
+    function acceptAdmin() public {
+        initConfig();
+        IZkSyncHyperchain adminContract = IZkSyncHyperchain(config.admin);
+        Utils.executeUpgrade({
+            _governor: config.governor,
+            _salt: bytes32(0),
+            _target: config.admin,
+            _data: abi.encodeCall(adminContract.acceptAdmin, ()),
             _value: 0,
             _delay: 0
         });
