2586aff3a5df2271cb2a23ac0753228e8d54930b
tommysr <47206288+tommysr@users.noreply.github.com>
2024-05-20 15:48:09 +0200

    add additional setters to utils facet

--- l1-contracts/test/foundry/unit/concrete/Utils/Utils.sol
+++ l1-contracts/test/foundry/unit/concrete/Utils/Utils.sol
@@ -256,7 +256,7 @@ library Utils {
     }
 
     function getUtilsFacetSelectors() public pure returns (bytes4[] memory) {
-        bytes4[] memory selectors = new bytes4[](38);
+        bytes4[] memory selectors = new bytes4[](40);
         selectors[0] = UtilsFacet.util_setChainId.selector;
         selectors[1] = UtilsFacet.util_getChainId.selector;
         selectors[2] = UtilsFacet.util_setBridgehub.selector;
@@ -295,6 +295,8 @@ library Utils {
         selectors[35] = UtilsFacet.util_getIsFrozen.selector;
         selectors[36] = UtilsFacet.util_setTransactionFilterer.selector;
         selectors[37] = UtilsFacet.util_setBaseTokenGasPriceMultiplierDenominator.selector;
+        selectors[38] = UtilsFacet.util_setTotalBatchesExecuted.selector;
+        selectors[39] = UtilsFacet.util_setl2LogsRootHash.selector;
         return selectors;
     }
 
