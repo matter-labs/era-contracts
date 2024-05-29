bcabb20d5a28361f20567864284da63b94f0ecc5
Stanislav Breadless <stanislavbezkor@gmail.com>
2024-05-29 15:04:54 +0200

    fmt

--- l1-contracts/src.ts/deploy.ts
+++ l1-contracts/src.ts/deploy.ts
@@ -820,18 +820,13 @@ export class Deployer {
     const stm = this.stateTransitionManagerContract(this.deployWallet);
     const diamondProxyAddress = await stm.getHyperchain(this.chainId);
     const hyperchain = IZkSyncHyperchainFactory.connect(diamondProxyAddress, this.deployWallet);
-    
+
     const receipt = await (await hyperchain.setPendingAdmin(this.addresses.Governance)).wait();
     if (this.verbose) {
       console.log(`Governance set as pending admin, gas used: ${receipt.gasUsed.toString()}`);
     }
 
-    await this.executeUpgrade(
-      hyperchain.address,
-      0,
-      hyperchain.interface.encodeFunctionData("acceptAdmin"),
-      false
-    );
+    await this.executeUpgrade(hyperchain.address, 0, hyperchain.interface.encodeFunctionData("acceptAdmin"), false);
 
     if (this.verbose) {
       console.log(`Pending admin successfully accepted`);
