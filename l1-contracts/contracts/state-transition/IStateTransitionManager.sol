fe0b7f4d641fff75dd8c8fee4e05a3ec78dbdc9a
Stanislav Bezkorovainyi <stanislavbezkor@gmail.com>
2024-05-31 14:41:32 +0200

    Change how we update chain creation params (#502)

--- l1-contracts/contracts/state-transition/IStateTransitionManager.sol
+++ l1-contracts/contracts/state-transition/IStateTransitionManager.sol
@@ -10,21 +10,28 @@ import {FeeParams} from "./chain-deps/ZkSyncHyperchainStorage.sol";
 /// @dev We use struct instead of raw parameters in `initialize` function to prevent "Stack too deep" error
 /// @param owner The address who can manage non-critical updates in the contract
 /// @param validatorTimelock The address that serves as consensus, i.e. can submit blocks to be processed
+/// @param chainCreationParams The struct that contains the fields that define how a new chain should be created
+/// @param protocolVersion The initial protocol version on the newly deployed chain
+struct StateTransitionManagerInitializeData {
+    address owner;
+    address validatorTimelock;
+    ChainCreationParams chainCreationParams;
+    uint256 protocolVersion;
+}
+
+/// @notice The struct that contains the fields that define how a new chain should be created
+/// within this STM.
 /// @param genesisUpgrade The address that is used in the diamond cut initialize address on chain creation
 /// @param genesisBatchHash Batch hash of the genesis (initial) batch
 /// @param genesisIndexRepeatedStorageChanges The serial number of the shortcut storage key for the genesis batch
 /// @param genesisBatchCommitment The zk-proof commitment for the genesis batch
 /// @param diamondCut The diamond cut for the first upgrade transaction on the newly deployed chain
-/// @param protocolVersion The initial protocol version on the newly deployed chain
-struct StateTransitionManagerInitializeData {
-    address owner;
-    address validatorTimelock;
+struct ChainCreationParams {
     address genesisUpgrade;
     bytes32 genesisBatchHash;
     uint64 genesisIndexRepeatedStorageChanges;
     bytes32 genesisBatchCommitment;
     Diamond.DiamondCutData diamondCut;
-    uint256 protocolVersion;
 }
 
 interface IStateTransitionManager {
@@ -48,8 +55,14 @@ interface IStateTransitionManager {
     /// @notice ValidatorTimelock changed
     event NewValidatorTimelock(address indexed oldValidatorTimelock, address indexed newValidatorTimelock);
 
-    /// @notice InitialCutHash changed
-    event NewInitialCutHash(bytes32 indexed oldInitialCutHash, bytes32 indexed newInitialCutHash);
+    /// @notice chain creation parameters changed
+    event NewChainCreationParams(
+        address genesisUpgrade,
+        bytes32 genesisBatchHash,
+        uint64 genesisIndexRepeatedStorageChanges,
+        bytes32 genesisBatchCommitment,
+        bytes32 newInitialCutHash
+    );
 
     /// @notice new UpgradeCutHash
     event NewUpgradeCutHash(uint256 indexed protocolVersion, bytes32 indexed upgradeCutHash);
@@ -85,9 +98,9 @@ interface IStateTransitionManager {
 
     function initialize(StateTransitionManagerInitializeData calldata _initializeData) external;
 
-    function setInitialCutHash(Diamond.DiamondCutData calldata _diamondCut) external;
-
     function setValidatorTimelock(address _validatorTimelock) external;
+
+    function setChainCreationParams(ChainCreationParams calldata _chainCreationParams) external;
 
     function getChainAdmin(uint256 _chainId) external view returns (address);
 
