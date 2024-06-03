4bdd6fe02557b9a9a0058fb3e976ebb8eec771de
Tomasz Jakubas <47206288+tommysr@users.noreply.github.com>
2024-06-03 10:24:05 +0200

    Revert bridgehub changes (#23)
    * revert bridgehub changes
    
    * removed dummy bridge from erc20bridge tests
    
    * replaced dummy shared bridge with actual bridge

--- l1-contracts/contracts/bridgehub/Bridgehub.sol
+++ l1-contracts/contracts/bridgehub/Bridgehub.sol
@@ -314,11 +314,6 @@ contract Bridgehub is IBridgehub, ReentrancyGuard, Ownable2StepUpgradeable, Paus
 
         address hyperchain = getHyperchain(_request.chainId);
 
-        require(
-            _request.secondBridgeAddress > BRIDGEHUB_MIN_SECOND_BRIDGE_ADDRESS,
-            "Bridgehub: second bridge address too low"
-        ); // to avoid calls to precompiles
-
         // slither-disable-next-line arbitrary-send-eth
         L2TransactionRequestTwoBridgesInner memory outputRequest = IL1SharedBridge(_request.secondBridgeAddress)
             .bridgehubDeposit{value: _request.secondBridgeValue}(
@@ -332,6 +327,10 @@ contract Bridgehub is IBridgehub, ReentrancyGuard, Ownable2StepUpgradeable, Paus
 
         address refundRecipient = AddressAliasHelper.actualRefundRecipient(_request.refundRecipient, msg.sender);
 
+        require(
+            _request.secondBridgeAddress > BRIDGEHUB_MIN_SECOND_BRIDGE_ADDRESS,
+            "Bridgehub: second bridge address too low"
+        ); // to avoid calls to precompiles
         canonicalTxHash = IZkSyncHyperchain(hyperchain).bridgehubRequestL2Transaction(
             BridgehubL2TransactionRequest({
                 sender: _request.secondBridgeAddress,
