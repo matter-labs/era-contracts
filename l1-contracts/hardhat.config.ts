fb4bac2f4d01125ec06ca3e74ccd78fe2270a7a3
Zach Kolodny <zach.kolodny@gmail.com>
2024-06-03 20:38:18 -0400

    undo debug changes to hardhat config

--- l1-contracts/hardhat.config.ts
+++ l1-contracts/hardhat.config.ts
@@ -47,10 +47,6 @@ export default {
         enabled: process.env.TEST_CONTRACTS_FORK === "1",
       },
     },
-    local: {
-      url: "http://127.0.0.1:8545/",
-      accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80", "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"]
-    }
   },
   etherscan: {
     apiKey: process.env.MISC_ETHERSCAN_API_KEY,
