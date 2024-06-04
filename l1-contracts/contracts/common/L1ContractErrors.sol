eb79cc286d76e6454ec3980c4cd3e8d4ebef15df
Zach Kolodny <zach.kolodny@gmail.com>
2024-06-03 20:26:23 -0400

    added error selectors

--- l1-contracts/contracts/common/L1ContractErrors.sol
+++ l1-contracts/contracts/common/L1ContractErrors.sol
@@ -141,36 +141,65 @@ error CantRevertExecutedBatch();
 error PointEvalFailed(bytes);
 // 0xfc7ab1d3
 error EmptyBlobVersionHash(uint256 index);
-// 0x92290acc
+// 0x4c991078
 error TxHashMismatch();
+// 0x92290acc
 error NonEmptyBlobVersionHash(uint256 index);
+// 0xafd53e2f
 error BlobHashCommitmentError(uint256 index, bool blobHashEmpty, bool blobCommitmentEmpty);
+// 0xf3ed9dfa
 error OnlyEraSupported();
+// 0x2078a6a0
 error BatchNotExecuted(uint256 batchNumber);
+// 0xd356e6ba
 error HashedLogIsDefault();
+// 0x6ef9a972
 error BaseTokenGasPriceDenominatorNotSet();
+// 0x00c5a6a9
 error TransactionNotAllowed();
+// 0xc91cf3b1
 error GasPerPubdataMismatch();
+// 0x76da24b9
 error TooManyFactoryDeps();
+// 0xb385a3da
 error MsgValueTooLow(uint256 required, uint256 provided);
+// 0xa6fef710
 error NoFunctionsForDiamondCut();
+// 0xe52478c7
 error UndefinedDiamondCutAction();
+// 0x86bb51b8
 error AddressHasNoCode(address);
+// 0xac4a3f98
 error FacetExists(bytes4 selector, address);
+// 0x4ef79e5a
 error NonZeroAddress(address);
+// 0xd3b6535b
 error SelectorsMustAllHaveSameFreezability();
+// 0xc21b1ab7
 error NonEmptyCalldata();
+// 0x6afd6c20
 error BadReturnData();
+// 0x8e23ac1a
 error MerklePathEmpty();
+// 0x1c500385
 error MerklePathOutOfBounds();
+// 0x9bb54c35
 error MerkleIndexOutOfBounds();
+// 0x63c36549
 error QueueIsEmpty();
+// 0x5f1aa154
 error InvalidUpgradeTxn(UpgradeTxVerifyParam);
+// 0xdd629f86
 error NotEnoughGas();
+// 0x5cb29523
 error InvalidTxType(uint256 txType);
+// 0x4ee1b0ed
 error NewProtocolVersionNotInUpgradeTxn();
+// 0x07218375
 error UnexpectedNumberOfFactoryDeps();
+// 0x101ba748
 error PreviousUpgradeNotFinalized(bytes32 txHash);
+// 0xa0f47245
 error PreviousUpgradeNotCleaned();
 
 enum SharedBridgeKey {
--- l1-contracts/contracts/upgrades/ZkSyncUpgradeErrors.sol
+++ l1-contracts/contracts/upgrades/ZkSyncUpgradeErrors.sol
@@ -2,9 +2,15 @@
 
 pragma solidity 0.8.24;
 
+//0x6d172ab2
 error ProtocolVersionShouldBeGreater(uint256 _oldProtocolVersion, uint256 _newProtocolVersion);
+//0xe1a9736b
 error ProtocolVersionDeltaTooLarge(uint256 _proposedDelta, uint256 _maxDelta);
+//0xa0bdf77d
 error PreviousUpgradeNotFinalized();
+//0xd7f8c13e
 error PreviousUpgradeBatchNotCleared();
+//0x7a47c9a2
 error InvalidChainId();
+//0xd92e233d
 error ZeroAddress();
