1f1649b736ff944c7ead6b9cbd5e37c15e60b7e4
dimazhornyk <dima.zhornyk@gmail.com>
2024-06-11 16:00:28 +0200

    extend conditions

--- l1-contracts/src.ts/deploy.ts
+++ l1-contracts/src.ts/deploy.ts
@@ -820,7 +820,7 @@ export class Deployer {
           console.log(`Validium mode set, gas used: ${receipt5.gasUsed.toString()}`);
         }
       }
-    } else {
+    } else if (useGovernance) {
       await this.executeUpgrade(
         stateTransitionManager.address,
         0,
@@ -832,6 +832,10 @@ export class Deployer {
           "Validium mode can't be set through the governance, please set it separately, using the admin account"
         );
       }
+    } else {
+      console.warn(
+        "BaseTokenMultiplier and Validium mode can't be set through the governance, please set it separately, using the admin account"
+      );
     }
   }
 
