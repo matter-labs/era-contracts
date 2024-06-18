ab961dc7fabf6e8de057e9e6a2a167e2df029a60
Lyova Potyomkin <lyova.potyomkin@gmail.com>
2024-06-18 05:32:58 +0300

    Fix some lints

--- l1-contracts/contracts/state-transition/libraries/PriorityTree.sol
+++ l1-contracts/contracts/state-transition/libraries/PriorityTree.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.0;
 
+// solhint-disable gas-custom-errors
+
 import {DynamicIncrementalMerkle} from "../../common/libraries/openzeppelin/IncrementalMerkle.sol";
 import {Merkle} from "./Merkle.sol";
 
