078619e1469a80284dc3d78ad0b963d88e17ee00
Lyova Potyomkin <lyova.potyomkin@gmail.com>
2024-06-28 17:05:00 +0300

    Fix lint

--- l1-contracts/contracts/common/libraries/Merkle.sol
+++ l1-contracts/contracts/common/libraries/Merkle.sol
@@ -56,7 +56,7 @@ library Merkle {
         require(pathLength < 256, "Merkle: path too long");
         uint256 levelLen = _itemHashes.length;
         // Edge case: we want to be able to prove an element in a single-node tree.
-        require(pathLength > 0 || _startIndex == 0 && levelLen == 1, "Merkle: empty paths");
+        require(pathLength > 0 || (_startIndex == 0 && levelLen == 1), "Merkle: empty paths");
         require(levelLen > 0, "Merkle: nothing to prove");
         require(_startIndex + levelLen <= (1 << pathLength), "Merkle: index/height mismatch");
         bytes32[] memory itemHashes = _itemHashes;
