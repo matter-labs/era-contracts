2a818397a819b5eadb80a1de244bff2f4b1a89b6
Raid Ateir <ra@matterlabs.dev>
2024-07-03 18:00:59 +0400

    Revert "(feat): separate out nullifier from asset router"
    This reverts commit 46955b3827f1d1b42972107baff6452ae52377c1.

--- l1-contracts/contracts/bridge/interfaces/IL1AssetRouter.sol
+++ l1-contracts/contracts/bridge/interfaces/IL1AssetRouter.sol
@@ -6,12 +6,20 @@ import {L2TransactionRequestTwoBridgesInner} from "../../bridgehub/IBridgehub.so
 import {IBridgehub} from "../../bridgehub/IBridgehub.sol";
 import {IL1ERC20Bridge} from "./IL1ERC20Bridge.sol";
 import {IL1NativeTokenVault} from "./IL1NativeTokenVault.sol";
-import {IL1Nullifier} from "./IL1Nullifier.sol";
 
 /// @title L1 Bridge contract interface
 /// @author Matter Labs
 /// @custom:security-contact security@matterlabs.dev
 interface IL1AssetRouter {
+    event LegacyDepositInitiated(
+        uint256 indexed chainId,
+        bytes32 indexed l2DepositTxHash,
+        address indexed from,
+        address to,
+        address l1Asset,
+        uint256 amount
+    );
+
     event BridgehubDepositInitiated(
         uint256 indexed chainId,
         bytes32 indexed txDataHash,
@@ -58,6 +66,53 @@ interface IL1AssetRouter {
 
     event AssetHandlerRegistered(bytes32 indexed assetId, address indexed assetHandlerAddress);
 
+    function isWithdrawalFinalized(
+        uint256 _chainId,
+        uint256 _l2BatchNumber,
+        uint256 _l2MessageIndex
+    ) external view returns (bool);
+
+    function depositLegacyErc20Bridge(
+        address _msgSender,
+        address _l2Receiver,
+        address _l1Token,
+        uint256 _amount,
+        uint256 _l2TxGasLimit,
+        uint256 _l2TxGasPerPubdataByte,
+        address _refundRecipient
+    ) external payable returns (bytes32 txHash);
+
+    function claimFailedDepositLegacyErc20Bridge(
+        address _depositSender,
+        address _l1Token,
+        uint256 _amount,
+        bytes32 _l2TxHash,
+        uint256 _l2BatchNumber,
+        uint256 _l2MessageIndex,
+        uint16 _l2TxNumberInBatch,
+        bytes32[] calldata _merkleProof
+    ) external;
+
+    function claimFailedDeposit(
+        uint256 _chainId,
+        address _depositSender,
+        address _l1Token,
+        uint256 _amount,
+        bytes32 _l2TxHash,
+        uint256 _l2BatchNumber,
+        uint256 _l2MessageIndex,
+        uint16 _l2TxNumberInBatch,
+        bytes32[] calldata _merkleProof
+    ) external;
+
+    function finalizeWithdrawalLegacyErc20Bridge(
+        uint256 _l2BatchNumber,
+        uint256 _l2MessageIndex,
+        uint16 _l2TxNumberInBatch,
+        bytes calldata _message,
+        bytes32[] calldata _merkleProof
+    ) external returns (address l1Receiver, address l1Token, uint256 amount);
+
     function finalizeWithdrawal(
         uint256 _chainId,
         uint256 _l2BatchNumber,
@@ -65,9 +120,15 @@ interface IL1AssetRouter {
         uint16 _l2TxNumberInBatch,
         bytes calldata _message,
         bytes32[] calldata _merkleProof
-    ) external returns (address l1Receiver, bytes32 assetId, uint256 amount);
+    ) external;
+
+    function L1_WETH_TOKEN() external view returns (address);
 
     function BRIDGE_HUB() external view returns (IBridgehub);
+
+    function legacyBridge() external view returns (IL1ERC20Bridge);
+
+    function depositHappened(uint256 _chainId, bytes32 _l2TxHash) external view returns (bytes32);
 
     /// data is abi encoded :
     /// address _l1Token,
@@ -87,6 +148,10 @@ interface IL1AssetRouter {
         uint256 _amount
     ) external payable;
 
+    function bridgehubConfirmL2Transaction(uint256 _chainId, bytes32 _txDataHash, bytes32 _txHash) external;
+
+    function hyperbridgingEnabled(uint256 _chainId) external view returns (bool);
+
     function setAssetHandlerAddressInitial(bytes32 _additionalData, address _assetHandlerAddress) external;
 
     function setAssetHandlerAddressOnCounterPart(
@@ -105,10 +170,6 @@ interface IL1AssetRouter {
 
     function setNativeTokenVault(IL1NativeTokenVault _nativeTokenVault) external;
 
-    function setL1Nullifier(IL1Nullifier _l1Nullifier) external;
-
-    function bridgehubConfirmL2Transaction(uint256 _chainId, bytes32 _txDataHash, bytes32 _txHash) external;
-
     function bridgeRecoverFailedTransfer(
         uint256 _chainId,
         address _depositSender,
@@ -121,9 +182,9 @@ interface IL1AssetRouter {
         bytes32[] calldata _merkleProof
     ) external;
 
-    function _getDepositL2Calldata(
-        address _l1Sender,
-        bytes32 _assetId,
-        bytes memory _transferData
-    ) external view returns (bytes memory);
+    function chainBalance(uint256 _chainId, address _token) external view returns (uint256);
+
+    function transferTokenToNTV(address _token) external;
+
+    function clearChainBalance(uint256 _chainId, address _token) external;
 }
