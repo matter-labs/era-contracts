d1e2500cddc31c21764ca2ec049786ffc0c7b3a5
Moshe Shababo <17073733+moshababo@users.noreply.github.com>
2024-07-02 22:47:41 -0700

    Import items explicitly

--- l2-contracts/contracts/ConsensusAuthority.sol
+++ l2-contracts/contracts/ConsensusAuthority.sol
@@ -2,8 +2,8 @@
 
 pragma solidity 0.8.20;
 
-import "./ValidatorRegistry.sol";
-import "./AttesterRegistry.sol";
+import {ValidatorRegistry} from "./ValidatorRegistry.sol";
+import {AttesterRegistry} from "./AttesterRegistry.sol";
 
 contract ConsensusAuthority {
     // Owner of the contract (i.e., the governance contract).
