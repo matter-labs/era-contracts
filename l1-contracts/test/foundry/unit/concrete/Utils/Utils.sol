ffa839d56165b1a1db1241bd1292cccc9ad15ff3
Zach Kolodny <zach.kolodny@gmail.com>
2024-07-11 13:27:43 -0400

    update tests

--- l1-contracts/test/foundry/unit/concrete/Utils/Utils.sol
+++ l1-contracts/test/foundry/unit/concrete/Utils/Utils.sol
@@ -518,6 +518,20 @@ library Utils {
         }
     }
 
+    function generatePriorityOps(uint256 len) internal pure returns (PriorityOpsBatchInfo[] memory _ops) {
+        _ops = new PriorityOpsBatchInfo[](len);
+        bytes32[] memory empty = new bytes32[](0);
+        PriorityOpsBatchInfo memory info = PriorityOpsBatchInfo({
+            leftPath: empty,
+            rightPath: empty,
+            itemHashes: empty
+        });
+
+        for (uint256 i = 0; i < len; ++i) {
+            _ops[i] = info;
+        }
+    }
+
     // add this to be excluded from coverage report
     function test() internal {}
 }
