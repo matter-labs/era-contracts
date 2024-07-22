eed959edc30e4a11b6c262098f6c1897d65b8353
Moshe Shababo <17073733+moshababo@users.noreply.github.com>
2024-07-22 15:00:42 -0700

    Lint

--- l2-contracts/src/deploy-consensus-registry-through-l1.ts
+++ l2-contracts/src/deploy-consensus-registry-through-l1.ts
@@ -12,38 +12,33 @@ async function main() {
   const program = new Command();
 
   program
-      .version("0.1.0")
-      .name("deploy-consensus-registry")
-      .option("--chain-id <chain-id>")
-      .description("Deploys the validator registry contract to L2");
+    .version("0.1.0")
+    .name("deploy-consensus-registry")
+    .option("--chain-id <chain-id>")
+    .description("Deploys the consensus registry contract to L2");
 
   program.option("--private-key <private-key>").action(async (cmd) => {
     const chainId: string = cmd.chainId ? cmd.chainId : process.env.CHAIN_ETH_ZKSYNC_NETWORK_ID;
     const deployWallet = cmd.privateKey
-        ? new Wallet(cmd.privateKey, provider)
-        : Wallet.fromMnemonic(
-            process.env.MNEMONIC ? process.env.MNEMONIC : ethTestConfig.mnemonic,
-            "m/44'/60'/0'/0/1"
+      ? new Wallet(cmd.privateKey, provider)
+      : Wallet.fromMnemonic(
+          process.env.MNEMONIC ? process.env.MNEMONIC : ethTestConfig.mnemonic,
+          "m/44'/60'/0'/0/1"
         ).connect(provider);
     console.log(`Using deployer wallet: ${deployWallet.address}`);
 
     const bytecode = hre.artifacts.readArtifactSync("ConsensusRegistry").bytecode;
     const create2Salt = ethers.constants.HashZero;
-    const address = computeL2Create2Address(
-        deployWallet,
-        bytecode,
-        deployWallet.address,
-        create2Salt
-    );
+    const address = computeL2Create2Address(deployWallet, bytecode, deployWallet.address, create2Salt);
 
     // TODO: request from API how many L2 gas needs for the transaction.
     await create2DeployFromL1(
-        chainId,
-        deployWallet,
-        bytecode,
-        deployWallet.address,
-        create2Salt,
-        priorityTxMaxGasLimit
+      chainId,
+      deployWallet,
+      bytecode,
+      deployWallet.address,
+      create2Salt,
+      priorityTxMaxGasLimit
     );
 
     console.log(`CONTRACTS_L2_CONSENSUS_REGISTRY_ADDR=${address}`);
@@ -53,8 +48,8 @@ async function main() {
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch((err) => {
-      console.error("Error:", err);
-      process.exit(1);
-    });
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.error("Error:", err);
+    process.exit(1);
+  });
