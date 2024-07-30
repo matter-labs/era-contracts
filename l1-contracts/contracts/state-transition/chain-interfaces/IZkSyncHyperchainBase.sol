15447c75685352c2fceb21c78d2d89c2b0ae713d
kelemeno <kl@matterlabs.dev>
2024-07-30 23:49:54 +0100

    wrong license

--- l1-contracts/contracts/state-transition/chain-interfaces/IZkSyncHyperchainBase.sol
+++ l1-contracts/contracts/state-transition/chain-interfaces/IZkSyncHyperchainBase.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
 /// @title The interface of the zkSync contract, responsible for the main zkSync logic.
