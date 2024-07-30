f07e7506553e81160b6f451457442c2386710cee
kelemeno <kl@matterlabs.dev>
2024-07-30 23:45:14 +0100

    extra imports

--- l1-contracts/contracts/state-transition/chain-deps/facets/Mailbox.sol
+++ l1-contracts/contracts/state-transition/chain-deps/facets/Mailbox.sol
@@ -26,9 +26,6 @@ import {REQUIRED_L2_GAS_PRICE_PER_PUBDATA, L1_GAS_PER_PUBDATA_BYTE, L2_L1_LOGS_T
 import {L2_BOOTLOADER_ADDRESS, L2_TO_L1_MESSENGER_SYSTEM_CONTRACT_ADDR, L2_BRIDGEHUB_ADDR} from "../../../common/L2ContractAddresses.sol";
 
 import {IL1AssetRouter} from "../../../bridge/interfaces/IL1AssetRouter.sol";
-import {IBridgehub} from "../../../bridgehub/IBridgehub.sol";
-
-import {IStateTransitionManager} from "../../IStateTransitionManager.sol";
 
 // While formally the following import is not used, it is needed to inherit documentation from it
 import {IZkSyncHyperchainBase} from "../../chain-interfaces/IZkSyncHyperchainBase.sol";
