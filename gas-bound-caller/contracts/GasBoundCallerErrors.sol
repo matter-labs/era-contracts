7aaa639b10889b981b88d70c88eee77837d444dd
Zach Kolodny <zach.kolodny@gmail.com>
2024-08-05 12:50:21 -0400

    revert GBC changes to keep same bytecode

--- gas-bound-caller/contracts/GasBoundCallerErrors.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-// We use a floating point pragma here so it can be used within other projects that interact with the zkSync ecosystem without using our exact pragma version.
-pragma solidity ^0.8.21;
-
-// 0x8fdb52d9
-error InsufficientGasProvided();
-// 0xa303e7dd
-error NotEnoughGasForPubdata();
