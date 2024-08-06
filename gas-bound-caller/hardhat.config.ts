1948b7118eb9601811799387e893f617dec55286
Zach Kolodny <zach.kolodny@gmail.com>
2024-08-06 13:39:42 -0400

    revert config changes

--- gas-bound-caller/hardhat.config.ts
+++ gas-bound-caller/hardhat.config.ts
@@ -1,15 +1,30 @@
 import "@matterlabs/hardhat-zksync-chai-matchers";
 import "@matterlabs/hardhat-zksync-node";
 import "@matterlabs/hardhat-zksync-solc";
-import "@matterlabs/hardhat-zksync-verify";
 import "@nomiclabs/hardhat-ethers";
 import "hardhat-typechain";
 
+// This version of system contracts requires a pre release of the compiler
+const COMPILER_VERSION = "1.5.0";
+const PRE_RELEASE_VERSION = "prerelease-a167aa3-code4rena";
+function getZksolcUrl(): string {
+  // @ts-ignore
+  const platform = { darwin: "macosx", linux: "linux", win32: "windows" }[process.platform];
+  // @ts-ignore
+  const toolchain = { linux: "-musl", win32: "-gnu", darwin: "" }[process.platform];
+  const arch = process.arch === "x64" ? "amd64" : process.arch;
+  const ext = process.platform === "win32" ? ".exe" : "";
+
+  return `https://github.com/matter-labs/era-compiler-solidity/releases/download/${PRE_RELEASE_VERSION}/zksolc-${platform}-${arch}${toolchain}-v${COMPILER_VERSION}${ext}`;
+}
+
+console.log(`Using zksolc from ${getZksolcUrl()}`);
+
 export default {
   zksolc: {
-    version: "1.5.0",
     compilerSource: "binary",
     settings: {
+      compilerPath: getZksolcUrl(),
       isSystem: true,
     },
   },
@@ -39,20 +54,6 @@ export default {
       url: "http://127.0.0.1:8011",
       ethNetwork: "localhost",
       zksync: true,
-    },
-    zkSyncTestnet: {
-      url: "https://sepolia.era.zksync.dev",
-      ethNetwork: "sepolia",
-      zksync: true,
-      // contract verification endpoint
-      verifyURL: "https://explorer.sepolia.era.zksync.dev/contract_verification",
-    },
-    zkSyncMainnet: {
-      url: "https://mainnet.era.zksync.io",
-      ethNetwork: "mainnet",
-      zksync: true,
-      // contract verification endpoint
-      verifyURL: "https://zksync2-mainnet-explorer.zksync.io/contract_verification",
     },
   },
   paths: {
