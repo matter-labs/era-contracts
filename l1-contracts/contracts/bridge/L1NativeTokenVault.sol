e48fef12d9c774b6b0b832796687facddae32e05
Raid5594 <52794079+Raid5594@users.noreply.github.com>
2024-08-06 16:32:53 +0400

    (fix): L-06 ETH’s L2StandardERC20 Representation Does Not Have name or symbol (#629)
    Co-authored-by: Raid Ateir <ra@matterlabs.dev>

--- l1-contracts/contracts/bridge/L1NativeTokenVault.sol
+++ l1-contracts/contracts/bridge/L1NativeTokenVault.sol
@@ -182,8 +182,8 @@ contract L1NativeTokenVault is IL1NativeTokenVault, IL1AssetHandler, Ownable2Ste
     /// @dev Receives and parses (name, symbol, decimals) from the token contract
     function getERC20Getters(address _token) public view returns (bytes memory) {
         if (_token == ETH_TOKEN_ADDRESS) {
-            bytes memory name = bytes("Ether");
-            bytes memory symbol = bytes("ETH");
+            bytes memory name = abi.encode("Ether");
+            bytes memory symbol = abi.encode("ETH");
             bytes memory decimals = abi.encode(uint8(18));
             return abi.encode(name, symbol, decimals); // when depositing eth to a non-eth based chain it is an ERC20
         }
