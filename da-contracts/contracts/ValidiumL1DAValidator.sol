15064e7d8189f54e06c5247f5a44f3a1312dce01
kelemeno <kl@matterlabs.dev>
2024-08-07 12:01:41 +0100

    eip 165 cleanup

--- da-contracts/contracts/ValidiumL1DAValidator.sol
+++ da-contracts/contracts/ValidiumL1DAValidator.sol
@@ -24,9 +24,6 @@ contract ValidiumL1DAValidator is IL1DAValidator {
     }
 
     function supportsInterface(bytes4 interfaceId) external pure returns (bool) {
-        if (interfaceId == this.supportsInterface.selector) {
-            return true;
-        }
-        return interfaceId == type(IL1DAValidator).interfaceId;
+        return (interfaceId == this.supportsInterface.selector) || (interfaceId == type(IL1DAValidator).interfaceId);
     }
 }
