42839e665c4f27600a11aaf7df90a2dbe08573cb
kelemeno <34402761+kelemeno@users.noreply.github.com>
2024-08-08 12:36:16 +0100

    fix: pragma versions (#657)

--- system-contracts/contracts/interfaces/IL2GenesisUpgrade.sol
+++ system-contracts/contracts/interfaces/IL2GenesisUpgrade.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
+// We use a floating point pragma here so it can be used within other projects that interact with the zkSync ecosystem without using our exact pragma version.
 pragma solidity ^0.8.20;
 
 interface IL2GenesisUpgrade {
