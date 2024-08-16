572e1b32a457bae4e3c3fc6671f8f8947e653ce8
Stanislav Breadless <stanislavbezkor@gmail.com>
2024-08-16 13:34:42 +0200

    getters

--- l1-contracts/contracts/state-transition/chain-interfaces/IGetters.sol
+++ l1-contracts/contracts/state-transition/chain-interfaces/IGetters.sol
@@ -34,7 +34,7 @@ interface IGetters is IZkSyncHyperchainBase {
     function getBaseToken() external view returns (address);
 
     /// @return The chain id of the ZK Chain.
-    function getChainId() external view returns (uint256)
+    function getChainId() external view returns (uint256);
 
     /// @return The address of the base token bridge
     function getBaseTokenBridge() external view returns (address);
