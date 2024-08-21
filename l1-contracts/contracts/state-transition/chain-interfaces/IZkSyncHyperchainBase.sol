a405f71ad8803d4a44f9f3a1f714a6fbef4662c5
Zach Kolodny <zach.kolodny@gmail.com>
2024-08-21 19:06:09 -0400

    chore(contracts): add missing spdx liceses

--- l1-contracts/contracts/state-transition/chain-interfaces/IZkSyncHyperchainBase.sol
+++ l1-contracts/contracts/state-transition/chain-interfaces/IZkSyncHyperchainBase.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 // We use a floating point pragma here so it can be used within other projects that interact with the zkSync ecosystem without using our exact pragma version.
 pragma solidity ^0.8.21;
 
--- l1-contracts/deploy-scripts/DeployL2Contracts.sol
+++ l1-contracts/deploy-scripts/DeployL2Contracts.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.21;
 
 import {Script} from "forge-std/Script.sol";
