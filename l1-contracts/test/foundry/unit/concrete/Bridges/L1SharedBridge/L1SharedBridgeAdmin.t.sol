3f17b69c087a00d8c1b135f16a756d402baea7af
Stanislav Breadless <stanislavbezkor@gmail.com>
2024-08-21 10:56:10 +0200

    remove unused imports

--- l1-contracts/test/foundry/unit/concrete/Bridges/L1SharedBridge/L1SharedBridgeAdmin.t.sol
+++ l1-contracts/test/foundry/unit/concrete/Bridges/L1SharedBridge/L1SharedBridgeAdmin.t.sol
@@ -3,18 +3,6 @@ pragma solidity 0.8.24;
 
 import {L1SharedBridgeTest} from "./_L1SharedBridge_Shared.t.sol";
 
-import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
-import {L1SharedBridge} from "contracts/bridge/L1SharedBridge.sol";
-import {ETH_TOKEN_ADDRESS} from "contracts/common/Config.sol";
-import {IBridgehub} from "contracts/bridgehub/IBridgehub.sol";
-import {L2Message, TxStatus} from "contracts/common/Messaging.sol";
-import {IMailbox} from "contracts/state-transition/chain-interfaces/IMailbox.sol";
-import {IL1ERC20Bridge} from "contracts/bridge/interfaces/IL1ERC20Bridge.sol";
-import {L2_BASE_TOKEN_SYSTEM_CONTRACT_ADDR} from "contracts/common/L2ContractAddresses.sol";
-import {IGetters} from "contracts/state-transition/chain-interfaces/IGetters.sol";
-
 /// We are testing all the specified revert and require cases.
 contract L1SharedBridgeAdminTest is L1SharedBridgeTest {
     function testAdminCanInitializeChainGovernance() public {
