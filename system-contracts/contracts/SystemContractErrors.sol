17666b2b828656da6dee7979f81432d854a1b19b
IAvecilla <ignacio.avecilla@lambdaclass.com>
2024-08-22 11:09:02 -0300

    Use back the floating point version for specific contracts

--- system-contracts/contracts/SystemContractErrors.sol
+++ system-contracts/contracts/SystemContractErrors.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.24;
+// We use a floating point pragma here so it can be used within other projects that interact with the zkSync ecosystem without using our exact pragma version.
+pragma solidity ^0.8.24;
 
 // 0x86bb51b8
 error AddressHasNoCode(address);
