f741346ab9b84a14c6158aaf5b3d095fa2b538e8
Stanislav Breadless <stanislavbezkor@gmail.com>
2024-08-22 13:13:01 +0200

    wip

--- l2-contracts/test/foundry/unit/erc20/L1ERC20Bridge.t.sol
+++ l2-contracts/test/foundry/unit/erc20/L1ERC20Bridge.t.sol
@@ -2,9 +2,73 @@
 
 pragma solidity 0.8.20;
 
+import {Vm} from "forge-std/Vm.sol";
+
+import {Script, console2 as console} from "forge-std/Script.sol";
+
 import {Test} from "forge-std/Test.sol";
 
 import {L2StandardERC20} from "contracts/bridge/L2StandardERC20.sol";
+import {L2AssetRouter}  from "contracts/bridge/L2AssetRouter.sol";
+
+import { IContractDeployer, DEPLOYER_SYSTEM_CONTRACT, L2ContractHelper, L2_ASSET_ROUTER } from "contracts/L2ContractHelper.sol";
+
+
+contract SomeOtherContract {
+    constructor() {
+
+    }
+}
+
+library Utils {
+    address internal constant VM_ADDRESS = address(uint160(uint256(keccak256("hevm cheat code"))));
+    Vm internal constant vm = Vm(VM_ADDRESS);
+
+    address constant L2_FORCE_DEPLOYER_ADDR = address(0x8007);
+
+    string internal constant L2_ASSET_ROUTER_PATH = "./zkout/L2AssetRouter.sol/L2AssetRouter.json"; 
+
+    function readEraBytecode(string memory _path) internal returns (bytes memory bytecode) {
+        string memory artifact = vm.readFile(_path);
+        bytecode = vm.parseJsonBytes(artifact, ".bytecode.object");
+    }
+
+    function forceDeployAssetRouter(
+        uint256 _l1ChainId, uint256 _eraChainId, address _l1AssetRouter, address _legacySharedBridge
+    ) internal {
+        // to ensure that the bytecode is known
+        {
+            L2AssetRouter dummy = new L2AssetRouter(_l1ChainId, _eraChainId, _l1AssetRouter, _legacySharedBridge);
+            bytes32 myhash;
+            assembly {
+                myhash := extcodehash(dummy)
+            }
+            console.logBytes32(myhash);
+        }
+
+        bytes memory bytecode = readEraBytecode(L2_ASSET_ROUTER_PATH);
+
+        bytes32 bytecodehash = L2ContractHelper.hashL2BytecodeMemory(bytecode);
+        console.logBytes32(bytecodehash);
+
+        IContractDeployer.ForceDeployment[] memory deployments = new IContractDeployer.ForceDeployment[](1);
+        deployments[0] = IContractDeployer.ForceDeployment({
+            bytecodeHash: L2ContractHelper.hashL2BytecodeMemory(bytecode),
+            newAddress: address(L2_ASSET_ROUTER),
+            callConstructor: true,
+            value: 0,
+            input: abi.encode(_l1ChainId, _eraChainId, _l1AssetRouter, _legacySharedBridge)
+        });
+
+        // console.log(IContractDeployer(DEPLOYER_SYSTEM_CONTRACT).getNewAddressCreate2(address(0), bytes32(0), bytes32(0), new bytes(0)));
+
+        // vm.zkVm(true);
+        vm.prank(L2_FORCE_DEPLOYER_ADDR);
+        IContractDeployer(DEPLOYER_SYSTEM_CONTRACT).forceDeployOnAddresses(
+            deployments
+        );
+    }
+}
 
 contract L1Erc20BridgeTest is Test {
     // L1ERC20Bridge internal bridge;
@@ -39,6 +103,6 @@ contract L1Erc20BridgeTest is Test {
     // function test() internal virtual {}
 
     function test_Stuff() public {
-        L2StandardERC20 l2StandardERC20 = new L2StandardERC20();
+        Utils.forceDeployAssetRouter(9, 9, address(1), address(0));
     }
 }
