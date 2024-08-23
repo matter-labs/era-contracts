0ecbe61e0794ae9e4c82ff230e13e14ea91d84a0
Javier Chatruc <jrchatruc@gmail.com>
2024-08-23 19:03:54 -0300

    Check for the `to` address to be zero when dispatching to createEVM on the `DefaultAccount`

--- system-contracts/contracts/DefaultAccount.sol
+++ system-contracts/contracts/DefaultAccount.sol
@@ -140,7 +140,7 @@ contract DefaultAccount is IAccount {
         uint32 gas = Utils.safeCastToU32(gasleft());
 
         // TODO: if possible, maybe implement some way to avoid memory copying here.
-        if (_transaction.reserved[1] != 0) {
+        if ((_transaction.reserved[1] != 0) && (to == address(0))) {
             DEPLOYER_SYSTEM_CONTRACT.createEVM{value: value}(data);
             return;
         }
