1d361926beeb7f776d7e685286482e980ccff210
IAvecilla <ignacio.avecilla@lambdaclass.com>
2024-08-27 15:49:28 -0300

    Remove download compiler step since old hardhat version already does it

--- system-contracts/scripts/utils.ts
+++ system-contracts/scripts/utils.ts
@@ -207,15 +207,6 @@ export async function getSolcLocation(): Promise<string> {
 
   const solcVersion = hre.config.solidity.compilers[0].version;
 
-  if (!(await downloader.isCompilerDownloaded(solcVersion))) {
-    console.log(`Compiler ${solcVersion} not found, downloading...`);
-    await downloader.downloadCompiler(
-      solcVersion,
-      async () => {},
-      async () => {}
-    );
-  }
-
   return (await downloader.getCompiler(solcVersion))!.compilerPath;
 }
 
