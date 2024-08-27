ad52dd8bc455a578d357ba26db7ef1b7fbe161aa
IAvecilla <ignacio.avecilla@lambdaclass.com>
2024-08-27 13:19:07 -0300

    Fix lint errors

--- system-contracts/scripts/utils.ts
+++ system-contracts/scripts/utils.ts
@@ -207,9 +207,13 @@ export async function getSolcLocation(): Promise<string> {
 
   const solcVersion = hre.config.solidity.compilers[0].version;
 
-  if (!await downloader.isCompilerDownloaded(solcVersion)) {
+  if (!(await downloader.isCompilerDownloaded(solcVersion))) {
     console.log(`Compiler ${solcVersion} not found, downloading...`);
-    await downloader.downloadCompiler(solcVersion, async () => false, async () => false);
+    await downloader.downloadCompiler(
+      solcVersion,
+      async () => {},
+      async () => {}
+    );
   }
 
   return (await downloader.getCompiler(solcVersion))!.compilerPath;
