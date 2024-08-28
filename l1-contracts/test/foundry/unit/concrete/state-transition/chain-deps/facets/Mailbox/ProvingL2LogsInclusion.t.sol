a102e7589eab8d11dbef9df5e01cf8cdb026f20e
Stanislav Breadless <stanislavbezkor@gmail.com>
2024-08-28 11:16:21 +0200

    fix l1 tests

--- l1-contracts/test/foundry/unit/concrete/state-transition/chain-deps/facets/Mailbox/ProvingL2LogsInclusion.t.sol
+++ l1-contracts/test/foundry/unit/concrete/state-transition/chain-deps/facets/Mailbox/ProvingL2LogsInclusion.t.sol
@@ -224,7 +224,7 @@ contract MailboxL2LogsProve is MailboxTest {
         }
 
         // Prove log inclusion reverts
-        _proveL2LogInclusion(batchNumber, secondLogIndex, log, secondLogProof, bytes(HashedLogIsDefault.selector));
+        _proveL2LogInclusion(batchNumber, secondLogIndex, log, secondLogProof, bytes.concat(HashedLogIsDefault.selector));
     }
 
     function test_success_proveL1ToL2TransactionStatus() public {
