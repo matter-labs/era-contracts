edc55d1c0ec1659ba52697373b6433ee23058fd7
qedk <1994constant@gmail.com>
2024-08-30 19:42:26 +0400

    feat: add avail validium impl

--- da-contracts/contracts/AvailValidiumL1DAValidator.sol
+++ da-contracts/contracts/AvailValidiumL1DAValidator.sol
@@ -5,8 +5,12 @@ pragma solidity 0.8.24;
 // solhint-disable gas-custom-errors, reason-string
 
 import {IL1DAValidator, L1DAValidatorOutput} from "./IL1DAValidator.sol";
+import {IAvailBridge} from "./IAvailBridge.sol";
+import {AvailAttestationLib} from "./AvailAttestationLib.sol";
 
-contract ValidiumL1DAValidator is IL1DAValidator {
+contract ValidiumL1DAValidator is IL1DAValidator, AvailAttestationLib {
+    constructor(IAvailBridge _availBridge) AvailAttestationLib(_availBridge) {}
+
     function checkDA(
         uint256, // _chainId
         uint256, // _batchNumber
@@ -19,6 +23,25 @@ contract ValidiumL1DAValidator is IL1DAValidator {
         require(_operatorDAInput.length == 32, "ValL1DA wrong input length");
 
         bytes32 stateDiffHash = abi.decode(_operatorDAInput, (bytes32));
+
+        // The rest of the fields that relate to blobs are empty.
+        output.stateDiffHash = stateDiffHash;
+    }
+
+    function checkDA(
+        uint256, // _chainId
+        uint256, // _batchNumber
+        bytes32, // _l2DAValidatorOutputHash
+        bytes calldata _operatorDAInput,
+        IAvailBridge.MerkleProofInput calldata _attestationProof,
+        uint256 // maxBlobsSupported
+    ) external returns (L1DAValidatorOutput memory output) {
+        // For Validiums, we expect the operator to just provide the data for us.
+        // We don't need to do any checks with regard to the l2DAValidatorOutputHash.
+        require(_operatorDAInput.length == 32, "ValL1DA wrong input length");
+
+        bytes32 stateDiffHash = abi.decode(_operatorDAInput, (bytes32));
+        _attest(_attestationProof);
 
         // The rest of the fields that relate to blobs are empty.
         output.stateDiffHash = stateDiffHash;
