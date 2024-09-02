1355f5ff0402db3797fbae3338e3a439328d8133
Yberjon <nunnom1@gmail.com>
2024-09-02 13:52:07 +0200

    L1 Bridge Errors

---
+++ l1-contracts/contracts/bridge/L1BridgeContractErrors.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.21;
+
+error NotNTV();
+
+error EthTransferFailed();
+
+error NativeTokenVaultAlreadySet();
+
+error NativeTokenVault0();
+
+error LegacycFD();
+
+error LegacyEthWithdrawal();
+
+error LegacyTokenWithdrawal();
+
+error WrongMsgLength();
+
+error NotNTVorADT();
+
+error AssetHandlerNotSet();
+
+error NewEncodingFormatNotYetSupportedForNTV();
+
+error AssetHandlerDoesNotExistForAssetId();
+
+error EthOnlyAcceptedFromSharedBridge();
+
+error ZeroAmountToTransfer();
+
+error WrongAmountTransferred();
+
+error EmptyToken();
+
+error ClaimFailedDepositFailed();
+
+
