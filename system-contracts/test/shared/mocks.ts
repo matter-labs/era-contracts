8e778e1b20740228f4d4f4e49c79d0c866d4650e
Stanislav Breadless <stanislavbezkor@gmail.com>
2024-09-04 12:59:11 +0200

    fix spellcheck

--- system-contracts/test/shared/mocks.ts
+++ system-contracts/test/shared/mocks.ts
@@ -44,7 +44,7 @@ const TEST_SYSTEM_CONTRACTS_MOCKS = {
   // For bridgehub we mock the real address for simplicity.
   // In case of need, it can be ported to use the test address.
   IBridgehub: REAL_BRIDGEHUB_ADDRESS,
-  // For similar reasons we mock the L2 message real root only for simplicitly
+  // For similar reasons we mock the L2 message real root only for simplicity
   IMessageRoot: REAL_L2_MESSAGE_ROOT_ADDRESS,
 };
 
