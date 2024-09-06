ef3353982c5bec076d0812a53649f19da76720ed
Stanislav Breadless <stanislavbezkor@gmail.com>
2024-09-06 13:44:44 +0200

    lint

--- l1-contracts/test/foundry/unit/concrete/Bridgehub/experimental_bridge.t.sol
+++ l1-contracts/test/foundry/unit/concrete/Bridgehub/experimental_bridge.t.sol
@@ -339,7 +339,11 @@ contract ExperimentalBridgeTest is Test {
         bridgeHub.addToken(randomAddress);
     }
 
-    function test_addToken_cannotBeCalledByRandomAddress(address randomAddress, address randomCaller, uint256 randomValue) public useRandomToken(randomValue) {
+    function test_addToken_cannotBeCalledByRandomAddress(
+        address randomAddress,
+        address randomCaller,
+        uint256 randomValue
+    ) public useRandomToken(randomValue) {
         vm.assume(randomCaller != bridgeOwner);
         vm.assume(randomCaller != bridgeHub.admin());
 
