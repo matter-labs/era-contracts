0f1383aef1173d5971a67c9af6811036e8e9bfcd
Javier Chatruc <jrchatruc@gmail.com>
2024-09-10 16:27:46 -0300

    Fix foundry L1 tests

--- l1-contracts/test/foundry/unit/concrete/Utils/Utils.sol
+++ l1-contracts/test/foundry/unit/concrete/Utils/Utils.sol
@@ -210,7 +210,7 @@ library Utils {
     }
 
     function getGettersSelectors() public pure returns (bytes4[] memory) {
-        bytes4[] memory selectors = new bytes4[](30);
+        bytes4[] memory selectors = new bytes4[](31);
         selectors[0] = GettersFacet.getVerifier.selector;
         selectors[1] = GettersFacet.getAdmin.selector;
         selectors[2] = GettersFacet.getPendingAdmin.selector;
