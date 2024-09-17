11d4bc5596d1c64b995d222ddaf35aaf0b068f55
dimazhornyk <dima.zhornyk@gmail.com>
2024-09-17 13:09:37 +0200

    fix contract name

--- l2-contracts/contracts/data-availability/da-layers/LayerNameL2DAValidator.sol
+++ l2-contracts/contracts/data-availability/da-layers/LayerNameL2DAValidator.sol
@@ -5,7 +5,7 @@ pragma solidity 0.8.24;
 import {IL2DAValidator} from "../../interfaces/IL2DAValidator.sol";
 
 /// Rollup DA validator. It will publish data that would allow to use either calldata or blobs.
-contract ValidiumL2DAValidator is IL2DAValidator {
+contract LayerNameL2DAValidator is IL2DAValidator {
     function validatePubdata(
         // The rolling hash of the user L2->L1 logs.
         bytes32,
