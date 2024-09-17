b9c2fef049440f4f06630c991ac7e30b8d5e4bb1
kelemeno <kl@matterlabs.dev>
2024-09-17 11:47:36 +0100

    lint

--- l2-contracts/contracts/interfaces/IConsensusRegistry.sol
+++ l2-contracts/contracts/interfaces/IConsensusRegistry.sol
@@ -2,7 +2,6 @@
 // We use a floating point pragma here so it can be used within other projects that interact with the ZKsync ecosystem without using our exact pragma version.
 pragma solidity ^0.8.20;
 
-
 /// @author Matter Labs
 /// @custom:security-contact security@matterlabs.dev
 /// @title ConsensusRegistry contract interface
