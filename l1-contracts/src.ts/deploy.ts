cd7f4b764c04c8464e738e702bafbf07cba914fa
Stanislav Breadless <stanislavbezkor@gmail.com>
2024-09-20 12:25:26 +0200

    hopefully fix l1 contracts ci

--- l1-contracts/src.ts/deploy.ts
+++ l1-contracts/src.ts/deploy.ts
@@ -1486,14 +1486,12 @@ export class Deployer {
     const l1SharedBridge = this.defaultSharedBridge(this.deployWallet);
 
     if (isCurrentNetworkLocal()) {
-      const eraChainId = getNumberFromEnv("CONTRACTS_ERA_CHAIN_ID");
-
       const l2SharedBridgeImplementationBytecode = L2_SHARED_BRIDGE_IMPLEMENTATION.bytecode;
 
       const l2SharedBridgeImplAddress = computeL2Create2Address(
         this.deployWallet.address,
         l2SharedBridgeImplementationBytecode,
-        ethers.utils.defaultAbiCoder.encode(["uint256"], [eraChainId]),
+        '0x',
         ethers.constants.HashZero
       );
 
@@ -1502,7 +1500,6 @@ export class Deployer {
       const l2SharedBridgeInterface = new Interface(L2_SHARED_BRIDGE_IMPLEMENTATION.abi);
       const proxyInitializationParams = l2SharedBridgeInterface.encodeFunctionData("initialize", [
         l1SharedBridge.address,
-        this.addresses.Bridges.ERC20BridgeProxy,
         hashL2Bytecode(L2_STANDARD_TOKEN_PROXY.bytecode),
         l2GovernorAddress,
       ]);
@@ -1561,7 +1558,7 @@ export class Deployer {
     const l2SharedBridgeImplAddress = computeL2Create2Address(
       this.deployWallet.address,
       l2SharedBridgeImplementationBytecode,
-      ethers.utils.defaultAbiCoder.encode(["uint256"], [eraChainId]),
+      '0x',
       ethers.constants.HashZero
     );
     this.addresses.Bridges.L2LegacySharedBridgeImplementation = l2SharedBridgeImplAddress;
@@ -1602,7 +1599,6 @@ export class Deployer {
     const l2SharedBridgeInterface = new Interface(L2_SHARED_BRIDGE_IMPLEMENTATION.abi);
     const proxyInitializationParams = l2SharedBridgeInterface.encodeFunctionData("initialize", [
       l1SharedBridge.address,
-      this.addresses.Bridges.ERC20BridgeProxy,
       hashL2Bytecode(L2_STANDARD_TOKEN_PROXY.bytecode),
       l2GovernorAddress,
     ]);
