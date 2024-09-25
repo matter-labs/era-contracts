b66f74c0b0c771b69d0c1d8fe5899a9c164658f1
Stanislav Breadless <stanislavbezkor@gmail.com>
2024-09-25 16:05:44 +0200

    fix lint

--- l1-contracts/test/foundry/l2/unit/L2AdminFactory/L2AdminFactory.t.sol
+++ l1-contracts/test/foundry/l2/unit/L2AdminFactory/L2AdminFactory.t.sol
@@ -5,9 +5,9 @@ pragma solidity ^0.8.20;
 import {Test} from "forge-std/Test.sol";
 
 import {IBridgehub} from "contracts/bridgehub/IBridgehub.sol";
-import { L2AdminFactory } from "contracts/governance/L2AdminFactory.sol";
-import { PermanentRestriction } from "contracts/governance/PermanentRestriction.sol";
-import { IPermanentRestriction } from "contracts/governance/IPermanentRestriction.sol";
+import {L2AdminFactory} from "contracts/governance/L2AdminFactory.sol";
+import {PermanentRestriction} from "contracts/governance/PermanentRestriction.sol";
+import {IPermanentRestriction} from "contracts/governance/IPermanentRestriction.sol";
 
 contract L2AdminFactoryTest is Test {
     function testL2AdminFactory() public {
@@ -23,16 +23,12 @@ contract L2AdminFactoryTest is Test {
         allRestrictions[0] = requiredRestrictions[0];
         allRestrictions[1] = additionalRestrictions[0];
 
-
         bytes32 salt = keccak256("salt");
 
         address admin = factory.deployAdmin(additionalRestrictions, salt);
 
         // Now, we need to check whether it would be able to accept such an admin
-        PermanentRestriction restriction = new PermanentRestriction(
-            IBridgehub(address(0)),
-            address(factory)
-        );
+        PermanentRestriction restriction = new PermanentRestriction(IBridgehub(address(0)), address(factory));
 
         bytes32 codeHash;
         assembly {
@@ -41,10 +37,6 @@ contract L2AdminFactoryTest is Test {
 
         vm.expectEmit(true, false, false, true);
         emit IPermanentRestriction.AllowL2Admin(admin);
-        restriction.allowL2Admin(
-            salt,
-            codeHash,
-            keccak256(abi.encode(allRestrictions))
-        );
+        restriction.allowL2Admin(salt, codeHash, keccak256(abi.encode(allRestrictions)));
     }
 }
