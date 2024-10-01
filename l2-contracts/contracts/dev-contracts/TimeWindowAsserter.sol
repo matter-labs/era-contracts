d458096e4e0c9c3da188622dea1d86b01c371252
Ivan Schasny <ivan.schasny@gmail.com>
2024-10-01 11:33:30 +0100

    Add time window asserter

---
+++ l2-contracts/contracts/dev-contracts/TimeWindowAsserter.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+interface ITimeWindowAsserter {
+    function assertTimestampInRange(uint256 a, uint256 b) external view;
+}
+
+contract TimeWindowAsserter is ITimeWindowAsserter {
+    // Minimum interval that the end of the time window should be ahead of the block.timestamp in seconds
+    uint256 public constant MIN_INTERVAL = 60;
+    // Minimum difference between time window start and end in seconds
+    uint256 public constant MIN_DIFFERENCE = 100;
+
+    function assertTimestampInRange(uint256 start, uint256 end) public view {
+        require(end >= start + MIN_DIFFERENCE, "Time window end must be at least 100 seconds after the time window start");
+        require(end >= block.timestamp + MIN_INTERVAL, "Time window end must be at least 60 seconds after the current block time");
+    }
+}
