5682148076f4afbb84ef2e6175ca590c1f2e7295
Vladimir Radosavljevic <vr@matterlabs.dev>
2024-10-10 16:13:32 +0200

    feat(EVM-emulator): add support for propagating LLVM options
    Signed-off-by: Vladimir Radosavljevic <vr@matterlabs.dev>

--- system-contracts/scripts/compile-yul.ts
+++ system-contracts/scripts/compile-yul.ts
@@ -1,4 +1,5 @@
 // hardhat import should be the first import in the file
+import { existsSync } from "fs";
 import type { CompilerPaths } from "./utils";
 import {
   spawn,
@@ -18,13 +19,26 @@ const CONTRACTS_DIR = "contracts-preprocessed";
 const BOOTLOADER_DIR = "bootloader";
 const TIMESTAMP_FILE_YUL = "last_compilation_yul.timestamp";
 const TIMESTAMP_FILE_BOOTLOADER = "last_compilation_bootloader.timestamp";
+const LLVM_OPTIONS_FILE_EXTENSION = ".llvm.options";
 
 export async function compileYul(paths: CompilerPaths, file: string) {
   const solcCompilerPath = await getSolcLocation();
 
   const zksolcLocation = await compilerLocation(COMPILER_VERSION, IS_COMPILER_PRE_RELEASE);
+
+  const filePath = `${paths.absolutePathSources}/${file}`;
+  const llvmOptionsFilePath = `${filePath}${LLVM_OPTIONS_FILE_EXTENSION}`;
+  let llvmOptions = "";
+  if (existsSync(llvmOptionsFilePath)) {
+    const llvmOptionsFileContent = (await fs.promises.readFile(llvmOptionsFilePath)).toString();
+    if (!llvmOptionsFileContent.startsWith("'") || !llvmOptionsFileContent.endsWith("'")) {
+      throw new Error(`Content in ${llvmOptionsFilePath} must start and end with a single quote.`);
+    }
+    llvmOptions = `--llvm-options=${llvmOptionsFileContent}`;
+  }
+
   await spawn(
-    `${zksolcLocation} ${paths.absolutePathSources}/${file} --solc ${solcCompilerPath} --optimization 3 --system-mode --yul --bin --overwrite -o ${paths.absolutePathArtifacts}`
+    `${zksolcLocation} ${filePath} --optimization 3 ${llvmOptions} --system-mode --yul --bin --overwrite -o ${paths.absolutePathArtifacts}`
   );
 }
 
