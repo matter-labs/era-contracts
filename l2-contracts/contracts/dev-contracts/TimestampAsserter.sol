f7df526e183cb642d92f5eb9b59aed9d09e49d45
Ivan Schasny <ivan.schasny@gmail.com>
2024-10-10 11:26:15 +0100

    Moved constants to the server level

--- l2-contracts/contracts/dev-contracts/TimestampAsserter.sol
+++ l2-contracts/contracts/dev-contracts/TimestampAsserter.sol
@@ -6,19 +6,10 @@ interface ITimestampAsserter {
 }
 
 contract TimestampAsserter is ITimestampAsserter {
-    // Minimum interval that the end of the time window should be ahead of the block.timestamp in seconds
-    uint256 public constant MIN_INTERVAL = 60;
-    // Minimum difference between time window start and end in seconds
-    uint256 public constant MIN_DIFFERENCE = 100;
-
     function assertTimestampInRange(uint256 start, uint256 end) public view {
         require(
-            end >= start + MIN_DIFFERENCE,
-            "Time window end must be at least 100 seconds after the time window start"
-        );
-        require(
-            end >= block.timestamp + MIN_INTERVAL,
-            "Time window end must be at least 60 seconds after the current block time"
+            start <= block.timestamp && block.timestamp <= end,
+            "Block timestamp is outside of the specified range"
         );
     }
 }
