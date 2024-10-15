e9202db584a0111e918e125443581bb51a3a0035
Vladislav Volosnikov <Volosnikov.apmath@gmail.com>
2024-10-15 17:22:57 +0300

    Update zksolc

--- system-contracts/hardhat.config.ts
+++ system-contracts/hardhat.config.ts
@@ -6,8 +6,8 @@ import "@nomiclabs/hardhat-ethers";
 import "hardhat-typechain";
 
 // This version of system contracts requires a pre release of the compiler
-const COMPILER_VERSION = "fix-output-directory"; // 1.5.5
-const PRE_RELEASE_VERSION = "72ea6d2";
+const COMPILER_VERSION = "v1.5.5";
+const PRE_RELEASE_VERSION = "1.5.5";
 function getZksolcUrl(): string {
   // @ts-ignore
   const platform = { darwin: "macosx", linux: "linux", win32: "windows" }[process.platform];
