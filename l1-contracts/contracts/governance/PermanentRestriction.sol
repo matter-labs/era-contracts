0c7b47ff8073e55db50ad2c4792df521168b665b
Stanislav Breadless <stanislavbezkor@gmail.com>
2024-10-15 13:43:16 +0200

    fix typos

--- l1-contracts/contracts/governance/PermanentRestriction.sol
+++ l1-contracts/contracts/governance/PermanentRestriction.sol
@@ -58,7 +58,7 @@ contract PermanentRestriction is Restriction, IPermanentRestriction, Ownable2Ste
         L2_ADMIN_FACTORY = _l2AdminFactory;
     }
 
-    /// @notice The intialization function for the proxy contract.
+    /// @notice The initialization function for the proxy contract.
     /// @param _initialOwner The initial owner of the permanent restriction.
     /// @dev Expected to be delegatecalled by the `TransparentUpgradableProxy`
     /// upon initialization.
