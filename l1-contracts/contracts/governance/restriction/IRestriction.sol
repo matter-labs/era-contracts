64cceff4556da3d41377102328c4c54f889cf80b
Stanislav Breadless <stanislavbezkor@gmail.com>
2024-10-23 16:59:09 +0200

    fix suggestion

--- l1-contracts/contracts/governance/restriction/IRestriction.sol
+++ l1-contracts/contracts/governance/restriction/IRestriction.sol
@@ -18,5 +18,5 @@ interface IRestriction {
     /// @notice Ensures that the invoker has the required role to call the function.
     /// @param _call The call data.
     /// @param _invoker The address of the invoker.
-    function validateCall(Call calldata _call, address _invoker) external virtual view;
+    function validateCall(Call calldata _call, address _invoker) external view;
 }
