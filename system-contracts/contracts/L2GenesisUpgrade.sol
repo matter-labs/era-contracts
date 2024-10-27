416c2aa11da4ad8cb32ec3166231f24fa4b9962e
Raid Ateir <ra@matterlabs.dev>
2024-10-27 16:52:10 +0400

    (fix): imports

--- system-contracts/contracts/L2GenesisUpgrade.sol
+++ system-contracts/contracts/L2GenesisUpgrade.sol
@@ -2,9 +2,7 @@
 
 pragma solidity 0.8.24;
 
-import {DEPLOYER_SYSTEM_CONTRACT, SYSTEM_CONTEXT_CONTRACT, L2_BRIDGE_HUB, L2_ASSET_ROUTER, L2_MESSAGE_ROOT} from "./Constants.sol";
-import {IContractDeployer, ForceDeployment} from "./interfaces/IContractDeployer.sol";
-import {SystemContractHelper} from "./libraries/SystemContractHelper.sol";
+import {SYSTEM_CONTEXT_CONTRACT} from "./Constants.sol";
 import {ISystemContext} from "./interfaces/ISystemContext.sol";
 import {InvalidChainId} from "contracts/SystemContractErrors.sol";
 import {IL2GenesisUpgrade} from "./interfaces/IL2GenesisUpgrade.sol";
