36a208221f356afdc188d031f093d858a7b615e4
vladbochok <vladbochok1@gmail.com>
2024-10-29 22:25:51 +0300

    Fix tests

--- l2-contracts/hardhat.config.ts
+++ l2-contracts/hardhat.config.ts
@@ -1,6 +1,7 @@
 import "@matterlabs/hardhat-zksync-solc";
 import "@matterlabs/hardhat-zksync-verify";
 import "@nomicfoundation/hardhat-chai-matchers";
+import "@matterlabs/hardhat-zksync-node";
 import "@nomiclabs/hardhat-ethers";
 import "hardhat-typechain";
 
