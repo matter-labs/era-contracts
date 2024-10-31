6614935eccd66b8bb8e96dd62c95ef91225c8320
kelemeno <kl@matterlabs.dev>
2024-10-31 21:44:41 +0000

    Fix contracts preprocessing

--- system-contracts/scripts/preprocess-system-contracts.ts
+++ system-contracts/scripts/preprocess-system-contracts.ts
@@ -1,3 +1,4 @@
+import * as fs from "fs";
 import { copyFileSync, existsSync, mkdirSync, writeFileSync } from "fs";
 import path from "path";
 import { renderFile } from "template-file";
@@ -19,6 +20,8 @@ async function preprocess(testMode: boolean) {
     console.log("\x1b[31mWarning: test mode for the preprocessing being used!\x1b[0m");
     params.SYSTEM_CONTRACTS_OFFSET = "0x9000";
   }
+  const substring = "uint160 constant SYSTEM_CONTRACTS_OFFSET = 0x8000;"
+  const replacingSubstring = `uint160 constant SYSTEM_CONTRACTS_OFFSET = ${params.SYSTEM_CONTRACTS_OFFSET};`
 
   const timestampFilePath = path.join(process.cwd(), TIMESTAMP_FILE);
   const folderToCheck = path.join(process.cwd(), CONTRACTS_DIR);
@@ -37,9 +40,10 @@ async function preprocess(testMode: boolean) {
     { onlyFiles: true }
   );
 
-  for (const contract of contracts) {
-    const preprocessed = await renderFile(contract, params);
-    const fileName = `${OUTPUT_DIR}/${contract.slice(CONTRACTS_DIR.length)}`;
+  for (const contractPath of contracts) {
+    let contract = fs.readFileSync(contractPath,'utf8');
+    const preprocessed = await contract.replace(substring, replacingSubstring);
+    const fileName = `${OUTPUT_DIR}/${contractPath.slice(CONTRACTS_DIR.length)}`;
     const directory = path.dirname(fileName);
     if (!existsSync(directory)) {
       mkdirSync(directory, { recursive: true });
