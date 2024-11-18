7b623b5e5ea993443945d9356614094de3e9e6f9
Vladislav Volosnikov <Volosnikov.apmath@gmail.com>
2024-11-18 20:59:35 +0100

    Fix typo

--- l1-contracts/src.ts/deploy.ts
+++ l1-contracts/src.ts/deploy.ts
@@ -213,7 +213,7 @@ export class Deployer {
   public async deployChainAdmin(create2Salt: string, ethTxOptions: ethers.providers.TransactionRequest) {
     ethTxOptions.gasLimit ??= 10_000_000;
 
-    // Wedeploy the ChainAdmin contract itself
+    // We deploy the ChainAdmin contract itself
     const contractAddress = await this.deployViaCreate2(
       "ChainAdmin",
       [this.ownerAddress, ethers.constants.AddressZero],
