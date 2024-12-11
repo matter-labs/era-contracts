e4c49c3bc4efd4be6261a535ce1e75dce32e6f7e
Vladislav Volosnikov <Volosnikov.apmath@gmail.com>
2024-12-11 18:25:31 +0100

    Fix typo

--- l1-contracts/deploy-scripts/EnableEvmEmulator.s.sol
+++ l1-contracts/deploy-scripts/EnableEvmEmulator.s.sol
@@ -6,7 +6,7 @@ import {Script} from "forge-std/Script.sol";
 import {IZkSyncHyperchain} from "contracts/state-transition/chain-interfaces/IZkSyncHyperchain.sol";
 import {IChainAdmin} from "contracts/governance/IChainAdmin.sol";
 
-contract AcceptAdmin is Script {
+contract EnableEvmEmulator is Script {
     // This function should be called by the owner to update token multiplier setter role
     function chainAllowEvmEmulation(address chainAdmin, address target) public {
         IChainAdmin admin = IChainAdmin(chainAdmin);
