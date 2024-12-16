dd63e632e6493b16d79712f44e71945649b3dd9a
Raid Ateir <ra@matterlabs.dev>
2024-12-16 22:55:53 +0400

    fix: lint

--- l1-contracts/contracts/common/L1ContractErrors.sol
+++ l1-contracts/contracts/common/L1ContractErrors.sol
@@ -277,7 +277,7 @@ error Unauthorized(address caller);
 error UndefinedDiamondCutAction();
 // 0x6aa39880
 error UnexpectedSystemLog(uint256 logKey);
-//
+// 0xa4dde386
 error UnimplementedMessage(string);
 // 0xf093c2e5
 error UpgradeBatchNumberIsNotZero();
