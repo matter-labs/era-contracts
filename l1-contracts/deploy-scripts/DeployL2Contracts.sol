e7d601193f11b01d51a969376b503ef23ff88fa8
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2024-12-30 12:07:03 -0300

    Add eigenda to DAValidatorType

--- l1-contracts/deploy-scripts/DeployL2Contracts.sol
+++ l1-contracts/deploy-scripts/DeployL2Contracts.sol
@@ -24,7 +24,8 @@ contract DeployL2Script is Script {
     enum DAValidatorType {
         Rollup,
         NoDA,
-        Avail
+        Avail,
+        EigenDA
     }
 
     // solhint-disable-next-line gas-struct-packing
@@ -151,7 +152,7 @@ contract DeployL2Script is Script {
         config.eraChainId = toml.readUint("$.era_chain_id");
 
         uint256 validatorTypeUint = toml.readUint("$.da_validator_type");
-        require(validatorTypeUint < 3, "Invalid DA validator type");
+        require(validatorTypeUint < 4, "Invalid DA validator type");
         config.validatorType = DAValidatorType(validatorTypeUint);
     }
 
@@ -176,6 +177,8 @@ contract DeployL2Script is Script {
             bytecode = L2ContractsBytecodesLib.readNoDAL2DAValidatorBytecode();
         } else if (config.validatorType == DAValidatorType.Avail) {
             bytecode = L2ContractsBytecodesLib.readAvailL2DAValidatorBytecode();
+        } else if (config.validatorType == DAValidatorType.EigenDA) {
+            bytecode = L2ContractsBytecodesLib.readNoDAL2DAValidatorBytecode(); // TODO: update to EigenDA
         } else {
             revert("Invalid DA validator type");
         }
