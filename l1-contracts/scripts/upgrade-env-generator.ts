4498e1412cbae47f19a5e1bd061d07373183f7dd
Stanislav Breadless <stanislavbezkor@gmail.com>
2025-01-02 16:42:40 +0100

    upd data for devnet

--- l1-contracts/scripts/upgrade-env-generator.ts
+++ l1-contracts/scripts/upgrade-env-generator.ts
@@ -5,7 +5,8 @@
 import * as hardhat from "hardhat";
 
 import { Command } from "commander";
-import { BigNumber, ethers, Wallet } from "ethers";
+import {Provider} from "zksync-ethers";
+import {ethers} from "ethers";
 
 async function main() {
   const program = new Command();
@@ -13,10 +14,17 @@ async function main() {
   program.version("0.1.0").name("upgrade-new-generator");
 
   program
-    .option("--bridgehub-address <bridgehub-address>")
-    .option("--era-chain-id <era-chain-id>")
-    .option("--testnet-verifier <testnet-verifier>")
+    .option("--l2-rpc-url <web3-rpc-url>")
+    .option("--l1-rpc-url <web3-rpc-url>")
     .action(async (cmd) => {
+      const l1Provider = new ethers.providers.JsonRpcProvider(cmd.l1RpcUrl);
+      const l2Provider = new Provider(cmd.l2RpcUrl);
+
+      const bridgehubAddr = await l2Provider.getBridgehubContractAddress();
+      const proxyAdmin = await l1Provider.getStorageAt(bridgehubAddr, '0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103');
+
+      console.log(`bridgehub_proxy_address = "${bridgehubAddr}"`);
+      console.log(`transparent_proxy_admin = "0x${proxyAdmin.substring(26)}"`);
 
     });
 
