626fd7110de28843ad421169948af88c2544ffc9
Stanislav Breadless <stanislavbezkor@gmail.com>
2025-01-22 13:34:05 +0100

    basic migrator

---
+++ l1-contracts/contracts/upgrades/Migrator.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.24;
+
+
+import {ZkSyncHyperchainBase} from "../state-transition/chain-deps/facets/ZkSyncHyperchainBase.sol";
+import {StateTransitionManager} from "../state-transition/StateTransitionManager.sol";
+import {Bridgehub} from "../bridgehub/Bridgehub.sol";
+import {IVerifier} from "../state-transition/chain-interfaces/IVerifier.sol";
+
+struct MigrationParams {
+    address newCTM;
+    address newBridgehub;
+    address newVerifier;
+}
+
+/// @author Matter Labs
+/// @custom:security-contact security@matterlabs.dev
+contract Migrator is ZkSyncHyperchainBase {
+    event BlockStateInfo(uint256 committed, uint256 proved, uint256 executed);
+
+    /// @notice The main function that will be called by the upgrade proxy.
+    /// @param params The migration params
+    function upgrade(MigrationParams memory params) public returns (bytes32) {
+        address currentCTM = StateTransitionManager(s.stateTransitionManager).validatorTimelock();
+        address newTimelock = StateTransitionManager(params.newCTM).validatorTimelock();
+
+        s.validators[currentCTM] = false;
+        s.validators[newTimelock] = true;
+
+        s.verifier = IVerifier(params.newVerifier);
+
+        emit BlockStateInfo(s.totalBatchesCommitted, s.totalBatchesVerified, s.totalBatchesExecuted);
+    
+        s.bridgehub = params.newBridgehub;
+        s.stateTransitionManager = params.newCTM;
+        s.baseTokenBridge = address(Bridgehub(params.newBridgehub).sharedBridge());
+    }
+}
