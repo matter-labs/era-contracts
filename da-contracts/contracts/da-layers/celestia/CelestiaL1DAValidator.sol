9221530d660b54e9c8293b3d4d9db85aa221dd2e
c-node <chuckhead7@gmail.com>
2025-01-27 18:18:18 -0500

    reinstall sp1

--- da-contracts/contracts/da-layers/celestia/CelestiaL1DAValidator.sol
+++ da-contracts/contracts/da-layers/celestia/CelestiaL1DAValidator.sol
@@ -1,8 +1,20 @@
 pragma solidity 0.8.24;
 
 import {IL1DAValidator, L1DAValidatorOutput} from "../../IL1DAValidator.sol";
+import {ISP1Verifier} from "../../../../lib/sp1-contracts/contracts/src/ISP1Verifier.sol";
+
+struct KeccakInclusionToDataRootOutput {
+    bytes32 keccakHash;
+    bytes32 dataRoot;
+}
 
 contract CelestiaL1DAValidator is IL1DAValidator {
+
+    ISP1Verifier public sp1Verifier;
+
+    constructor(ISP1Verifier _sp1Verifier) {
+        sp1Verifier = _sp1Verifier;
+    }
 
     function checkDA(
         uint256 _chainId,
