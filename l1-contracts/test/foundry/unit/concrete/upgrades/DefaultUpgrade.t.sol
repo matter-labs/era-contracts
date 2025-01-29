42ebfe0e5b7123fdb710ef76d74c1546db1a47f0
Rahul Saxena <rsa@matterlabs.dev>
2025-01-29 18:29:23 +0530

    resolved issue of missing import files

--- l1-contracts/test/foundry/unit/concrete/upgrades/DefaultUpgrade.t.sol
+++ l1-contracts/test/foundry/unit/concrete/upgrades/DefaultUpgrade.t.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.24;
 
 import {Test} from "forge-std/Test.sol";
 import {DefaultUpgrade} from "contracts/upgrades/DefaultUpgrade.sol";
-import {PubdataPricingMode, FeeParams} from "contracts/state-transition/chain-deps/ZkSyncHyperchainStorage.sol";
+import {PubdataPricingMode, FeeParams} from "contracts/state-transition/chain-deps/ZKChainStorage.sol";
 import {Diamond} from "contracts/state-transition/libraries/Diamond.sol";
 
 import {BaseUpgrade} from "./_SharedBaseUpgrade.t.sol";
