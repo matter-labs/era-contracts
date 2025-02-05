e0520ea11ebf1d1d2205abd1ffed6f8dc75487f9
Nikita Stupin <178824211+nikitastupin-matterlabs@users.noreply.github.com>
2025-02-05 14:14:06 +0100

    add invariant test

---
+++ l1-contracts/test/foundry/invariant/AssetRouterTest.t.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+
+import {DeployUtils} from "deploy-scripts/DeployUtils.s.sol";
+
+import {L1AssetRouterActorHandler} from "./handlers/L1AssetRouterActorHandler.sol";
+import {AssetRouterProperties} from "./AssetRouterProperties.sol";
+import {SharedL2ContractL1DeployerUtils} from "../l1/integration/l2-tests-in-l1-context/_SharedL2ContractL1DeployerUtils.sol";
+import {SharedL2ContractDeployer} from "../l1/integration/l2-tests-in-l1-context/_SharedL2ContractDeployer.sol";
+import {SystemContractsArgs} from "../l1/integration/l2-tests-in-l1-context/_SharedL2ContractL1DeployerUtils.sol";
+
+contract AssetRouterTest is Test, SharedL2ContractL1DeployerUtils, SharedL2ContractDeployer, AssetRouterProperties {
+    function test() internal virtual override(DeployUtils, SharedL2ContractL1DeployerUtils) {}
+
+    function initSystemContracts(
+        SystemContractsArgs memory _args
+    ) internal virtual override(SharedL2ContractDeployer, SharedL2ContractL1DeployerUtils) {
+        super.initSystemContracts(_args);
+    }
+
+    function deployL2Contracts(
+        uint256 _l1ChainId
+    ) public virtual override(SharedL2ContractDeployer, SharedL2ContractL1DeployerUtils) {
+        super.deployL2Contracts(_l1ChainId);
+
+        h = new L1AssetRouterActorHandler(l1AssetRouter);
+        address ts = makeAddr("targetSender");
+        deal(ts, 10_000 ether);
+        targetContract(address(h));
+        targetSender(ts);
+    }
+}
---
+++ l1-contracts/test/foundry/invariant/handlers/L1AssetRouterActorHandler.sol
@@ -0,0 +1,75 @@
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+
+import {L2_ASSET_ROUTER_ADDR} from "contracts/common/L2ContractAddresses.sol";
+import {AddressAliasHelper} from "contracts/vendor/AddressAliasHelper.sol";
+import {L2AssetRouter} from "contracts/bridge/asset-router/L2AssetRouter.sol";
+
+contract L1AssetRouterActorHandler is Test {
+    address l1Token;
+    address l1Router;
+    address vault;
+    // ghost variables
+    // https://book.getfoundry.sh/forge/invariant-testing#handler-ghost-variables
+    uint256 public totalDeposits;
+    // constants
+    // borrowed from https://github.com/matter-labs/era-contracts/blob/16dedf6d77695ce00f81fce35a3066381b97fca1/l1-contracts/test/foundry/l1/integration/l2-tests-in-l1-context/_SharedL2ContractDeployer.sol#L64-L68
+    address internal constant L1_TOKEN_ADDRESS = 0x1111100000000000000000000000000000011111;
+    string internal constant TOKEN_DEFAULT_NAME = "TestnetERC20Token";
+    string internal constant TOKEN_DEFAULT_SYMBOL = "TET";
+    uint8 internal constant TOKEN_DEFAULT_DECIMALS = 18;
+    address internal l1AssetRouter;
+    address internal aliasedL1AssetRouter;
+
+    constructor(address _l1AssetRouter) {
+        l1AssetRouter = _l1AssetRouter;
+        aliasedL1AssetRouter = AddressAliasHelper.applyL1ToL2Alias(l1AssetRouter);
+        l1Token = L1_TOKEN_ADDRESS;
+    }
+
+    function finalizeDeposit(uint256 _amount, address _sender, uint256 _receiverUint) public {
+        _amount = bound(_amount, 0, 1e30);
+        address _receiver = address(uint160(bound(_receiverUint, 1, type(uint160).max)));
+
+        // vm.prank(aliasedL1AssetRouter);
+        // address _s = makeAddr("_s");
+        // address _r = makeAddr("_r");
+
+        L2AssetRouter(L2_ASSET_ROUTER_ADDR).finalizeDeposit({
+            _l1Sender: _sender,
+            _l2Receiver: _receiver,
+            _l1Token: l1Token,
+            _amount: _amount,
+            _data: encodeTokenData(TOKEN_DEFAULT_NAME, TOKEN_DEFAULT_SYMBOL, TOKEN_DEFAULT_DECIMALS)
+        });
+
+        totalDeposits += _amount;
+    }
+
+    function withdraw(uint256 _amount, address _receiver) public {
+        _amount = bound(_amount, 0, 1e30);
+
+        address l2Token = L2AssetRouter(L2_ASSET_ROUTER_ADDR).l2TokenAddress(l1Token);
+        L2AssetRouter(L2_ASSET_ROUTER_ADDR).withdraw(_receiver, l2Token, _amount);
+
+        totalDeposits -= _amount;
+    }
+
+    // borrowed from https://github.com/matter-labs/era-contracts/blob/16dedf6d77695ce00f81fce35a3066381b97fca1/l1-contracts/test/foundry/l1/integration/l2-tests-in-l1-context/_SharedL2ContractDeployer.sol#L203-L217
+    /// @notice Encodes the token data.
+    /// @param name The name of the token.
+    /// @param symbol The symbol of the token.
+    /// @param decimals The decimals of the token.
+    function encodeTokenData(
+        string memory name,
+        string memory symbol,
+        uint8 decimals
+    ) internal pure returns (bytes memory) {
+        bytes memory encodedName = abi.encode(name);
+        bytes memory encodedSymbol = abi.encode(symbol);
+        bytes memory encodedDecimals = abi.encode(decimals);
+
+        return abi.encode(encodedName, encodedSymbol, encodedDecimals);
+    }
+}
