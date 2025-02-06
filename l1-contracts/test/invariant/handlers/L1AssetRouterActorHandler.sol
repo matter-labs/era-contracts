6d403196816388beda5941430dbe7de2c2c67bdd
Nikita Stupin <178824211+nikitastupin-matterlabs@users.noreply.github.com>
2025-02-06 17:17:36 +0100

    cleanup

--- l1-contracts/test/invariant/handlers/L1AssetRouterActorHandler.sol
+++ l1-contracts/test/invariant/handlers/L1AssetRouterActorHandler.sol
@@ -8,10 +8,9 @@ import {AddressAliasHelper} from "contracts/vendor/AddressAliasHelper.sol";
 import {L2AssetRouter} from "contracts/bridge/asset-router/L2AssetRouter.sol";
 import {BridgedStandardERC20} from "contracts/bridge/BridgedStandardERC20.sol";
 
+// no cheatcodes here because they won't work with `--zksync`
+// forge 0.0.2 (27360d4 2024-12-02T00:28:35.872943000Z)
 contract L1AssetRouterActorHandler is Test {
-    address l1Token;
-    address l1Router;
-    address vault;
     // ghost variables
     // https://book.getfoundry.sh/forge/invariant-testing#handler-ghost-variables
     uint256 public totalDeposits;
@@ -21,27 +20,15 @@ contract L1AssetRouterActorHandler is Test {
     string internal constant TOKEN_DEFAULT_NAME = "TestnetERC20Token";
     string internal constant TOKEN_DEFAULT_SYMBOL = "TET";
     uint8 internal constant TOKEN_DEFAULT_DECIMALS = 18;
-    address internal l1AssetRouter;
-    address internal aliasedL1AssetRouter;
-
-    constructor(address _l1AssetRouter) {
-        l1AssetRouter = _l1AssetRouter;
-        aliasedL1AssetRouter = AddressAliasHelper.applyL1ToL2Alias(l1AssetRouter);
-        l1Token = L1_TOKEN_ADDRESS;
-    }
 
     function finalizeDeposit(uint256 _amount, address _sender, uint256 _receiverUint) public {
         _amount = bound(_amount, 0, 1e30);
         address _receiver = address(uint160(bound(_receiverUint, 1, type(uint160).max)));
 
-        // vm.prank(aliasedL1AssetRouter);
-        // address _s = makeAddr("_s");
-        // address _r = makeAddr("_r");
-
         L2AssetRouter(L2_ASSET_ROUTER_ADDR).finalizeDeposit({
             _l1Sender: _sender,
             _l2Receiver: _receiver,
-            _l1Token: l1Token,
+            _l1Token: L1_TOKEN_ADDRESS,
             _amount: _amount,
             _data: encodeTokenData(TOKEN_DEFAULT_NAME, TOKEN_DEFAULT_SYMBOL, TOKEN_DEFAULT_DECIMALS)
         });
@@ -50,14 +37,14 @@ contract L1AssetRouterActorHandler is Test {
     }
 
     function withdraw(uint256 _amount, address _receiver) public {
-        address l2Token = L2AssetRouter(L2_ASSET_ROUTER_ADDR).l2TokenAddress(l1Token);
+        address l2Token = L2AssetRouter(L2_ASSET_ROUTER_ADDR).l2TokenAddress(L1_TOKEN_ADDRESS);
 
         // this limits the variety of scenarios but otherwise there're many reverts 
         // when it tries to withdraw more than it has
         _amount = bound(_amount, 0, BridgedStandardERC20(l2Token).balanceOf(address(this)));
 
         uint256 l1ChainId = L2AssetRouter(L2_ASSET_ROUTER_ADDR).L1_CHAIN_ID();
-        bytes32 assetId = DataEncoding.encodeNTVAssetId(l1ChainId, l1Token);
+        bytes32 assetId = DataEncoding.encodeNTVAssetId(l1ChainId, L1_TOKEN_ADDRESS);
         bytes memory data = DataEncoding.encodeBridgeBurnData(_amount, _receiver, l2Token);
 
         if (_amount == 0) {
