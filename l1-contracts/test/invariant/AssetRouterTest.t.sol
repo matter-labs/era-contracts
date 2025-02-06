0a81a63a3be5b9422bf659e6d43b49694bf70c59
Nikita Stupin <178824211+nikitastupin-matterlabs@users.noreply.github.com>
2025-02-06 16:59:08 +0100

    remove monkey patch

--- l1-contracts/test/invariant/AssetRouterTest.t.sol
+++ l1-contracts/test/invariant/AssetRouterTest.t.sol
@@ -31,5 +31,7 @@ contract AssetRouterTest is Test, SharedL2ContractL1DeployerUtils, SharedL2Contr
         deal(ts, 10_000 ether);
         targetContract(address(h));
         targetSender(ts);
+        // deposit a unit of token to deploy the L2 token
+        h.finalizeDeposit(1, address(1), 1);
     }
 }
