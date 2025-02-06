2e92d1b6e6f60f182a4cb806c9175e11aac9d01f
Nikita Stupin <178824211+nikitastupin-matterlabs@users.noreply.github.com>
2025-02-06 16:05:29 +0100

    move `invariant` from `test/foundry` to `test`

--- l1-contracts/test/invariant/AssetRouterTest.t.sol
+++ l1-contracts/test/invariant/AssetRouterTest.t.sol
@@ -8,9 +8,9 @@ import {DeployUtils} from "deploy-scripts/DeployUtils.s.sol";
 
 import {L1AssetRouterActorHandler} from "./handlers/L1AssetRouterActorHandler.sol";
 import {AssetRouterProperties} from "./AssetRouterProperties.sol";
-import {SharedL2ContractL1DeployerUtils} from "../l1/integration/l2-tests-in-l1-context/_SharedL2ContractL1DeployerUtils.sol";
-import {SharedL2ContractDeployer} from "../l1/integration/l2-tests-in-l1-context/_SharedL2ContractDeployer.sol";
-import {SystemContractsArgs} from "../l1/integration/l2-tests-in-l1-context/_SharedL2ContractL1DeployerUtils.sol";
+import {SharedL2ContractL1DeployerUtils} from "../foundry/l1/integration/l2-tests-in-l1-context/_SharedL2ContractL1DeployerUtils.sol";
+import {SharedL2ContractDeployer} from "../foundry/l1/integration/l2-tests-in-l1-context/_SharedL2ContractDeployer.sol";
+import {SystemContractsArgs} from "../foundry/l1/integration/l2-tests-in-l1-context/_SharedL2ContractL1DeployerUtils.sol";
 
 contract AssetRouterTest is Test, SharedL2ContractL1DeployerUtils, SharedL2ContractDeployer, AssetRouterProperties {
     function test() internal virtual override(DeployUtils, SharedL2ContractL1DeployerUtils) {}
