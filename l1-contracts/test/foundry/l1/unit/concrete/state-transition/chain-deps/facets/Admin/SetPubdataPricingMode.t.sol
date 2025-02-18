a5a3a6aea28f2b60273bdef179ee2b2b01ed46d6
Rahul Saxena <rsa@matterlabs.dev>
2025-02-18 10:42:55 +0530

    fixed import issues in SetPubdataPricingMode.sol

--- l1-contracts/test/foundry/l1/unit/concrete/state-transition/chain-deps/facets/Admin/SetPubdataPricingMode.t.sol
+++ l1-contracts/test/foundry/l1/unit/concrete/state-transition/chain-deps/facets/Admin/SetPubdataPricingMode.t.sol
@@ -3,43 +3,31 @@ pragma solidity 0.8.24;
 
 import {AdminTest} from "./_Admin_Shared.t.sol";
 import {Unauthorized, ChainAlreadyLive} from "contracts/common/L1ContractErrors.sol";
-import {PubdataPricingMode} from "contracts/state-transition/chain-deps/ZkChainStorage.sol";
+import {PubdataPricingMode} from "contracts/state-transition/chain-deps/ZKChainStorage.sol";
 
 contract SetPubdataPricingModeTest is AdminTest {
-    event ValidiumModeStatusUpdate(PubdataPricingMode _pricingMode);
+    event PubdataPricingModeUpdate(PubdataPricingMode _pricingMode);
 
     function test_revertWhen_calledByNonAdmin() public {
-        assertTrue(true);
-        // address nonAdmin = makeAddr("nonAdmin");
+        address nonAdmin = makeAddr("nonAdmin");
 
-        // vm.startPrank(nonAdmin);
+        vm.startPrank(nonAdmin);
 
-        // vm.expectRevert(abi.encodeWithSelector(Unauthorized.selector, nonAdmin));
-        // adminFacet.setPubdataPricingMode(PubdataPricingMode.Validium);
+        vm.expectRevert(abi.encodeWithSelector(Unauthorized.selector, nonAdmin));
+        adminFacet.setPubdataPricingMode(PubdataPricingMode.Validium);
 
-        // vm.stopPrank();
+        vm.stopPrank();
     }
 
-    // function test_revertWhen_totalBatchesCommittedGreaterThenZero() public {
-    //     address admin = utilsFacet.util_getAdmin();
+    function test_SuccessfulSet() public {
+        address admin = utilsFacet.util_getAdmin();
 
-    //     utilsFacet.util_setTotalBatchesCommitted(1);
+        vm.expectEmit(true, true, true, true, address(adminFacet));
+        emit PubdataPricingModeUpdate(PubdataPricingMode.Validium);
 
-    //     vm.startPrank(admin);
+        vm.startPrank(admin);
+        adminFacet.setPubdataPricingMode(PubdataPricingMode.Validium);
 
-    //     vm.expectRevert(ChainAlreadyLive.selector);
-    //     adminFacet.setPubdataPricingMode(PubdataPricingMode.Validium);
-    // }
-
-    // function test_SuccessfulSet() public {
-    //     address admin = utilsFacet.util_getAdmin();
-
-    //     vm.expectEmit(true, true, true, true, address(adminFacet));
-    //     emit ValidiumModeStatusUpdate(PubdataPricingMode.Validium);
-
-    //     vm.startPrank(admin);
-    //     adminFacet.setPubdataPricingMode(PubdataPricingMode.Validium);
-
-    //     assert(utilsFacet.util_getFeeParams().pubdataPricingMode == PubdataPricingMode.Validium);
-    // }
+        assert(utilsFacet.util_getFeeParams().pubdataPricingMode == PubdataPricingMode.Validium);
+    }
 }
