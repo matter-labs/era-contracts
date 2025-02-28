8286ad41060abfa5b00d031afee5bede4ef7baca
Nikita Stupin <178824211+nikitastupin-matterlabs@users.noreply.github.com>
2025-02-28 15:22:32 +0100

    add `registerTokenWithVault` and `registerTokenWithVaultV2` and refactor

--- l1-contracts/test/invariant/handlers/UserActorHandler.sol
+++ l1-contracts/test/invariant/handlers/UserActorHandler.sol
@@ -14,6 +14,10 @@ import {L2NativeTokenVault} from "contracts/bridge/ntv/L2NativeTokenVault.sol";
 contract UserActorHandler is Test {
     address[] public l1Tokens;
 
+    IL2SharedBridgeLegacy l2SharedBridge;
+    L2NativeTokenVault l2NativeTokenVault;
+    L2AssetRouter l2AssetRouter;
+
     uint256 public ghost_totalWithdrawalAmount;
     uint256 public ghost_totalFunctionCalls;
 
@@ -24,12 +28,15 @@ contract UserActorHandler is Test {
             revert ArrayIsEmpty();
         }
         l1Tokens = _l1Tokens;
+
+        l2NativeTokenVault = L2NativeTokenVault(L2_NATIVE_TOKEN_VAULT_ADDR);
+        l2AssetRouter = L2AssetRouter(L2_ASSET_ROUTER_ADDR);
+        l2SharedBridge = IL2SharedBridgeLegacy(l2AssetRouter.L2_LEGACY_SHARED_BRIDGE());
     }
 
-    function withdraw(uint256 _amount, address _receiver, uint256 _l1TokenIndex) public {
+    function withdraw(uint256 _amount, address _receiver, uint256 _l1TokenIndex) external {
         uint256 l1TokenIndex = bound(_l1TokenIndex, 0, l1Tokens.length - 1);
 
-        L2AssetRouter l2AssetRouter = L2AssetRouter(L2_ASSET_ROUTER_ADDR);
         address l1Token = l1Tokens[l1TokenIndex];
         address l2Token = l2AssetRouter.l2TokenAddress(l1Token);
 
@@ -48,7 +55,7 @@ contract UserActorHandler is Test {
         // too many reverts (around 50%) without this condition
         vm.assume(amount != 0);
         // using `L2NativeTokenVault` instead of `IL2NativeTokenVault` because the latter doesn't have `L2_LEGACY_SHARED_BRIDGE`
-        vm.assume(L2NativeTokenVault(L2_NATIVE_TOKEN_VAULT_ADDR).L2_LEGACY_SHARED_BRIDGE().l1TokenAddress(l2Token) != address(0));
+        vm.assume(l2NativeTokenVault.L2_LEGACY_SHARED_BRIDGE().l1TokenAddress(l2Token) != address(0));
 
 
         uint256 l1ChainId = l2AssetRouter.L1_CHAIN_ID();
@@ -60,4 +67,38 @@ contract UserActorHandler is Test {
         ghost_totalWithdrawalAmount += amount;
         ghost_totalFunctionCalls++;
     }
+
+    function registerTokenWithVault(uint256 _l1TokenIndex) external {
+        uint256 l1TokenIndex = bound(_l1TokenIndex, 0, l1Tokens.length - 1);
+
+        address l1Token = l1Tokens[l1TokenIndex];
+        address l2Token = l2AssetRouter.l2TokenAddress(l1Token);
+
+        vm.assume(l2SharedBridge.l1TokenAddress(l2Token) != address(0));
+        vm.assume(l2AssetRouter.l1TokenAddress(l2Token) == address(0));
+
+        l2NativeTokenVault.setLegacyTokenAssetId(l2Token);
+    }
+
+    function registerTokenWithVaultV2(uint256 _l1TokenIndex) external {
+        uint256 l1TokenIndex = bound(_l1TokenIndex, 0, l1Tokens.length - 1);
+
+        address l1Token = l1Tokens[l1TokenIndex];
+        address l2Token = l2AssetRouter.l2TokenAddress(l1Token);
+
+        vm.assume(l2Token.code.length != 0);
+        vm.assume(l2SharedBridge.l1TokenAddress(l2Token) == address(0));
+        vm.assume(l2AssetRouter.l1TokenAddress(l2Token) == address(0));
+
+        l2NativeTokenVault.registerToken(l2Token);
+    }
+
+    // function registerTokenWithVaultV2(uint256 _l1TokenIndex) external {
+    //     uint256 l1TokenIndex = bound(_l1TokenIndex, 0, l1Tokens.length - 1);
+
+    //     address l1Token = l1Tokens[l1TokenIndex];
+    //     address l2Token = l2AssetRouter.l2TokenAddress(l1Token);
+    //     bytes memory d = DataEncoding.encodeBridgeBurnData(0, address(0), l2Token);
+    //     bytes32
+    // }
 }
