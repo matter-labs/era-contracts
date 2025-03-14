8320cfff1e04b4c794c86f86c862608e23d1dffd
Nikita Stupin <178824211+nikitastupin-matterlabs@users.noreply.github.com>
2025-03-14 16:04:45 +0100

    fix `deployL2Contracts`

--- l1-contracts/test/invariant/l2-context/AssetRouterTest.t.sol
+++ l1-contracts/test/invariant/l2-context/AssetRouterTest.t.sol
@@ -9,7 +9,8 @@ import {DeployUtils} from "deploy-scripts/DeployUtils.s.sol";
 import {L1AssetRouterActorHandler} from "../handlers/L1AssetRouterActorHandler.sol";
 import {UserActorHandler} from "../handlers/UserActorHandler.sol";
 import {AssetRouter_ActorHandler_Deployer} from "../deployers/AssetRouter_ActorHandler_Deployer.sol";
-import {Token} from "../common/Types.sol";
+import {AssetRouter_Token_Deployer} from "../deployers/AssetRouter_Token_Deployer.sol";
+import {Token, ActorHandlerAddresses} from "../common/Types.sol";
 
 import {AssetRouterProperties} from "../properties/AssetRouterProperties.sol";
 import {SharedL2ContractL2DeployerUtils, SystemContractsArgs} from "../../foundry/l2/integration/_SharedL2ContractL2DeployerUtils.sol";
@@ -21,7 +22,8 @@ contract AssetRouterTest is
     SharedL2ContractL2DeployerUtils,
     SharedL2ContractDeployer,
     AssetRouterProperties,
-    AssetRouter_ActorHandler_Deployer
+    AssetRouter_ActorHandler_Deployer,
+    AssetRouter_Token_Deployer
 {
     function test() internal virtual override(DeployUtils, SharedL2ContractL2DeployerUtils) {}
 
@@ -42,6 +44,12 @@ contract AssetRouterTest is
         uint256 _l1ChainId
     ) public virtual override(SharedL2ContractDeployer, SharedL2ContractL1DeployerUtils) {
         super.deployL2Contracts(_l1ChainId);
-        deployActorHandlers(new Token[](0));
+
+        Token[] memory deployedTokens = _deployTokens();
+        ActorHandlerAddresses memory actorHandlerAddresses = deployActorHandlers(deployedTokens);
+
+        assertEq(deployedTokens.length, 6);
+
+        initAssetRouterProperties(deployedTokens, actorHandlerAddresses);
     }
 }
