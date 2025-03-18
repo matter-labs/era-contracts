fc26d9fc0d5d775f108a453af7aada8b1cd96d39
Stanislav Bezkorovainyi <stanislavbezkor@gmail.com>
2025-03-18 13:11:55 +0100

    Fix v27 scripts to work with the server (#1346)

--- l1-contracts/deploy-scripts/DeployL1.s.sol
+++ l1-contracts/deploy-scripts/DeployL1.s.sol
@@ -519,7 +519,16 @@ contract DeployL1Script is Script, DeployUtils {
             "blob_versioned_hash_retriever_addr",
             addresses.blobVersionedHashRetriever
         );
-        vm.serializeAddress("deployed_addresses", "server_notifier", addresses.stateTransition.serverNotifierProxy);
+        vm.serializeAddress(
+            "deployed_addresses",
+            "server_notifier_proxy_addr",
+            addresses.stateTransition.serverNotifierProxy
+        );
+        vm.serializeAddress(
+            "deployed_addresses",
+            "server_notifier_implementation_address",
+            addresses.stateTransition.serverNotifierImplementation
+        );
         vm.serializeAddress("deployed_addresses", "governance_addr", addresses.governance);
         vm.serializeAddress("deployed_addresses", "transparent_proxy_admin_addr", addresses.transparentProxyAdmin);
 
