4ce8459185febe4afd9bfe3fa6d15d2b3a54a079
0xValera <valera7743@icloud.com>
2025-03-20 18:55:01 +0100

    fix hashes + update recompute_hashes.sh
    Fixed hashes, calculated with correct Foundry version.
    Added check of Foundry version to recompute_hashes.sh.

--- recompute_hashes.sh
+++ recompute_hashes.sh
@@ -1,6 +1,28 @@
 # Script to recompile and recreate the hashes for the contracts.
 # source ./recompute_hashes.sh
 
+# Expected Foundry version and commit
+EXPECTED_VERSION="0.0.2"
+EXPECTED_COMMIT="27360d4c8"
+
+# Check if Foundry is installed
+if ! command -V forge &> /dev/null; then
+  echo "Foundry is not installed. Please install it using foundryup-zksync with commit ${EXPECTED_COMMIT}."
+  return 1
+fi
+
+# Get installed Foundry version
+FORGE_OUTPUT=$(forge --version)
+INSTALLED_VERSION=$(echo "$FORGE_OUTPUT" | awk '{print $2}')
+INSTALLED_COMMIT=$(echo "$FORGE_OUTPUT" | awk -F'[()]' '{print $2}' | awk '{print $1}')
+
+# Check if Foundry version is as expected
+if [ "$INSTALLED_VERSION" != "$EXPECTED_VERSION" ] || [ "$INSTALLED_COMMIT" != "$EXPECTED_COMMIT" ]; then
+  echo "Incorrect Foundry version."
+  echo "Expected: forge ${EXPECTED_VERSION} (${EXPECTED_COMMIT})"
+  echo "Found:    forge ${INSTALLED_VERSION} (${INSTALLED_COMMIT})"
+  return 1
+fi
 
 # Update submodules (just in case)
 git submodule update --init --recursive
