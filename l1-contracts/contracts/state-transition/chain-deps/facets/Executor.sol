c76cf83b50e382d701089000bcb6ca149efe3532
perekopskiy <mikeson.dp@gmail.com>
2025-03-25 22:58:20 +0200

    comment out stuff

--- l1-contracts/contracts/state-transition/chain-deps/facets/Executor.sol
+++ l1-contracts/contracts/state-transition/chain-deps/facets/Executor.sol
@@ -256,13 +256,14 @@ contract ExecutorFacet is ZKChainBase, IExecutor {
         // We only require 7 logs to be checked, the 8th is if we are expecting a protocol upgrade
         // Without the protocol upgrade we expect 7 logs: 2^7 - 1 = 127
         // With the protocol upgrade we expect 8 logs: 2^8 - 1 = 255
-        if (_expectedSystemContractUpgradeTxHash == bytes32(0)) {
-            if (processedLogs != 127) {
-                revert MissingSystemLogs(127, processedLogs);
-            }
-        } else if (processedLogs != 255) {
-            revert MissingSystemLogs(255, processedLogs);
-        }
+        // TODO: zkos
+        //        if (_expectedSystemContractUpgradeTxHash == bytes32(0)) {
+        //            if (processedLogs != 127) {
+        //                revert MissingSystemLogs(127, processedLogs);
+        //            }
+        //        } else if (processedLogs != 255) {
+        //            revert MissingSystemLogs(255, processedLogs);
+        //        }
     }
 
     /// @inheritdoc IExecutor
