08945039aab093cc9cd6b3ae5c3b1f3cfbcd6332
c-node <chuckhead7@gmail.com>
2025-04-01 19:43:09 +0200

    error types

--- da-contracts/contracts/da-layers/celestia/CelestiaL1DAValidator.sol
+++ da-contracts/contracts/da-layers/celestia/CelestiaL1DAValidator.sol
@@ -6,6 +6,9 @@ import {ISP1Verifier} from "./ISP1Verifier.sol";
 import {CelestiaZKStackInput} from "./types.sol";
 
 contract CelestiaL1DAValidator is IL1DAValidator {
+    error InvalidProof();
+    error OperatorDAHashMismatch(bytes32 expected, bytes32 actual);
+    error DataRootMismatch(bytes32 expected, bytes32 actual);
 
     address public immutable SP1_GROTH_16_VERIFIER;
     address public immutable BLOBSTREAM;
@@ -50,14 +53,14 @@ contract CelestiaL1DAValidator is IL1DAValidator {
         );
 
         // can use custom error or whatever matter labs likes the most
-        if (!valid) revert("INVALID_PROOF");
+        if (!valid) revert InvalidProof();
 
         output.stateDiffHash = l2DAValidatorOutputHash;
 
         if (output.stateDiffHash != eqKeccakHash)
-            revert("operator DA hash does not match value from equivalence proof");
+            revert OperatorDAHashMismatch(eqKeccakHash, output.stateDiffHash);
         if (input.attestationProof.tuple.dataRoot != eqDataRoot)
-            revert("data root does not match value from equivalence proof");
+            revert DataRootMismatch(eqDataRoot, input.attestationProof.tuple.dataRoot);
 
         output.blobsLinearHashes = new bytes32[](_maxBlobsSupported);
         output.blobsOpeningCommitments = new bytes32[](_maxBlobsSupported);
