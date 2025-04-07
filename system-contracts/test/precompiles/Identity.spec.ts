b81f2b3137fbcca644fe0250f6e526a8a8dfec6b
Vladislav Volosnikov <Volosnikov.apmath@gmail.com>
2025-04-07 11:08:59 -0500

    Test Identity in EVM context

--- system-contracts/test/precompiles/Identity.spec.ts
+++ system-contracts/test/precompiles/Identity.spec.ts
@@ -1,6 +1,7 @@
 import { expect } from "chai";
 import type { Contract } from "zksync-ethers";
-import { callFallback, deployContractYul } from "../shared/utils";
+import { callFallback, deployContractYul, enableEvmEmulation } from "../shared/utils";
+import { deployEvmPrecompileCaller } from "./shared/utils";
 
 describe("Identity tests", function () {
   let identity: Contract;
@@ -13,6 +14,15 @@ describe("Identity tests", function () {
     it("Returns data", async () => {
       const data = "0xff00ff00ff00ff00ff";
       const returnData = await callFallback(identity, data);
+      expect(returnData).to.be.equal(data);
+    });
+
+    it("Returns data in EVM context", async () => {
+      enableEvmEmulation();
+      const precompileCaller = await deployEvmPrecompileCaller(identity.address);
+
+      const data = "0xff00ff00ff00ff00ff";
+      const returnData = await callFallback(precompileCaller, data);
       expect(returnData).to.be.equal(data);
     });
   });
