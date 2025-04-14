dd73f05a3a792a0dd3c67e84c736ea2d6cc75c00
Raid Ateir <ateirraid@gmail.com>
2025-04-14 22:40:31 +0400

    update hashes, add back tests, cleanup modexp

--- system-contracts/test/precompiles/Modexp.spec.ts
+++ system-contracts/test/precompiles/Modexp.spec.ts
@@ -22,261 +22,161 @@ describe("Modexp tests", function () {
       });
 
       describe("Ethereum Tests", function () {
-        it("Test 1: (0, 0) mod 0", async () => {
-          const input = "0x" + "00".padStart(64, "0") + "00".padStart(64, "0") + "00".padStart(64, "0");
-          const returnData = await callFallback(modexp, input);
-          console.log("Returned:", returnData);
-        });
-
-        it("Test 2: (0, 0) mod 1", async () => {
-          const input = "0x" + "00".padStart(64, "0") + "00".padStart(64, "0") + "01".padStart(64, "0") + "01";
-          const returnData = await callFallback(modexp, input);
-          console.log("Returned:", returnData);
-        });
-
-        it("Test 3: (0, 0) mod 2", async () => {
-          const input = "0x" + "00".padStart(64, "0") + "00".padStart(64, "0") + "01".padStart(64, "0") + "02";
-          const returnData = await callFallback(modexp, input);
-          console.log("Returned:", returnData);
-        });
-
-        it("Test 4: (0, 0) mod 4", async () => {
-          const input = "0x" + "00".padStart(64, "0") + "00".padStart(64, "0") + "01".padStart(64, "0") + "04";
-          const returnData = await callFallback(modexp, input);
-          console.log("Returned:", returnData);
-        });
-
-        it("Test 5: (0, 0) mod 8", async () => {
-          const input = "0x" + "00".padStart(64, "0") + "00".padStart(64, "0") + "01".padStart(64, "0") + "08";
-          const returnData = await callFallback(modexp, input);
-          console.log("Returned:", returnData);
-        });
-
-        it("Test 6: (0, 0) mod 16", async () => {
-          const input = "0x" + "00".padStart(64, "0") + "00".padStart(64, "0") + "01".padStart(64, "0") + "10";
-          const returnData = await callFallback(modexp, input);
-          console.log("Returned:", returnData);
-        });
-
-        it("Test 7: (0, 0) mod 32", async () => {
-          const input = "0x" + "00".padStart(64, "0") + "00".padStart(64, "0") + "01".padStart(64, "0") + "20";
-          const returnData = await callFallback(modexp, input);
-          console.log("Returned:", returnData);
-        });
-
-        it("Test 8: (0, 0) mod 64", async () => {
-          const input = "0x" + "00".padStart(64, "0") + "00".padStart(64, "0") + "01".padStart(64, "0") + "40";
-          const returnData = await callFallback(modexp, input);
-          console.log("Returned:", returnData);
-        });
-
-        it("Test 9: (0, 0) mod 100", async () => {
-          const input = "0x" + "00".padStart(64, "0") + "00".padStart(64, "0") + "01".padStart(64, "0") + "64";
-          const returnData = await callFallback(modexp, input);
-          console.log("Returned:", returnData);
-        });
-
-        it("Test 10: (0, 0) mod 128", async () => {
-          const input = "0x" + "00".padStart(64, "0") + "00".padStart(64, "0") + "01".padStart(64, "0") + "80";
-          const returnData = await callFallback(modexp, input);
-          console.log("Returned:", returnData);
-        });
-
-        // it("Test 4: (1^1) mod 1", async () => {
-        //   const input = "0x000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000001010101";
-        //   const returnData = await callFallback(modexp, input);
-        //   expect(returnData).to.equal("0x0");
-        // });
-
-        it("Test 6: (3^9984) mod 39936", async () => {
-          const input =
-            "0x0000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000020327009c00";
-          const returnData = await callFallback(modexp, input);
-          expect(returnData).to.equal("0x6801");
-        });
-
-        // it("Test 7: (9^37111) mod 37111", async () => {
-        //   const input = "0x00000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000209390f0";
-        //   const returnData = await callFallback(modexp, input);
-        //   expect(returnData).to.equal("0x1c3b");
-        // });
-
-        // it("Test 8: (49^2401) mod 2401", async () => {
-        //   const input = "0x000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000002311961";
-        //   const returnData = await callFallback(modexp, input);
-        //   expect(returnData).to.equal("0x0");
-        // });
-
-        // it("Test 9: (37120^22411) mod 22000", async () => {
-        //   const input = "0x00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000290f079081f50";
-        //   const returnData = await callFallback(modexp, input);
-        //   expect(returnData).to.equal("0x3e80");
-        // });
-
-        it("Test 10: (39936^1) mod 55201", async () => {
-          const input =
-            "0x0000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000029c0001d7a1";
-          const returnData = await callFallback(modexp, input);
-          expect(returnData).to.equal("0x9c00");
-        });
-
-        it("Test 11: (55190^55190) mod 42965", async () => {
+        it("0^0 mod 1", async () => {
           const input =
             "0x" +
-            "0000000000000000000000000000000000000000000000000000000000000002" + // baseLen = 2
-            "0000000000000000000000000000000000000000000000000000000000000002" + // expLen = 2
-            "0000000000000000000000000000000000000000000000000000000000000002" + // modLen = 2
-            "d796" + // base = 55190
-            "d796" + // exponent = 55190
-            "a7d5"; // modulus = 42965
+            "0000000000000000000000000000000000000000000000000000000000000001" + // baseLen = 1
+            "0000000000000000000000000000000000000000000000000000000000000001" + // expLen = 1
+            "0000000000000000000000000000000000000000000000000000000000000001" + // modLen = 1
+            "00" + // base=0
+            "00" + // exp=0
+            "01"; // mod=1
           const returnData = await callFallback(modexp, input);
-          console.log(returnData);
-          expect(returnData).to.equal("0x866a");
+          expect(returnData).to.equal("0x00");
         });
 
-        // it("Test 12: (0^0) mod 2", async () => {
-        //   const input = "0x" +
-        //     "0000000000000000000000000000000000000000000000000000000000000000" +
-        //     "0000000000000000000000000000000000000000000000000000000000000000" +
-        //     "0000000000000000000000000000000000000000000000000000000000000001" +
-        //     "00" + "00" + "02";
-        //   const returnData = await callFallback(modexp, input);
-        //   expect(returnData).to.equal("0x01");
-        // });
-
-        // it("Test 13: (1^0) mod 2", async () => {
-        //   const input = "0x" +
-        //     "0000000000000000000000000000000000000000000000000000000000000001" +
-        //     "0000000000000000000000000000000000000000000000000000000000000000" +
-        //     "0000000000000000000000000000000000000000000000000000000000000001" +
-        //     "01" + "00" + "02";
-        //   const returnData = await callFallback(modexp, input);
-        //   expect(returnData).to.equal("0x01");
-        // });
-
-        it("Test 14: (1^1) mod 2", async () => {
+        it("0^1 mod 1", async () => {
           const input =
             "0x" +
             "0000000000000000000000000000000000000000000000000000000000000001" +
             "0000000000000000000000000000000000000000000000000000000000000001" +
             "0000000000000000000000000000000000000000000000000000000000000001" +
-            "01" +
-            "01" +
-            "02";
-          const returnData = await callFallback(modexp, input);
-          expect(returnData).to.equal("0x01");
-        });
-
-        it("Test 15: (2^2) mod 5", async () => {
-          const input =
-            "0x" +
-            "0000000000000000000000000000000000000000000000000000000000000001" +
-            "0000000000000000000000000000000000000000000000000000000000000001" +
-            "0000000000000000000000000000000000000000000000000000000000000001" +
-            "02" +
-            "02" +
-            "05";
-          const returnData = await callFallback(modexp, input);
-          expect(returnData).to.equal("0x04");
-        });
-
-        it("Test 16: (3^2) mod 2", async () => {
-          const input =
-            "0x" +
-            "0000000000000000000000000000000000000000000000000000000000000001" +
-            "0000000000000000000000000000000000000000000000000000000000000001" +
-            "0000000000000000000000000000000000000000000000000000000000000001" +
-            "03" +
-            "02" +
-            "02";
-          const returnData = await callFallback(modexp, input);
-          expect(returnData).to.equal("0x01");
-        });
-
-        it("Test 17: (2^1) mod 1", async () => {
-          const input =
-            "0x" +
-            "0000000000000000000000000000000000000000000000000000000000000001" +
-            "0000000000000000000000000000000000000000000000000000000000000001" +
-            "0000000000000000000000000000000000000000000000000000000000000001" +
-            "02" +
+            "00" +
             "01" +
             "01";
           const returnData = await callFallback(modexp, input);
           expect(returnData).to.equal("0x00");
         });
 
-        it("Test 18: (0^2) mod 3", async () => {
+        it("1^0 mod 1", async () => {
           const input =
             "0x" +
             "0000000000000000000000000000000000000000000000000000000000000001" +
             "0000000000000000000000000000000000000000000000000000000000000001" +
             "0000000000000000000000000000000000000000000000000000000000000001" +
+            "01" +
             "00" +
-            "02" +
-            "03";
+            "01";
           const returnData = await callFallback(modexp, input);
           expect(returnData).to.equal("0x00");
         });
 
-        it("Test 19: (3^0) mod 5", async () => {
+        it("1^1 mod 1", async () => {
+          const input =
+            "0x" +
+            "0000000000000000000000000000000000000000000000000000000000000001" +
+            "0000000000000000000000000000000000000000000000000000000000000001" +
+            "0000000000000000000000000000000000000000000000000000000000000001" +
+            "01" +
+            "01" +
+            "01";
+          const returnData = await callFallback(modexp, input);
+          expect(returnData).to.equal("0x00");
+        });
+
+        it("3^5 mod 100", async () => {
           const input =
             "0x" +
             "0000000000000000000000000000000000000000000000000000000000000001" +
             "0000000000000000000000000000000000000000000000000000000000000001" +
             "0000000000000000000000000000000000000000000000000000000000000001" +
             "03" +
-            "00" +
-            "05";
+            "05" +
+            "64"; // 100 in hex
           const returnData = await callFallback(modexp, input);
-          expect(returnData).to.equal("0x01");
+          expect(returnData).to.equal("0x2b"); // 43 in hex
         });
 
-        it("0 bytes: (0, 0) mod 0", async () => {
+        it("3^9984 mod 39936", async () => {
           const input =
             "0x" +
-            "0000000000000000000000000000000000000000000000000000000000000000" + // baseLen
-            "0000000000000000000000000000000000000000000000000000000000000000" + // expLen
-            "0000000000000000000000000000000000000000000000000000000000000000"; // modLen
+            "0000000000000000000000000000000000000000000000000000000000000001" +
+            "0000000000000000000000000000000000000000000000000000000000000002" + // expLen = 2
+            "0000000000000000000000000000000000000000000000000000000000000002" +
+            "03" +
+            "2700" + // 9984 in hex
+            "9c00"; // 39936 in hex
           const returnData = await callFallback(modexp, input);
-          expect(returnData).to.be.equal("0x");
+          expect(returnData).to.equal("0x6801");
         });
 
-        it("1-byte base: (2^0, 0) mod 1", async () => {
+        it("49^2401 mod 2401", async () => {
           const input =
             "0x" +
-            "0000000000000000000000000000000000000000000000000000000000000001" + // baseLen = 1
-            "0000000000000000000000000000000000000000000000000000000000000001" + // expLen = 1
-            "0000000000000000000000000000000000000000000000000000000000000001" + // modLen = 1
-            "02" +
+            "0000000000000000000000000000000000000000000000000000000000000001" +
+            "0000000000000000000000000000000000000000000000000000000000000002" +
+            "0000000000000000000000000000000000000000000000000000000000000002" +
+            "31" + // 49 in hex
+            "0961" + // 2401 in hex
+            "0961"; // 2401 in hex
+          const returnData = await callFallback(modexp, input);
+          expect(returnData).to.equal("0x0000");
+        });
+
+        it("37120^37111 mod 37111", async () => {
+          const input =
+            "0x" +
+            "0000000000000000000000000000000000000000000000000000000000000002" +
+            "0000000000000000000000000000000000000000000000000000000000000002" +
+            "0000000000000000000000000000000000000000000000000000000000000002" +
+            "9100" + // 37120 in hex
+            "90f7" + // 37111 in hex
+            "90f7"; // 37111 in hex
+          const returnData = await callFallback(modexp, input);
+          expect(returnData).to.equal("0x1c3b");
+        });
+
+        it("39936^1 mod 55201", async () => {
+          const input =
+            "0x" +
+            "0000000000000000000000000000000000000000000000000000000000000002" +
+            "0000000000000000000000000000000000000000000000000000000000000001" +
+            "0000000000000000000000000000000000000000000000000000000000000002" +
+            "9c00" + // 39936 in hex
+            "01" +
+            "d7a1"; // 55201 in hex
+          const returnData = await callFallback(modexp, input);
+          expect(returnData).to.equal("0x9c00");
+        });
+
+        it("55190^55190 mod 42965", async () => {
+          const input =
+            "0x" +
+            "0000000000000000000000000000000000000000000000000000000000000002" +
+            "0000000000000000000000000000000000000000000000000000000000000002" +
+            "0000000000000000000000000000000000000000000000000000000000000002" +
+            "d796" + // 55190 in hex
+            "d796" + // 55190 in hex
+            "a7d5"; // 42965 in hex
+          const returnData = await callFallback(modexp, input);
+          expect(returnData).to.equal("0x866a");
+        });
+
+        // Edge cases
+        it("0^0 mod 0", async () => {
+          const input =
+            "0x" +
+            "0000000000000000000000000000000000000000000000000000000000000001" +
+            "0000000000000000000000000000000000000000000000000000000000000001" +
+            "0000000000000000000000000000000000000000000000000000000000000001" +
             "00" +
-            "01"; // base=2, exp=0, mod=1
+            "00" +
+            "00";
           const returnData = await callFallback(modexp, input);
           expect(returnData).to.equal("0x00");
         });
 
-        it("Small numbers: (2^5) mod 13 = 6", async () => {
-          const base = "02",
-            exp = "05",
-            mod = "0d";
+        it("0^1 mod 0", async () => {
           const input =
-            "0x" + "01".padStart(64, "0") + "01".padStart(64, "0") + "01".padStart(64, "0") + base + exp + mod;
-          const returnData = await callFallback(modexp, input);
-          expect(returnData).to.equal("0x06");
-        });
-
-        it("Base = 0, any exp, mod != 0 → result = 0", async () => {
-          const input =
-            "0x" + "01".padStart(64, "0") + "01".padStart(64, "0") + "01".padStart(64, "0") + "00" + "02" + "0f";
+            "0x" +
+            "0000000000000000000000000000000000000000000000000000000000000001" +
+            "0000000000000000000000000000000000000000000000000000000000000001" +
+            "0000000000000000000000000000000000000000000000000000000000000001" +
+            "00" +
+            "01" +
+            "00";
           const returnData = await callFallback(modexp, input);
           expect(returnData).to.equal("0x00");
-        });
-
-        it("Any base, exp = 0, mod != 0 → result = 1", async () => {
-          const input =
-            "0x" + "01".padStart(64, "0") + "01".padStart(64, "0") + "01".padStart(64, "0") + "03" + "00" + "05"; // base = 3, exp = 0, mod = 5
-          const returnData = await callFallback(modexp, input);
-          expect(returnData).to.equal("0x01");
         });
 
         it("Max supported bytes (32-byte base/exp/mod)", async () => {
