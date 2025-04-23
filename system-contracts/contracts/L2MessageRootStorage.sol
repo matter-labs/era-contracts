eccc9470a86d18ba586b6d2ebbd31b5e973c7d60
kelemeno <kl@matterlabs.dev>
2025-04-23 12:22:36 +0200

    more renaming

--- system-contracts/contracts/L2MessageRootStorage.sol
+++ system-contracts/contracts/L2MessageRootStorage.sol
@@ -4,25 +4,25 @@ pragma solidity ^0.8.24;
 
 import {SystemContractBase} from "./abstract/SystemContractBase.sol";
 
-event MessageRootAdded(uint256 indexed chainId, uint256 indexed blockNumber, bytes32[] sides);
+event InteropRootAdded(uint256 indexed chainId, uint256 indexed blockNumber, bytes32[] sides);
 error SidesLengthNotOne();
-error MessageRootAlreadyExists();
+error InteropRootAlreadyExists();
 
 /**
  * @author Matter Labs
  * @custom:security-contact security@matterlabs.dev
- * @notice MessageRootStorage contract responsible for storing the message roots of other chains on the L2.
+ * @notice InteropRootStorage contract responsible for storing the message roots of other chains on the L2.
  */
 contract L2MessageRootStorage is SystemContractBase {
     /// @notice Mapping of chain ID to block or batch number to message root.
     mapping(uint256 chainId => mapping(uint256 blockOrBatchNumber => bytes32 msgRoot)) public msgRoots;
 
-    /// @dev Adds a message root to the L2MessageRootStorage contract.
+    /// @dev Adds a message root to the L2InteropRootStorage contract.
     /// @param chainId The chain ID of the chain that the message root is for.
     /// @param blockOrBatchNumber The block or batch number of the message root.
     /// For proof based interop it is block number. For commit based interop it is batch number.
     /// @param sides The message root sides.
-    function addMessageRoot(
+    function addInteropRoot(
         uint256 chainId,
         uint256 blockOrBatchNumber,
         bytes32[] calldata sides
@@ -32,10 +32,10 @@ contract L2MessageRootStorage is SystemContractBase {
             revert SidesLengthNotOne();
         }
         if (msgRoots[chainId][blockOrBatchNumber] != bytes32(0)) {
-            revert MessageRootAlreadyExists();
+            revert InteropRootAlreadyExists();
         }
         msgRoots[chainId][blockOrBatchNumber] = sides[0];
 
-        emit MessageRootAdded(chainId, blockOrBatchNumber, sides);
+        emit InteropRootAdded(chainId, blockOrBatchNumber, sides);
     }
 }
