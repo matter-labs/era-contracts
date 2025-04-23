fabece223918fd81aab9dc4b5c91a71764ef21bc
0xValera <valera7743@icloud.com>
2025-04-23 17:15:15 +0200

    fix hashes and test

--- l1-contracts/deploy-scripts/GatewayCTMDeployerHelper.sol
+++ l1-contracts/deploy-scripts/GatewayCTMDeployerHelper.sol
@@ -97,7 +97,7 @@ library GatewayCTMDeployerHelper {
         address serverNotifierImplementation = _deployInternal(
             "ServerNotifier",
             "ServerNotifier.sol",
-            abi.encode(true),
+            hex"",
             innerConfig
         );
 
