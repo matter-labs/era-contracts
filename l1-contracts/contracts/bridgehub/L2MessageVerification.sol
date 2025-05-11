bf26946e8ddea480527964c06b61b8030fef4692
0xValera <55665573+0xValera@users.noreply.github.com>
2025-05-11 20:14:55 +0200

    Clean-up in preparation for internal audit. (#1465)

--- l1-contracts/contracts/bridgehub/L2MessageVerification.sol
+++ l1-contracts/contracts/bridgehub/L2MessageVerification.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.24;
 
 import {MessageVerification} from "../state-transition/chain-deps/facets/MessageVerification.sol";
 import {MessageHashing, ProofVerificationResult} from "../common/libraries/MessageHashing.sol";
-import {L2_MESSAGE_ROOT_STORAGE} from "../common/l2-helpers/L2ContractAddresses.sol";
+import {L2_INTEROP_ROOT_STORAGE} from "../common/l2-helpers/L2ContractAddresses.sol";
 
 /// @title The interface of the ZKsync L2MessageVerification contract that can be used to prove L2 message inclusion on the L2.
 /// @author Matter Labs
@@ -12,28 +12,28 @@ import {L2_MESSAGE_ROOT_STORAGE} from "../common/l2-helpers/L2ContractAddresses.
 contract L2MessageVerification is MessageVerification {
     function _proveL2LeafInclusion(
         uint256 _chainId,
-        uint256 _batchOrBlockNumber,
+        uint256 _blockOrBatchNumber,
         uint256 _leafProofMask,
         bytes32 _leaf,
         bytes32[] calldata _proof
     ) internal view override returns (bool) {
         ProofVerificationResult memory proofVerificationResult = MessageHashing.hashProof({
             _chainId: _chainId,
-            _batchNumber: _batchOrBlockNumber,
+            _batchNumber: _blockOrBatchNumber,
             _leafProofMask: _leafProofMask,
             _leaf: _leaf,
             _proof: _proof
         });
         if (proofVerificationResult.finalProofNode) {
-            // For proof based interop this is the SL MessageRoot at block number _batchOrBlockNumber
-            bytes32 correctBatchRoot = L2_MESSAGE_ROOT_STORAGE.msgRoots(_chainId, _batchOrBlockNumber);
+            // For proof based interop this is the SL InteropRoot at block number _blockOrBatchNumber
+            bytes32 correctBatchRoot = L2_INTEROP_ROOT_STORAGE.interopRoots(_chainId, _blockOrBatchNumber);
             return correctBatchRoot == proofVerificationResult.batchSettlementRoot;
         }
 
         return
             this.proveL2LeafInclusionShared({
                 _chainId: proofVerificationResult.settlementLayerChainId,
-                _batchNumber: proofVerificationResult.settlementLayerBatchNumber, // SL block number
+                _blockOrBatchNumber: proofVerificationResult.settlementLayerBatchNumber, // SL block number
                 _leafProofMask: proofVerificationResult.settlementLayerBatchRootMask,
                 _leaf: proofVerificationResult.chainIdLeaf,
                 _proof: MessageHashing.extractSliceUntilEnd(_proof, proofVerificationResult.ptr)
