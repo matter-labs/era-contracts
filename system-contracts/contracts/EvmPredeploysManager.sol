4f06d6c356750cd14b6e221566f7d0c60a7b4aa8
Vladislav Volosnikov <Volosnikov.apmath@gmail.com>
2025-05-19 12:13:14 -0500

    Prettier

--- system-contracts/contracts/EvmPredeploysManager.sol
+++ system-contracts/contracts/EvmPredeploysManager.sol
@@ -70,28 +70,38 @@ contract EvmPredeploysManager {
 
     // https://github.com/safe-global/safe-smart-account/blob/v1.0.0/contracts/proxies/ProxyFactory.sol
     address private constant SAFE_V_1_0_0_PROXY_FACTORY = 0x12302fE9c02ff50939BaAaaf415fc226C078613C;
-    bytes32 private constant SAFE_V_1_0_0_PROXY_FACTORY_INPUT_HASH = 0xbcacfea0a55dc0390ab8ef100d102719f7661a826928ed9a46c14e2edfea3bf6;
-    bytes32 private constant SAFE_V_1_0_0_PROXY_FACTORY_BYTECODE_HASH = 0x020008c4f0279eb49f9f14b7406d60eb289ca2787d82f483cb86d9e99bb8a101;
+    bytes32 private constant SAFE_V_1_0_0_PROXY_FACTORY_INPUT_HASH =
+        0xbcacfea0a55dc0390ab8ef100d102719f7661a826928ed9a46c14e2edfea3bf6;
+    bytes32 private constant SAFE_V_1_0_0_PROXY_FACTORY_BYTECODE_HASH =
+        0x020008c4f0279eb49f9f14b7406d60eb289ca2787d82f483cb86d9e99bb8a101;
 
     // https://github.com/safe-global/safe-smart-account/blob/v1.0.0/contracts/common/MasterCopy.sol
     address private constant SAFE_V_1_0_0_MASTER_COPY = 0xb6029EA3B2c51D09a50B53CA8012FeEB05bDa35A;
-    bytes32 private constant SAFE_V_1_0_0_MASTER_COPY_INPUT_HASH = 0x97b9ea932e099f632f552d14b853fc6e515cd5e9ecd2deb4ce4af482d6080220;
-    bytes32 private constant SAFE_V_1_0_0_MASTER_COPY_BYTECODE_HASH = 0x02005e93b66a5362f40710b265a726a3e54980ec0671818218495686a7db4dec;
+    bytes32 private constant SAFE_V_1_0_0_MASTER_COPY_INPUT_HASH =
+        0x97b9ea932e099f632f552d14b853fc6e515cd5e9ecd2deb4ce4af482d6080220;
+    bytes32 private constant SAFE_V_1_0_0_MASTER_COPY_BYTECODE_HASH =
+        0x02005e93b66a5362f40710b265a726a3e54980ec0671818218495686a7db4dec;
 
     // https://github.com/safe-global/safe-smart-account/blob/v1.1.1/contracts/proxies/ProxyFactory.sol
     address private constant SAFE_V_1_1_1_PROXY_FACTORY = 0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B;
-    bytes32 private constant SAFE_V_1_1_1_PROXY_FACTORY_INPUT_HASH = 0xdc9862936dccef7f4d711ccb053f33cf4dab76cd12a6206d36c34831f53e2fc4;
-    bytes32 private constant SAFE_V_1_1_1_PROXY_FACTORY_BYTECODE_HASH = 0x02000f73eadfd3b87e92b698ac1ff0b7a437ef26e843a66a9946b7a308620d4c;
+    bytes32 private constant SAFE_V_1_1_1_PROXY_FACTORY_INPUT_HASH =
+        0xdc9862936dccef7f4d711ccb053f33cf4dab76cd12a6206d36c34831f53e2fc4;
+    bytes32 private constant SAFE_V_1_1_1_PROXY_FACTORY_BYTECODE_HASH =
+        0x02000f73eadfd3b87e92b698ac1ff0b7a437ef26e843a66a9946b7a308620d4c;
 
     // https://github.com/safe-global/safe-smart-account/blob/v1.1.1/contracts/common/MasterCopy.sol
     address private constant SAFE_V_1_1_1_MASTER_COPY = 0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F;
-    bytes32 private constant SAFE_V_1_1_1_MASTER_COPY_INPUT_HASH = 0x3da4b71b70c73554af3f11c48a590eafa3808a746a69f712cd80b941d28de36e;
-    bytes32 private constant SAFE_V_1_1_1_MASTER_COPY_BYTECODE_HASH = 0x02005de8d155206e252a1b26b5483528523d0b4b4f87e3a5f76e38d52f5d232a;
+    bytes32 private constant SAFE_V_1_1_1_MASTER_COPY_INPUT_HASH =
+        0x3da4b71b70c73554af3f11c48a590eafa3808a746a69f712cd80b941d28de36e;
+    bytes32 private constant SAFE_V_1_1_1_MASTER_COPY_BYTECODE_HASH =
+        0x02005de8d155206e252a1b26b5483528523d0b4b4f87e3a5f76e38d52f5d232a;
 
     // https://github.com/safe-global/safe-smart-account/blob/v1.2.0/contracts/common/MasterCopy.sol
     address private constant SAFE_V_1_2_0_MASTER_COPY = 0x6851D6fDFAfD08c0295C392436245E5bc78B0185;
-    bytes32 private constant SAFE_V_1_2_0_MASTER_COPY_INPUT_HASH = 0x502b90a6c8b2e4005695010b5106ea006c1d128de42fca407ce309024b9266a7;
-    bytes32 private constant SAFE_V_1_2_0_MASTER_COPY_BYTECODE_HASH = 0x02005f5a803376f8aa603948a4d3b6b56e47cdbf0824bcc3ceab8fe14ce27d52;
+    bytes32 private constant SAFE_V_1_2_0_MASTER_COPY_INPUT_HASH =
+        0x502b90a6c8b2e4005695010b5106ea006c1d128de42fca407ce309024b9266a7;
+    bytes32 private constant SAFE_V_1_2_0_MASTER_COPY_BYTECODE_HASH =
+        0x02005f5a803376f8aa603948a4d3b6b56e47cdbf0824bcc3ceab8fe14ce27d52;
 
     /// @notice The method used to deploy pre-defined contract at specified address
     /// @dev Can only use the date of the corresponding presigned deployment transaction as constructorInput
@@ -138,23 +148,18 @@ contract EvmPredeploysManager {
         } else if (contractAddress == SAFE_V_1_0_0_PROXY_FACTORY) {
             expectedInputHash = SAFE_V_1_0_0_PROXY_FACTORY_INPUT_HASH;
             expectedBytecodeHash = SAFE_V_1_0_0_PROXY_FACTORY_BYTECODE_HASH;
-
         } else if (contractAddress == SAFE_V_1_0_0_MASTER_COPY) {
             expectedInputHash = SAFE_V_1_0_0_MASTER_COPY_INPUT_HASH;
             expectedBytecodeHash = SAFE_V_1_0_0_MASTER_COPY_BYTECODE_HASH;
-
         } else if (contractAddress == SAFE_V_1_1_1_PROXY_FACTORY) {
             expectedInputHash = SAFE_V_1_1_1_PROXY_FACTORY_INPUT_HASH;
             expectedBytecodeHash = SAFE_V_1_1_1_PROXY_FACTORY_BYTECODE_HASH;
-
         } else if (contractAddress == SAFE_V_1_1_1_MASTER_COPY) {
             expectedInputHash = SAFE_V_1_1_1_MASTER_COPY_INPUT_HASH;
             expectedBytecodeHash = SAFE_V_1_1_1_MASTER_COPY_BYTECODE_HASH;
-
         } else if (contractAddress == SAFE_V_1_2_0_MASTER_COPY) {
             expectedInputHash = SAFE_V_1_2_0_MASTER_COPY_INPUT_HASH;
             expectedBytecodeHash = SAFE_V_1_2_0_MASTER_COPY_BYTECODE_HASH;
-
         } else {
             revert InvalidInput();
         }
