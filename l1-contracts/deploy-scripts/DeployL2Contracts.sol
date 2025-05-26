1ad65153a4ef77cc6273681cea0e1ccba994b243
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2025-05-26 17:07:43 -0300

    Add EigenDAV2Secure

--- l1-contracts/deploy-scripts/DeployL2Contracts.sol
+++ l1-contracts/deploy-scripts/DeployL2Contracts.sol
@@ -25,9 +25,8 @@ contract DeployL2Script is Script {
         Rollup,
         NoDA,
         Avail,
-        EigenDAV1M0,
-        EigenDAV2M0,
-        EigenDAV2M1
+        EigenDA,
+        EigenDAV2Secure
     }
 
     // solhint-disable-next-line gas-struct-packing
@@ -154,7 +153,7 @@ contract DeployL2Script is Script {
         config.eraChainId = toml.readUint("$.era_chain_id");
 
         uint256 validatorTypeUint = toml.readUint("$.da_validator_type");
-        require(validatorTypeUint < 6, "Invalid DA validator type");
+        require(validatorTypeUint < 5, "Invalid DA validator type");
         config.validatorType = DAValidatorType(validatorTypeUint);
     }
 
@@ -179,11 +178,9 @@ contract DeployL2Script is Script {
             bytecode = L2ContractsBytecodesLib.readNoDAL2DAValidatorBytecode();
         } else if (config.validatorType == DAValidatorType.Avail) {
             bytecode = L2ContractsBytecodesLib.readAvailL2DAValidatorBytecode();
-        } else if (config.validatorType == DAValidatorType.EigenDAV1M0) {
+        } else if (config.validatorType == DAValidatorType.EigenDA) {
             bytecode = L2ContractsBytecodesLib.readNoDAL2DAValidatorBytecode();
-        } else if (config.validatorType == DAValidatorType.EigenDAV2M0) {
-            bytecode = L2ContractsBytecodesLib.readNoDAL2DAValidatorBytecode();
-        } else if (config.validatorType == DAValidatorType.EigenDAV2M1) {
+        } else if (config.validatorType == DAValidatorType.EigenDAV2Secure) {
             bytecode = L2ContractsBytecodesLib.readEigenDAL2DAValidatorBytecode();
         } else {
             revert("Invalid DA validator type");
