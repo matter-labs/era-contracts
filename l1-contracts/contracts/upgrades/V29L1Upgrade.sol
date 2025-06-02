d69c533238b4f4019374b86c1a8dc3c875a99a91
Stanislav Breadless <stanislavbezkor@gmail.com>
2025-06-02 11:02:40 +0200

    delete old and use new validators

--- l1-contracts/contracts/upgrades/V29L1Upgrade.sol
+++ l1-contracts/contracts/upgrades/V29L1Upgrade.sol
@@ -5,15 +5,30 @@ pragma solidity 0.8.28;
 import {Diamond} from "../state-transition/libraries/Diamond.sol";
 import {BaseZkSyncUpgrade, ProposedUpgrade} from "./BaseZkSyncUpgrade.sol";
 import {DEFAULT_PRECOMMITMENT_FOR_THE_LAST_BATCH} from "../common/Config.sol";
+import {IAdmin} from "../state-transition/chain-interfaces/IAdmin.sol";
 
 /// @author Matter Labs
 /// @custom:security-contact security@matterlabs.dev
 contract V29L1Upgrade is BaseZkSyncUpgrade {
+    struct V29UpgradeParams {
+        address oldValidatorTimelock;
+        address newValidatorTimelock;
+    }
+
     /// @notice The main function that will be delegate-called by the chain.
     /// @param _proposedUpgrade The upgrade to be executed.
     function upgrade(ProposedUpgrade calldata _proposedUpgrade) public override returns (bytes32) {
         super.upgrade(_proposedUpgrade);
         s.precommitmentForTheLatestBatch = DEFAULT_PRECOMMITMENT_FOR_THE_LAST_BATCH;
         return Diamond.DIAMOND_INIT_SUCCESS_RETURN_VALUE;
+    }
+
+    function _postUpgrade(bytes calldata _postUpgradeCalldata) internal override {
+        V29UpgradeParams memory params = abi.decode(_postUpgradeCalldata, (V29UpgradeParams));
+        s.validators[params.oldValidatorTimelock] = false;
+        s.validators[params.newValidatorTimelock] = true;
+
+        emit IAdmin.ValidatorStatusUpdate(params.oldValidatorTimelock, false);
+        emit IAdmin.ValidatorStatusUpdate(params.newValidatorTimelock, true);
     }
 }
