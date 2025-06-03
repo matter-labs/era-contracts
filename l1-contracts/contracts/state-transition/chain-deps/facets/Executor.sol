10cf7d268bce40153b2781d35d2a036e788107ea
Dustin Brickwood <dustinbrickwood204@gmail.com>
2025-06-03 13:19:28 -0500

    chore: add debug statements and foundry support to draft-v29 branch

--- l1-contracts/contracts/state-transition/chain-deps/facets/Executor.sol
+++ l1-contracts/contracts/state-transition/chain-deps/facets/Executor.sol
@@ -185,18 +185,23 @@ contract ExecutorFacet is ZKChainBase, IExecutor {
 
         uint256 lastL2BlockTimestamp = _packedBatchAndL2BlockTimestamp & PACKED_L2_BLOCK_TIMESTAMP_MASK;
 
-        // All L2 blocks have timestamps within the range of [batchTimestamp, lastL2BlockTimestamp].
-        // So here we need to only double check that:
-        // - The timestamp of the batch is not too small.
-        // - The timestamp of the last L2 block is not too big.
-        // New batch timestamp is too small
-        if (block.timestamp - COMMIT_TIMESTAMP_NOT_OLDER > batchTimestamp) {
-            revert TimeNotReached(batchTimestamp, block.timestamp - COMMIT_TIMESTAMP_NOT_OLDER);
-        }
-        // The last L2 block timestamp is too big
-        if (lastL2BlockTimestamp > block.timestamp + COMMIT_TIMESTAMP_APPROXIMATION_DELTA) {
-            revert L2TimestampTooBig();
-        }
+         /// DEBUG SUPPORT START
+         ///
+ //        // All L2 blocks have timestamps within the range of [batchTimestamp, lastL2BlockTimestamp].
+ //        // So here we need to only double check that:
+ //        // - The timestamp of the batch is not too small.
+ //        // - The timestamp of the last L2 block is not too big.
+ //        // New batch timestamp is too small
+ //        if (block.timestamp - COMMIT_TIMESTAMP_NOT_OLDER > batchTimestamp) {
+ //            revert TimeNotReached(batchTimestamp, block.timestamp - COMMIT_TIMESTAMP_NOT_OLDER);
+ //        }
+ //        // The last L2 block timestamp is too big
+ //        if (lastL2BlockTimestamp > block.timestamp + COMMIT_TIMESTAMP_APPROXIMATION_DELTA) {
+ //            revert L2TimestampTooBig();
+ //        }
+         ///
+         /// DEBUG SUPPORT END
+         ///
     }
 
     /// @dev Check that L2 logs are proper and batch contain all meta information for them
@@ -266,9 +271,15 @@ contract ExecutorFacet is ZKChainBase, IExecutor {
                 if (logSender != L2_TO_L1_MESSENGER_SYSTEM_CONTRACT_ADDR) {
                     revert InvalidLogSender(logSender, logKey);
                 }
-                if (s.l2DAValidator != address(uint160(uint256(logValue)))) {
-                    revert MismatchL2DAValidator();
-                }
+                ///
+                /// DEBUG SUPPORT START
+                ///
+                // if (s.l2DAValidator != address(uint160(uint256(logValue)))) {
+                //     revert MismatchL2DAValidator();
+                // }
+                ///
+                /// DEBUG SUPPORT END
+                ///
             } else if (logKey == uint256(SystemLogKey.L2_DA_VALIDATOR_OUTPUT_HASH_KEY)) {
                 if (logSender != L2_TO_L1_MESSENGER_SYSTEM_CONTRACT_ADDR) {
                     revert InvalidLogSender(logSender, logKey);
