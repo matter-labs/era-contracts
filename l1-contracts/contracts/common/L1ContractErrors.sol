64418f1d3f404942ce06b671dd428a05f3db73f6
Raid Ateir <ateirraid@gmail.com>
2025-06-06 19:25:37 +0400

    merge draft-v29 vs zksync-os

--- l1-contracts/contracts/common/L1ContractErrors.sol
+++ l1-contracts/contracts/common/L1ContractErrors.sol
@@ -162,6 +162,8 @@ error InvalidSystemLogsLength();
 error InvalidUpgradeTxn(UpgradeTxVerifyParam);
 // 0xfb5c22e6
 error L2TimestampTooBig();
+// 0x55bde247
+error IncorrectBatchChainId();
 // 0x97e1359e
 error L2WithdrawalMessageWrongLength(uint256 messageLen);
 // 0x8efef97a
