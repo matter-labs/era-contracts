0e4248920ede2d7af2be0ef2d36cdedd6aa17bc1
koloz193 <zach.kolodny@gmail.com>
2025-07-14 06:43:39 -0400

    feat: Release v28 (#1473)
    Co-authored-by: Nikita Masych <nikitamasich152@gmail.com>
    Co-authored-by: mm <mm@matterlabs.dev>
    Co-authored-by: Marcin M <128217157+mm-zk@users.noreply.github.com>
    Co-authored-by: Vlad Bochok <41153528+vladbochok@users.noreply.github.com>
    Co-authored-by: Vlad Bochok <vladbochok@Vlads-MacBook-Pro.local>
    Co-authored-by: Vladislav Volosnikov <vladyan18@gmail.com>
    Co-authored-by: Lyova Potyomkin <lyova.potyomkin@gmail.com>
    Co-authored-by: Vladislav Volosnikov <Volosnikov.apmath@gmail.com>
    Co-authored-by: 0xValera <valera7743@icloud.com>
    Co-authored-by: kelemeno <kl@matterlabs.dev>
    Co-authored-by: 0xValera <55665573+0xValera@users.noreply.github.com>
    Co-authored-by: Raid Ateir <ra@matterlabs.dev>
    Co-authored-by: Raid Ateir <ateirraid@gmail.com>
    Co-authored-by: Raid5594 <52794079+Raid5594@users.noreply.github.com>
    Co-authored-by: Fitznik <90274774+Fitznik@users.noreply.github.com>
    Co-authored-by: Stanislav Breadless <stanislavbezkor@gmail.com>
    Co-authored-by: kelemeno <34402761+kelemeno@users.noreply.github.com>
    Co-authored-by: zkzoomer <sergio@zkzoomer.io>
    Co-authored-by: bxpana <42230936+bxpana@users.noreply.github.com>
    Co-authored-by: nikitastupin-matterlabs <178824211+nikitastupin-matterlabs@users.noreply.github.com>
    Co-authored-by: Bruno França <bruno@franca.xyz>

--- l1-contracts/contracts/bridge/interfaces/IL1Nullifier.sol
+++ l1-contracts/contracts/bridge/interfaces/IL1Nullifier.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.24;
+pragma solidity 0.8.28;
 
 import {IBridgehub} from "../../bridgehub/IBridgehub.sol";
 import {IL1NativeTokenVault} from "../ntv/IL1NativeTokenVault.sol";
--- l1-contracts/test/foundry/l1/unit/concrete/Bridges/L1SharedBridge/L1SharedBridgeBase.t.sol
+++ l1-contracts/test/foundry/l1/unit/concrete/Bridges/L1SharedBridge/L1SharedBridgeBase.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
+pragma solidity 0.8.28;
 
 import {IERC20} from "@openzeppelin/contracts-v4/token/ERC20/IERC20.sol";
 
