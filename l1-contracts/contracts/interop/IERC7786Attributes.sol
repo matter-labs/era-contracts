66f743406d7d6b263425ec765ea5ba6520554fac
0xValera <valera7743@icloud.com>
2025-07-22 20:32:13 +0200

    Update IERC7786Attributes.sol

--- l1-contracts/contracts/interop/IERC7786Attributes.sol
+++ l1-contracts/contracts/interop/IERC7786Attributes.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.24;
 
 /// @title IERC778Attributes
 /// @notice Interface for the ERC7786 gateway source
+/// @dev When adding/removing a function here the InteropCenter must be updated to reflect the changes.
 /// https://github.com/ethereum/ERCs/blob/023a7d657666308568d3d1391c578d5972636093/ERCS/erc-7786.md
 interface IERC7786Attributes {
     function indirectCall(uint256 _indirectCallMessageValue) external pure;
