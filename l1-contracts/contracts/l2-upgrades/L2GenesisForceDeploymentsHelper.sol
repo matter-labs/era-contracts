469d88b1e4bb84659406f9a8b8a20b8618785c9e
zkzoomer <sergio@zkzoomer.io>
2025-09-03 22:19:48 +0200

    define errors

--- l1-contracts/contracts/l2-upgrades/L2GenesisForceDeploymentsHelper.sol
+++ l1-contracts/contracts/l2-upgrades/L2GenesisForceDeploymentsHelper.sol
@@ -15,6 +15,7 @@ import {L2MessageRoot} from "../bridgehub/L2MessageRoot.sol";
 import {L2Bridgehub} from "../bridgehub/L2Bridgehub.sol";
 import {L2AssetRouter} from "../bridge/asset-router/L2AssetRouter.sol";
 import {L2ChainAssetHandler} from "../bridgehub/L2ChainAssetHandler.sol";
+import {DeployFailed} from "../common/L1ContractErrors.sol";
 
 import {L2NativeTokenVaultZKOS} from "../bridge/ntv/L2NativeTokenVaultZKOS.sol";
 
@@ -60,9 +61,9 @@ library L2GenesisForceDeploymentsHelper {
         );
 
         // Note, that we dont use interface, but raw call to avoid Solidity checking for empty bytecode
-        (bool success, bytes memory returnData) = L2_DEPLOYER_SYSTEM_CONTRACT_ADDR.call(data);
+        (bool success, ) = L2_DEPLOYER_SYSTEM_CONTRACT_ADDR.call(data);
         if (!success) {
-            revert("Failed to call setBytecodeDetailsEVM");
+            revert DeployFailed();
         }
     }
 
