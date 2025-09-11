e3a70c43fd68b03f80e1ed8e4844629f1bf304df
Vladislav Volosnikov <Volosnikov.apmath@gmail.com>
2025-09-11 08:47:10 -0500

    Change addInteropRoot function

--- system-contracts/contracts/L2InteropRootStorage.sol
+++ system-contracts/contracts/L2InteropRootStorage.sol
@@ -9,6 +9,11 @@ error SidesLengthNotOne();
 error InteropRootAlreadyExists();
 error MessageRootIsZero();
 
+/// @dev For both proof-based and commit-based interop, the `sides` parameter contains only the root.
+/// @dev Once pre-commit interop is introduced, `sides` will include both the root and its associated sides.
+/// @dev This interface is preserved now so that enabling pre-commit interop later requires no changes in interface.
+/// @dev In proof-based and pre-commit interop, `blockOrBatchNumber` represents the block number, in commit-based interop,
+/// it represents the batch number. This distinction reflects the implementation requirements  of each interop finality form.
 /// @param chainId The chain ID of the chain that the message root is for.
 /// @param blockOrBatchNumber The block or batch number of the message root. Either of block number or batch number will be used,
 /// depends on finality form of interop.
@@ -29,21 +34,9 @@ contract L2InteropRootStorage is SystemContractBase {
     mapping(uint256 chainId => mapping(uint256 blockOrBatchNumber => bytes32 interopRoot)) public interopRoots;
 
     /// @dev Adds a message root to the L2InteropRootStorage contract.
-    /// @dev For both proof-based and commit-based interop, the `sides` parameter contains only the root.
-    /// @dev Once pre-commit interop is introduced, `sides` will include both the root and its associated sides.
-    /// @dev This interface is preserved now so that enabling pre-commit interop later requires no changes in interface.
-    /// @dev In proof-based and pre-commit interop, `blockOrBatchNumber` represents the block number, in commit-based interop,
-    /// it represents the batch number. This distinction reflects the implementation requirements  of each interop finality form.
-    /// @param chainId The chain ID of the chain that the message root is for.
-    /// @param blockOrBatchNumber The block or batch number of the message root. Either of block number or batch number will be used,
-    /// depends on finality form of interop, mentioned above.
-    /// @param sides The message root sides. Note, that `sides` here are coming from `DynamicIncrementalMerkle` nomenclature.
-    function addInteropRoot(
-        uint256 chainId,
-        uint256 blockOrBatchNumber,
-        bytes32[] calldata sides
-    ) external onlyCallFromBootloader {
-        _addInteropRoot(chainId, blockOrBatchNumber, sides);
+    /// @param interopRoot The interop roots. See the description of the corresponding struct.
+    function addInteropRoot(InteropRoot calldata interopRoot) external onlyCallFromBootloader {
+        _addInteropRoot(interopRoot.chainId, interopRoot.blockOrBatchNumber, interopRoot.sides);
     }
 
     /// @dev Adds a group of interop roots to the L2InteropRootStorage contract.
