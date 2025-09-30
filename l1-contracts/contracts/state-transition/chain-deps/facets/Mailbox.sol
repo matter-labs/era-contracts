d3681bf70ab2eb88baac6ef91b72813dd7939e92
0xValera <valera7743@icloud.com>
2025-09-30 20:09:02 +0200

    reformulate comment

--- l1-contracts/contracts/state-transition/chain-deps/facets/Mailbox.sol
+++ l1-contracts/contracts/state-transition/chain-deps/facets/Mailbox.sol
@@ -434,7 +434,7 @@ contract MailboxFacet is ZKChainBase, IMailboxImpl, MessageVerification {
     function _requestL2Transaction(WritePriorityOpParams memory _params) internal returns (bytes32 canonicalTxHash) {
         BridgehubL2TransactionRequest memory request = _params.request;
 
-        // For ZKsync OS factory deps will be ignored
+        // Factory deps are not used in ZKsync OS in non-upgrade transactions.
         if (request.factoryDeps.length > MAX_NEW_FACTORY_DEPS) {
             revert TooManyFactoryDeps();
         }
