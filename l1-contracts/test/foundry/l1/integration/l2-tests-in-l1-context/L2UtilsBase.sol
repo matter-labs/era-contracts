9abc1fcc9cf38ce559a4ba6fd7c8116233ab2fb3
kelemeno <kl@matterlabs.dev>
2025-10-01 22:24:48 +0100

    fix tests

--- l1-contracts/test/foundry/l1/integration/l2-tests-in-l1-context/L2UtilsBase.sol
+++ l1-contracts/test/foundry/l1/integration/l2-tests-in-l1-context/L2UtilsBase.sol
@@ -84,7 +84,6 @@ library L2UtilsBase {
             L2_CHAIN_ASSET_HANDLER_ADDR,
             aliasedL1ChainRegistrationSender
         );
-        L2Bridgehub(L2_BRIDGEHUB_ADDR).setChainAssetHandler(L2_CHAIN_ASSET_HANDLER_ADDR);
         {
             address l2messageVerification = address(new L2MessageVerification());
             vm.etch(address(L2_MESSAGE_VERIFICATION), l2messageVerification.code);
