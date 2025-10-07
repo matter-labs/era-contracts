7fcd5f28e33c9e96fe679195bfa353d566458f02
kelemeno <kl@matterlabs.dev>
2025-10-07 15:29:32 +0100

    fix test

--- l1-contracts/test/foundry/l2/integration/L2Utils.sol
+++ l1-contracts/test/foundry/l2/integration/L2Utils.sol
@@ -138,6 +138,7 @@ library L2Utils {
 
         forceDeployWithoutConstructor("InteropHandler", L2_INTEROP_HANDLER_ADDR);
         InteropHandler interopHandler = InteropHandler(L2_INTEROP_HANDLER_ADDR);
+        vm.prank(L2_COMPLEX_UPGRADER_ADDR);
         interopHandler.initL2(_args.l1ChainId);
     }
 
