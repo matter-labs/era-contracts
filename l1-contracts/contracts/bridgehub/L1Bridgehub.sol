f6c4fe5ef93b79b04fe566737939851ca764edfc
0xValera <valera7743@icloud.com>
2025-10-19 17:00:16 +0200

    redundant modifier removal

--- l1-contracts/contracts/bridgehub/L1Bridgehub.sol
+++ l1-contracts/contracts/bridgehub/L1Bridgehub.sol
@@ -15,7 +15,7 @@ import {IZKChain} from "../state-transition/chain-interfaces/IZKChain.sol";
 import {ICTMDeploymentTracker} from "./ICTMDeploymentTracker.sol";
 import {IMessageRoot} from "./IMessageRoot.sol";
 import {BridgehubL2TransactionRequest} from "../common/Messaging.sol";
-import {ChainIdAlreadyPresent, ChainNotLegacy, ChainNotPresentInCTM, NotL1, SecondBridgeAddressTooLow} from "./L1BridgehubErrors.sol";
+import {ChainIdAlreadyPresent, ChainNotLegacy, ChainNotPresentInCTM, SecondBridgeAddressTooLow} from "./L1BridgehubErrors.sol";
 import {SettlementLayersMustSettleOnL1} from "../common/L1ContractErrors.sol";
 import {ChainIdAlreadyExists, ChainIdMismatch, IncorrectBridgeHubAddress, MsgValueMismatch, WrongMagicValue, ZeroAddress} from "../common/L1ContractErrors.sol";
 
@@ -46,7 +46,7 @@ contract L1Bridgehub is BridgehubBase, IL1Bridgehub {
         MAX_NUMBER_OF_ZK_CHAINS = _maxNumberOfZKChains;
 
         // Note that this assumes that the bridgehub only accepts transactions on chains with ETH base token only.
-        // This is indeed true, since the only methods where this immutable is used are the ones with `onlyL1` modifier.
+        // This is indeed true, since the only methods where this immutable is used are the ones with `` modifier.
         // We will change this with interop.
         ETH_TOKEN_ASSET_ID = DataEncoding.encodeNTVAssetId(block.chainid, ETH_TOKEN_ADDRESS);
         _transferOwnership(_owner);
@@ -81,16 +81,9 @@ contract L1Bridgehub is BridgehubBase, IL1Bridgehub {
         return L1_CHAIN_ID;
     }
 
-    modifier onlyL1() {
-        if (block.chainid != L1_CHAIN_ID) {
-            revert NotL1(block.chainid, L1_CHAIN_ID);
-        }
-        _;
-    }
-
     /// @notice Used to set the legacy chain data for the upgrade.
     /// @param _chainId The chainId of the legacy chain we are migrating.
-    function registerLegacyChain(uint256 _chainId) external override onlyL1 {
+    function registerLegacyChain(uint256 _chainId) external override {
         address ctm = chainTypeManager[_chainId];
         if (ctm == address(0)) {
             revert ChainNotLegacy();
@@ -127,10 +120,7 @@ contract L1Bridgehub is BridgehubBase, IL1Bridgehub {
     /// @notice Used to register a chain as a settlement layer.
     /// @param _newSettlementLayerChainId the chainId of the chain
     /// @param _isWhitelisted whether the chain is a whitelisted settlement layer
-    function registerSettlementLayer(
-        uint256 _newSettlementLayerChainId,
-        bool _isWhitelisted
-    ) external onlyOwner onlyL1 {
+    function registerSettlementLayer(uint256 _newSettlementLayerChainId, bool _isWhitelisted) external onlyOwner {
         if (settlementLayer[_newSettlementLayerChainId] != block.chainid) {
             revert SettlementLayersMustSettleOnL1();
         }
@@ -156,7 +146,7 @@ contract L1Bridgehub is BridgehubBase, IL1Bridgehub {
         address _admin,
         bytes calldata _initData,
         bytes[] calldata _factoryDeps
-    ) external onlyOwnerOrAdmin nonReentrant whenNotPaused onlyL1 returns (uint256) {
+    ) external onlyOwnerOrAdmin nonReentrant whenNotPaused returns (uint256) {
         _validateChainParams({_chainId: _chainId, _assetId: _baseTokenAssetId, _chainTypeManager: _chainTypeManager});
 
         chainTypeManager[_chainId] = _chainTypeManager;
@@ -185,7 +175,7 @@ contract L1Bridgehub is BridgehubBase, IL1Bridgehub {
     /// In case allowance is provided to the Asset Router, then it will be transferred to NTV.
     function requestL2TransactionDirect(
         L2TransactionRequestDirect calldata _request
-    ) external payable override nonReentrant whenNotPaused onlyL1 returns (bytes32 canonicalTxHash) {
+    ) external payable override nonReentrant whenNotPaused returns (bytes32 canonicalTxHash) {
         // Note: If the ZK chain with corresponding `chainId` is not yet created,
         // the transaction will revert on `bridgehubRequestL2Transaction` as call to zero address.
         {
@@ -239,7 +229,7 @@ contract L1Bridgehub is BridgehubBase, IL1Bridgehub {
     /// @param _request the request for the L2 transaction
     function requestL2TransactionTwoBridges(
         L2TransactionRequestTwoBridgesOuter calldata _request
-    ) external payable override nonReentrant whenNotPaused onlyL1 returns (bytes32 canonicalTxHash) {
+    ) external payable override nonReentrant whenNotPaused returns (bytes32 canonicalTxHash) {
         if (_request.secondBridgeAddress <= BRIDGEHUB_MIN_SECOND_BRIDGE_ADDRESS) {
             revert SecondBridgeAddressTooLow(_request.secondBridgeAddress, BRIDGEHUB_MIN_SECOND_BRIDGE_ADDRESS);
         }
@@ -320,7 +310,7 @@ contract L1Bridgehub is BridgehubBase, IL1Bridgehub {
     /// @dev Registers an already deployed chain with the bridgehub
     /// @param _chainId The chain Id of the chain
     /// @param _zkChain Address of the zkChain
-    function registerAlreadyDeployedZKChain(uint256 _chainId, address _zkChain) external onlyOwner onlyL1 {
+    function registerAlreadyDeployedZKChain(uint256 _chainId, address _zkChain) external onlyOwner {
         if (_zkChain == address(0)) {
             revert ZeroAddress();
         }
