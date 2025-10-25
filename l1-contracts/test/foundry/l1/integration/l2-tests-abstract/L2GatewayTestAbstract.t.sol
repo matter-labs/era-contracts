7a3d1e958ccbee7b93e697c178dd42e88f5731b3
zkzoomer <sergio@zkzoomer.io>
2025-10-25 10:37:57 +0200

    more test fixes

--- l1-contracts/test/foundry/l1/integration/l2-tests-abstract/L2GatewayTestAbstract.t.sol
+++ l1-contracts/test/foundry/l1/integration/l2-tests-abstract/L2GatewayTestAbstract.t.sol
@@ -75,6 +75,7 @@ abstract contract L2GatewayTestAbstract is Test, SharedL2ContractDeployer {
     function test_withdrawFromGateway() public {
         finalizeDeposit();
         clearPriorityQueue(address(addresses.bridgehub.bridgehubProxy), mintChainId);
+        pauseDepositsBeforeInitiatingMigration(L2_BRIDGEHUB_ADDR, mintChainId);
         address newAdmin = address(0x1);
         bytes memory newDiamondCut = abi.encode();
         BridgehubBurnCTMAssetData memory data = BridgehubBurnCTMAssetData({
