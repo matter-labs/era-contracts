186b9d51f02e2b119d51c769e09f0511d4b748c7
zkzoomer <sergio@zkzoomer.io>
2025-10-27 11:12:49 +0100

    handle chains with paused deposits

--- l1-contracts/deploy-scripts/RegisterOnAllChains.s.sol
+++ l1-contracts/deploy-scripts/RegisterOnAllChains.s.sol
@@ -3,8 +3,10 @@
 pragma solidity ^0.8.21;
 
 import {Script} from "forge-std/Script.sol";
+import {console2 as console} from "forge-std/console2.sol";
 import {IBridgehubBase} from "contracts/bridgehub/IBridgehubBase.sol";
 import {ChainRegistrationSender} from "contracts/bridgehub/ChainRegistrationSender.sol";
+import {PAUSE_DEPOSITS_TIME_WINDOW_START, PAUSE_DEPOSITS_TIME_WINDOW_END} from "contracts/common/Config.sol";
 
 contract RegisterOnAllChainsScript is Script {
     function registerOnOtherChains(address _bridgehub, uint256 _chainId) public {
@@ -27,9 +29,28 @@ contract RegisterOnAllChainsScript is Script {
             ) {
                 continue;
             }
+            if (_depositsPaused(bridgehub, chainsToRegisterOn[i])) {
+                console.log(
+                    "Info: Deposits are paused on chain:",
+                    chainsToRegisterOn[i],
+                    ", skipping registration for chain:",
+                    _chainId
+                );
+                continue;
+            }
             vm.startBroadcast();
             chainRegistrationSender.registerChain(_chainId, chainsToRegisterOn[i]);
             vm.stopBroadcast();
         }
+    }
+
+    function _depositsPaused(IBridgehubBase bridgehub, uint256 chainToRegisterOn) internal view returns (bool) {
+        address zkChain = bridgehub.getZKChain(chainToRegisterOn);
+        bytes32 _pausedDepositsTimestamp = vm.load(zkChain, bytes32(uint256(62)));
+
+        uint256 timestamp = uint256(_pausedDepositsTimestamp);
+        return
+            timestamp + PAUSE_DEPOSITS_TIME_WINDOW_START <= block.timestamp &&
+            block.timestamp < timestamp + PAUSE_DEPOSITS_TIME_WINDOW_END;
     }
 }
