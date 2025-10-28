79cf25b8d7abada0c67602dfce934e08e926396a
zkzoomer <sergio@zkzoomer.io>
2025-10-28 17:18:23 +0100

    Revert "Merge pull request #1724 from matter-labs/kl/13-10-25/iterated-migrations"
    This reverts commit 3bef4052660148e4c6287820885d10dd0286f643, reversing
    changes made to 3fcd17de091693718e4b938b9756325b7a78c156.

--- l1-contracts/contracts/bridgehub/IChainAssetHandler.sol
+++ l1-contracts/contracts/bridgehub/IChainAssetHandler.sol
@@ -21,7 +21,5 @@ interface IChainAssetHandler is IAssetHandler {
 
     function migrationNumber(uint256 _chainId) external view returns (uint256);
 
-    function chainInitialMigrationNumber(uint256 _chainId) external view returns (uint256);
-
     function setMigrationNumberForV30(uint256 _chainId) external;
 }
