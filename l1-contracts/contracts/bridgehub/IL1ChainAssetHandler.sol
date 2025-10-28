0b5297c9e52b84e86bbde828267734f2a54b729e
zkzoomer <sergio@zkzoomer.io>
2025-10-28 12:07:29 +0100

    rm unused

--- l1-contracts/contracts/bridgehub/IL1ChainAssetHandler.sol
+++ l1-contracts/contracts/bridgehub/IL1ChainAssetHandler.sol
@@ -6,16 +6,4 @@ pragma solidity ^0.8.24;
 /// @custom:security-contact security@matterlabs.dev
 interface IL1ChainAssetHandler {
     function isMigrationInProgress(uint256 _chainId) external view returns (bool);
-
-    function confirmSuccessfulMigrationToGateway(
-        uint256 _chainId,
-        address _depositSender,
-        bytes32 _assetId,
-        bytes memory _assetData,
-        bytes32 _l2TxHash,
-        uint256 _l2BatchNumber,
-        uint256 _l2MessageIndex,
-        uint16 _l2TxNumberInBatch,
-        bytes32[] calldata _merkleProof
-    ) external;
 }
