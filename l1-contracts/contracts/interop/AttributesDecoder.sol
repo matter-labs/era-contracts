a04c5e6a9cca570c8fd9f91103355cd384d25504
0xValera <valera7743@icloud.com>
2025-10-31 00:02:27 +0100

    follow erc-7786 spec in AttributesDecoder.sol

--- l1-contracts/contracts/interop/AttributesDecoder.sol
+++ l1-contracts/contracts/interop/AttributesDecoder.sol
@@ -7,11 +7,11 @@ pragma solidity ^0.8.24;
 /// https://github.com/ethereum/ERCs/blob/023a7d657666308568d3d1391c578d5972636093/ERCS/erc-7786.md
 library AttributesDecoder {
     function decodeAddress(bytes calldata _data) internal pure returns (address) {
-        return (address(uint160(bytes20(_data[16:36]))));
+        return abi.decode(_data[4:], (address));
     }
 
     function decodeUint256(bytes calldata _data) internal pure returns (uint256) {
-        return (uint256(bytes32(_data[4:36])));
+        return abi.decode(_data[4:], (uint256));
     }
 
     function decodeInteroperableAddress(bytes calldata _data) internal pure returns (bytes memory) {
