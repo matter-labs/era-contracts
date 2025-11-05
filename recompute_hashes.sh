23e1f2f498849b1e1225e80f1fe53c3ed9aa56af
EmilLuta <virgil.luta@gmail.com>
2025-11-05 17:50:43 +0100

    Disable forge checks

--- recompute_hashes.sh
+++ recompute_hashes.sh
@@ -7,24 +7,24 @@ set -e
 EXPECTED_VERSION="forge 0.0.4"
 EXPECTED_COMMIT="ae913af65"
 
-# Check if Foundry is installed
-if ! command -V forge &> /dev/null; then
-  echo "Foundry is not installed. Please install it using foundryup-zksync with commit ${EXPECTED_COMMIT}."
-  exit 1
-fi
-
-# Get installed Foundry version (first line only)
-FORGE_OUTPUT=$(forge --version | head -n 1)
-
-# Accept anything that begins with: "${EXPECTED_VERSION} (${EXPECTED_COMMIT}"
-EXPECTED_PREFIX="${EXPECTED_VERSION} (${EXPECTED_COMMIT}"
-if [[ "$FORGE_OUTPUT" != "$EXPECTED_PREFIX"* ]]; then
-  echo "Incorrect Foundry version."
-  echo "Expected something starting with: ${EXPECTED_PREFIX}"
-  echo "Found:    ${FORGE_OUTPUT}"
-  echo "Run: foundryup-zksync --commit ${EXPECTED_COMMIT}"
-  exit 1
-fi
+# # Check if Foundry is installed
+# if ! command -V forge &> /dev/null; then
+#   echo "Foundry is not installed. Please install it using foundryup-zksync with commit ${EXPECTED_COMMIT}."
+#   exit 1
+# fi
+#
+# # Get installed Foundry version (first line only)
+# FORGE_OUTPUT=$(forge --version | head -n 1)
+#
+# # Accept anything that begins with: "${EXPECTED_VERSION} (${EXPECTED_COMMIT}"
+# EXPECTED_PREFIX="${EXPECTED_VERSION} (${EXPECTED_COMMIT}"
+# if [[ "$FORGE_OUTPUT" != "$EXPECTED_PREFIX"* ]]; then
+#   echo "Incorrect Foundry version."
+#   echo "Expected something starting with: ${EXPECTED_PREFIX}"
+#   echo "Found:    ${FORGE_OUTPUT}"
+#   echo "Run: foundryup-zksync --commit ${EXPECTED_COMMIT}"
+#   exit 1
+# fi
 
 if [ "$(git rev-parse --show-toplevel)" != "$PWD" ]; then
   echo "error: must be run at the root of matter-labs/era-contracts repository" >&2
