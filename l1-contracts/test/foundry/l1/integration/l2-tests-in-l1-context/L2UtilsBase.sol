fe5d83274933000ee67ee3c118b4d3e81c77d062
kelemeno <kl@matterlabs.dev>
2025-11-13 12:39:42 +0000

    linting

--- l1-contracts/test/foundry/l1/integration/l2-tests-in-l1-context/L2UtilsBase.sol
+++ l1-contracts/test/foundry/l1/integration/l2-tests-in-l1-context/L2UtilsBase.sol
@@ -145,7 +145,7 @@ library L2UtilsBase {
 
         // DummyL2L1Messenger dummyL2L1Messenger = new DummyL2L1Messenger();
         // vm.etch(L2_TO_L1_MESSENGER_SYSTEM_CONTRACT_ADDR, address(dummyL2L1Messenger).code);
-        {   
+        {
             address assetRouter = address(new L2AssetRouter());
             vm.etch(L2_ASSET_ROUTER_ADDR, assetRouter.code);
             vm.prank(L2_COMPLEX_UPGRADER_ADDR);
