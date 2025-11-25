fea90e7feff49e7be808f5f3fea3b8c5c2071698
jcsec-security <jc@jcsec.io>
2025-11-25 10:35:29 +0000

    Deprecation of finalizeEthWithdrawal and requestL2Transaction

--- l1-contracts/contracts/state-transition/chain-interfaces/IMailboxImpl.sol
+++ l1-contracts/contracts/state-transition/chain-interfaces/IMailboxImpl.sol
@@ -53,48 +53,6 @@ interface IMailboxImpl is IZKChainBase {
         TxStatus _status
     ) external view returns (bool);
 
-    /// @notice Finalize the withdrawal and release funds.
-    /// @param _l2BatchNumber The L2 batch number where the withdrawal was processed.
-    /// @param _l2MessageIndex The position in the L2 logs Merkle tree of the l2Log that was sent with the message.
-    /// @param _l2TxNumberInBatch The L2 transaction number in a batch, in which the log was sent.
-    /// @param _message The L2 withdraw data, stored in an L2 -> L1 message.
-    /// @param _merkleProof The Merkle proof of the inclusion L2 -> L1 message about withdrawal initialization.
-    function finalizeEthWithdrawal(
-        uint256 _l2BatchNumber,
-        uint256 _l2MessageIndex,
-        uint16 _l2TxNumberInBatch,
-        bytes calldata _message,
-        bytes32[] calldata _merkleProof
-    ) external;
-
-    /// @notice Request execution of L2 transaction from L1.
-    /// @param _contractL2 The L2 receiver address.
-    /// @param _l2Value `msg.value` of L2 transaction.
-    /// @param _calldata The input of the L2 transaction.
-    /// @param _l2GasLimit Maximum amount of L2 gas that transaction can consume during execution on L2.
-    /// @param _l2GasPerPubdataByteLimit The maximum amount L2 gas that the operator may charge the user for single byte of pubdata.
-    /// @param _factoryDeps An array of L2 bytecodes that will be marked as known on L2.
-    /// @param _refundRecipient The address on L2 that will receive the refund for the transaction.
-    /// @dev If the L2 deposit finalization transaction fails, the `_refundRecipient` will receive the `_l2Value`.
-    /// Please note, the contract may change the refund recipient's address to eliminate sending funds to addresses out of control.
-    /// - If `_refundRecipient` is a contract on L1, the refund will be sent to the aliased `_refundRecipient`.
-    /// - If `_refundRecipient` is set to `address(0)` and the sender has NO deployed bytecode on L1, the refund will be sent to the `msg.sender` address.
-    /// - If `_refundRecipient` is set to `address(0)` and the sender has deployed bytecode on L1, the refund will be sent to the aliased `msg.sender` address.
-    /// @dev The address aliasing of L1 contracts as refund recipient on L2 is necessary to guarantee that the funds are controllable,
-    /// since address aliasing to the from address for the L2 tx will be applied if the L1 `msg.sender` is a contract.
-    /// Without address aliasing for L1 contracts as refund recipients they would not be able to make proper L2 tx requests
-    /// through the Mailbox to use or withdraw the funds from L2, and the funds would be lost.
-    /// @return canonicalTxHash The hash of the requested L2 transaction. This hash can be used to follow the transaction status.
-    function requestL2Transaction(
-        address _contractL2,
-        uint256 _l2Value,
-        bytes calldata _calldata,
-        uint256 _l2GasLimit,
-        uint256 _l2GasPerPubdataByteLimit,
-        bytes[] calldata _factoryDeps,
-        address _refundRecipient
-    ) external payable returns (bytes32 canonicalTxHash);
-
     /// @notice Request execution of L2 transaction through the Bridgehub.
     /// @dev Only accessible from L1, this is getting checked in the Bridgehub.
     /// @param _request the request for the L2 transaction.
