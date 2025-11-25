d9108629c61ab8f27a5fc706fbd6164d1d39fdb5
jcsec-security <jc@jcsec.io>
2025-11-25 23:22:59 +0000

    Incorrect natspec

--- system-contracts/contracts/interfaces/IMailbox.sol
+++ system-contracts/contracts/interfaces/IMailbox.sol
@@ -7,11 +7,6 @@ pragma solidity ^0.8.20;
 interface IMailbox {
     /// @notice Finalize the withdrawal and release funds.
     /// @dev Deprecated in Mailbox
-    /// @param _l2BatchNumber The L2 batch number where the withdrawal was processed.
-    /// @param _l2MessageIndex The position in the L2 logs Merkle tree of the l2Log that was sent with the message.
-    /// @param _l2TxNumberInBatch The L2 transaction number in a batch, in which the log was sent.
-    /// @param _message The L2 withdraw data, stored in an L2 -> L1 message.
-    /// @param _merkleProof The Merkle proof of the inclusion L2 -> L1 message about withdrawal initialization.
     function finalizeEthWithdrawal(
         uint256 _l2BatchNumber,
         uint256 _l2MessageIndex,
