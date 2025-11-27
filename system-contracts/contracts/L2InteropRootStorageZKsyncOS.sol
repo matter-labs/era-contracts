2a398151391255c319f7054ae2bb0e99008fca0f
antoniolocascio <antonio.locascio1@gmail.com>
2025-11-27 15:38:58 +0800

    Separate L2InteropRootStorage contracts for Era and ZKsync OS

--- system-contracts/contracts/L2InteropRootStorageZKsyncOS.sol
+++ system-contracts/contracts/L2InteropRootStorageZKsyncOS.sol
@@ -4,12 +4,29 @@ pragma solidity ^0.8.24;
 
 import {L2InteropRootStorageBase, InteropRoot} from "./abstract/L2InteropRootStorageBase.sol";
 
+// TODO: where should this live? Also, use a constant + offset.
+address constant INTEROP_ROOT_REPORTER_HOOK = address(0x7003);
+error InteropRootReporterHookFailed();
+error InteropRootAlreadyAddedThisBlock();
+
 /**
  * @author Matter Labs
  * @custom:security-contact security@matterlabs.dev
- * @notice InteropRootStorage contract responsible for storing the message roots of other chains on the L2.
+ * @notice InteropRootStorageZKsyncOS contract responsible for storing the message roots of other chains on the L2 for ZKsync OS. Differences with Era are that this contract is only callable once per block by the coinbase and that it reports every new interop root to a ZKsync OS system hook.
  */
-contract L2InteropRootStorage is L2InteropRootStorageBase {
+contract L2InteropRootStorageZKsyncOS is L2InteropRootStorageBase {
+    /// @notice Last L2 block in which interop roots were added via any entrypoint.
+    uint256 public lastInteropRootAdditionBlock;
+
+    /// @dev Ensure interop roots can only be added once per block.
+    modifier onlyOncePerBlock() {
+        if (lastInteropRootAdditionBlock == block.number) {
+            revert InteropRootAlreadyAddedThisBlock();
+        }
+        lastInteropRootAdditionBlock = block.number;
+        _;
+    }
+
     /// @dev Adds a message root to the L2InteropRootStorage contract.
     /// @dev For both proof-based and commit-based interop, the `sides` parameter contains only the root.
     /// @dev Once pre-commit interop is introduced, `sides` will include both the root and its associated sides.
@@ -25,20 +42,36 @@ contract L2InteropRootStorage is L2InteropRootStorageBase {
         uint256 chainId,
         uint256 blockOrBatchNumber,
         bytes32[] calldata sides
-    ) external onlyCallFromBootloader {
+    ) external onlyCallFromCoinbase onlyOncePerBlock {
         _addInteropRootInternal(chainId, blockOrBatchNumber, sides);
     }
 
     /// @dev Adds a message root to the L2InteropRootStorage contract.
     /// @dev Currently duplicates `addInteropRoot` for backward compatibility.
     /// @param interopRoot The interop root to be added. See the description of the corresponding struct above.
-    function addSingleInteropRoot(InteropRoot calldata interopRoot) external onlyCallFromBootloader {
+    function addSingleInteropRoot(InteropRoot calldata interopRoot) external onlyCallFromCoinbase onlyOncePerBlock {
         _addSingleInteropRootInternal(interopRoot);
     }
 
     /// @dev Adds a group of interop roots to the L2InteropRootStorage contract.
     /// @param interopRootsInput The array of interop roots to be added. See the description of the corresponding struct above.
-    function addInteropRootsInBatch(InteropRoot[] calldata interopRootsInput) external onlyCallFromBootloader {
+    function addInteropRootsInBatch(
+        InteropRoot[] calldata interopRootsInput
+    ) external onlyCallFromCoinbase onlyOncePerBlock {
         _addInteropRootsInBatchInternal(interopRootsInput);
+    }
+
+    /// @dev ZKsync OS–specific hook: report to INTEROP_ROOT_REPORTER_HOOK.
+    function _afterInteropRootAdded(uint256 chainId, uint256 blockOrBatchNumber, bytes32 root) internal override {
+        // Binary layout:
+        // [ 0..31] chainId
+        // [32..63] blockOrBatchNumber
+        // [64..95] root
+        bytes memory message = abi.encodePacked(chainId, blockOrBatchNumber, root);
+
+        (bool ok, ) = INTEROP_ROOT_REPORTER_HOOK.call(message);
+        if (!ok) {
+            revert InteropRootReporterHookFailed();
+        }
     }
 }
---
+++ system-contracts/contracts/abstract/L2InteropRootStorageBase.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {SystemContractBase} from "./SystemContractBase.sol";
+
+event InteropRootAdded(uint256 indexed chainId, uint256 indexed blockNumber, bytes32[] sides);
+error SidesLengthNotOne();
+error InteropRootAlreadyExists();
+error MessageRootIsZero();
+
+/// @dev For both proof-based and commit-based interop, the `sides` parameter contains only the root.
+/// @dev Once pre-commit interop is introduced, `sides` will include both the root and its associated sides.
+/// @dev This interface is preserved now so that enabling pre-commit interop later requires no changes in interface.
+/// @dev In proof-based and pre-commit interop, `blockOrBatchNumber` represents the block number, in commit-based interop,
+/// it represents the batch number. This distinction reflects the implementation requirements  of each interop finality form.
+/// @param chainId The chain ID of the chain that the message root is for.
+/// @param blockOrBatchNumber The block or batch number of the message root. Either of block number or batch number will be used,
+/// depends on finality form of interop.
+/// @param sides The message root sides. Note, that `sides` here are coming from `DynamicIncrementalMerkle` nomenclature.
+struct InteropRoot {
+    uint256 chainId;
+    uint256 blockOrBatchNumber;
+    bytes32[] sides;
+}
+
+/**
+ * @author Matter Labs
+ * @custom:security-contact security@matterlabs.dev
+ * @notice An abstract contract responsible for the common logic related to
+   storing the message roots of other chains on the L2.
+ */
+abstract contract L2InteropRootStorageBase is SystemContractBase {
+    /// @notice Mapping of chain ID to block or batch number to message root.
+    mapping(uint256 chainId => mapping(uint256 blockOrBatchNumber => bytes32 interopRoot)) public interopRoots;
+
+    function _addInteropRootInternal(uint256 chainId, uint256 blockOrBatchNumber, bytes32[] calldata sides) internal {
+        // In the current code sides should only contain the Interop Root itself, as mentioned above.
+        if (sides.length != 1) {
+            revert SidesLengthNotOne();
+        }
+        if (sides[0] == bytes32(0)) {
+            revert MessageRootIsZero();
+        }
+
+        // Make sure that interopRoots for specified chainId and blockOrBatchNumber wasn't set already.
+        if (interopRoots[chainId][blockOrBatchNumber] != bytes32(0)) {
+            revert InteropRootAlreadyExists();
+        }
+
+        // Set interopRoots for specified chainId and blockOrBatchNumber.
+        interopRoots[chainId][blockOrBatchNumber] = sides[0];
+
+        // Optional hook (no-op by default, overridden in ZKsync OS version).
+        _afterInteropRootAdded(chainId, blockOrBatchNumber, sides[0]);
+
+        // Emit event.
+        emit InteropRootAdded(chainId, blockOrBatchNumber, sides);
+    }
+
+    function _addSingleInteropRootInternal(InteropRoot calldata interopRoot) internal {
+        _addInteropRootInternal(interopRoot.chainId, interopRoot.blockOrBatchNumber, interopRoot.sides);
+    }
+
+    function _addInteropRootsInBatchInternal(InteropRoot[] calldata interopRootsInput) internal {
+        unchecked {
+            uint256 amountOfRoots = interopRootsInput.length;
+            for (uint256 i; i < amountOfRoots; ++i) {
+                _addInteropRootInternal(
+                    interopRootsInput[i].chainId,
+                    interopRootsInput[i].blockOrBatchNumber,
+                    interopRootsInput[i].sides
+                );
+            }
+        }
+    }
+
+    /// @dev Hook for derived contracts. Default: no-op.
+    function _afterInteropRootAdded(uint256 chainId, uint256 blockOrBatchNumber, bytes32 root) internal virtual {
+        // default: do nothing
+    }
+}
