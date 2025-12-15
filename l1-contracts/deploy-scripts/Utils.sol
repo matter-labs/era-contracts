ec40de47ff67fde1d072d81555a6d44a114907d5
Danil <deniallugo@gmail.com>
2025-12-15 12:44:36 +0000

    Use proper admins
    Signed-off-by: Danil <deniallugo@gmail.com>

--- l1-contracts/deploy-scripts/Utils.sol
+++ l1-contracts/deploy-scripts/Utils.sol
@@ -1374,6 +1374,18 @@ library Utils {
         return address(uint160(uint256(value)));
     }
 
+    /**
+     * @dev Storage slot with the admin of the contract.
+     * This is the keccak-256 hash of "eip1967.proxy.admin" subtracted by 1, and is
+     * validated in the constructor.
+     */
+    bytes32 internal constant ADMIN_SLOT = 0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103;
+
+    function getProxyAdminAddress(address proxy) internal view returns (address) {
+        bytes32 value = vm.load(proxy, ADMIN_SLOT); // Foundry cheatcode
+        return address(uint160(uint256(value)));
+    }
+
     // add this to be excluded from coverage report
     function test() internal {}
 }
--- l1-contracts/deploy-scripts/upgrade/ValidatorTimelockUpgrade.sol
+++ l1-contracts/deploy-scripts/upgrade/ValidatorTimelockUpgrade.sol
@@ -1,25 +1,26 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.10;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console2 as console} from "forge-std/Script.sol";
 import {ZKsyncOSChainTypeManager} from "contracts/state-transition/ZKsyncOSChainTypeManager.sol";
 import {Create2FactoryUtils} from "deploy-scripts/Create2FactoryUtils.s.sol";
 import {MultisigCommitter} from "contracts/state-transition/MultisigCommitter.sol";
 import {ITransparentUpgradeableProxy} from "@openzeppelin/contracts-v4/proxy/transparent/TransparentUpgradeableProxy.sol";
 import {Call} from "contracts/governance/Common.sol";
 import {ProxyAdmin} from "@openzeppelin/contracts-v4/proxy/transparent/ProxyAdmin.sol";
+import {Utils} from "deploy-scripts/Utils.sol";
 
 contract ValidatorTimelockUpgrade is Script, Create2FactoryUtils {
     function run(address ctm, uint32 initialExecutionDelay) external {
+        instantiateCreate2Factory();
         ZKsyncOSChainTypeManager chainTypeManager = ZKsyncOSChainTypeManager(ctm);
+        console.logAddress(msg.sender);
         address validatorTimelock = chainTypeManager.validatorTimelockPostV29();
         address bridgehub = chainTypeManager.BRIDGE_HUB();
-        address proxyAdmin = MultisigCommitter(validatorTimelock).owner();
+        address proxyAdmin = Utils.getProxyAdminAddress(validatorTimelock);
 
-        vm.startBroadcast();
         address implementation = deployImplementation(proxyAdmin, bridgehub);
         upgradeValidatorTimelock(validatorTimelock, implementation, proxyAdmin, initialExecutionDelay);
-        vm.stopBroadcast();
     }
 
     function deployImplementation(address owner, address bridgehub) internal returns (address) {
@@ -32,18 +33,31 @@ contract ValidatorTimelockUpgrade is Script, Create2FactoryUtils {
             );
     }
 
+    function isContract(address a) internal view returns (bool) {
+        return a.code.length > 0;
+    }
+
     function upgradeValidatorTimelock(
         address proxy,
         address newImplementation,
         address admin,
         uint32 _initialExecutionDelay
     ) internal {
-        vm.prank(ProxyAdmin(admin).owner());
-        ProxyAdmin(admin).upgradeAndCall(
-            ITransparentUpgradeableProxy(payable(proxy)),
-            newImplementation,
-            abi.encodeCall(MultisigCommitter.initializeV2, (admin, _initialExecutionDelay))
-        );
+        vm.startBroadcast();
+        if (isContract(admin)) {
+            ProxyAdmin(admin).upgradeAndCall(
+                ITransparentUpgradeableProxy(payable(proxy)),
+                newImplementation,
+                abi.encodeCall(MultisigCommitter.initializeV2, (admin, _initialExecutionDelay))
+            );
+        } else {
+            vm.prank(msg.sender);
+            ITransparentUpgradeableProxy(payable(proxy)).upgradeToAndCall(
+                newImplementation,
+                abi.encodeCall(MultisigCommitter.initializeV2, (admin, _initialExecutionDelay))
+            );
+        }
+        vm.stopBroadcast();
     }
 
     function useCustomSigningSet(address validatorTimelock, address chain) public {
