ab2522ee4a84299cd198abf423636de4ecd88117
0xValera <valera7743@icloud.com>
2025-12-16 13:32:26 +0100

    Update L2InteropCenterTestAbstract.t.sol

--- l1-contracts/test/foundry/l1/integration/l2-tests-abstract/L2InteropCenterTestAbstract.t.sol
+++ l1-contracts/test/foundry/l1/integration/l2-tests-abstract/L2InteropCenterTestAbstract.t.sol
@@ -5,11 +5,13 @@ pragma solidity ^0.8.20;
 
 import {L2InteropLibraryBasicTestAbstract} from "./L2InteropLibraryBasicTestAbstract.t.sol";
 import {L2InteropNativeTokenDifferentBaseTestAbstract} from "./L2InteropNativeTokenDifferentBaseTestAbstract.t.sol";
+import {L2InteropNativeTokenSimpleTestAbstract} from "./L2InteropNativeTokenSimpleTestAbstract.t.sol";
 import {L2InteropMessageHandlerTestAbstract} from "./L2InteropMessageHandlerTestAbstract.t.sol";
 
 abstract contract L2InteropCenterTestAbstract is
     L2InteropLibraryBasicTestAbstract,
     L2InteropNativeTokenDifferentBaseTestAbstract,
+    L2InteropNativeTokenSimpleTestAbstract,
     L2InteropMessageHandlerTestAbstract
 {
     // This contract combines all the split test abstracts to maintain backward compatibility
