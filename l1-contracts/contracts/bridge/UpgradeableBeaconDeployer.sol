ae2afb5f7ad9157f0d6a6ec6557727e994914f4a
0xValera <valera7743@icloud.com>
2025-12-16 12:25:57 +0100

    address comments

--- l1-contracts/contracts/bridge/UpgradeableBeaconDeployer.sol
+++ l1-contracts/contracts/bridge/UpgradeableBeaconDeployer.sol
@@ -5,7 +5,6 @@ pragma solidity 0.8.28;
 import {UpgradeableBeacon} from "@openzeppelin/contracts-v4/proxy/beacon/UpgradeableBeacon.sol";
 
 import {BridgedStandardERC20} from "./BridgedStandardERC20.sol";
-import {EmptyAddress} from "../common/L1ContractErrors.sol";
 
 /// @author Matter Labs
 /// @custom:security-contact security@matterlabs.dev
@@ -14,10 +13,6 @@ import {EmptyAddress} from "../common/L1ContractErrors.sol";
 /// does not have to include BridgedStandardERC20 and UpgradeableBeacon and so can fit into the code size limit.
 contract UpgradeableBeaconDeployer {
     function deployUpgradeableBeacon(address _owner) external returns (address) {
-        if (_owner == address(0)) {
-            revert EmptyAddress();
-        }
-
         address l2StandardToken = address(new BridgedStandardERC20{salt: bytes32(0)}());
 
         UpgradeableBeacon tokenBeacon = new UpgradeableBeacon{salt: bytes32(0)}(l2StandardToken);
