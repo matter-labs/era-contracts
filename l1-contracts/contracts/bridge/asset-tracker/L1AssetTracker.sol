f5ccd06b744e6ded6aadf93f73a8bbc50a58d222
zkzoomer <sergio@zkzoomer.io>
2025-12-18 11:21:42 +0100

    Revert "handle base token case"
    This reverts commit a8263587d381da34fe99f6be95d9e390470e3810.

--- l1-contracts/contracts/bridge/asset-tracker/L1AssetTracker.sol
+++ l1-contracts/contracts/bridge/asset-tracker/L1AssetTracker.sol
@@ -260,9 +260,7 @@ contract L1AssetTracker is AssetTrackerBase, IL1AssetTracker {
         uint256 toChainId;
 
         // We check the assetId to make sure the chain is not lying about it.
-        if (data.assetId != BRIDGE_HUB.baseTokenAssetId(data.tokenOriginChainId)) {
-            DataEncoding.assetIdCheck(data.tokenOriginChainId, data.assetId, data.originToken);
-        }
+        DataEncoding.assetIdCheck(data.tokenOriginChainId, data.assetId, data.originToken);
 
         if (data.isL1ToGateway) {
             uint256 chainMigrationNumber = _getChainMigrationNumber(data.chainId);
