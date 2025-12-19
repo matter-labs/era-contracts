e3dd10b98d15f10dadad4bc4dbae00cfa592172c
kelemeno <kl@matterlabs.dev>
2025-12-19 16:32:29 +0100

    small changes

--- l1-contracts/contracts/l2-system/zksync-os/L2BaseToken.sol
+++ l1-contracts/contracts/l2-system/zksync-os/L2BaseToken.sol
@@ -2,11 +2,12 @@
 
 pragma solidity 0.8.28;
 
-import {L2_TO_L1_MESSENGER_SYSTEM_CONTRACT} from "contracts/common/l2-helpers/L2ContractAddresses.sol";
+import {L2_TO_L1_MESSENGER_SYSTEM_CONTRACT, L2_COMPLEX_UPGRADER_ADDR} from "contracts/common/l2-helpers/L2ContractAddresses.sol";
 import {IMailboxImpl} from "contracts/state-transition/chain-interfaces/IMailbox.sol";
 import {Burner} from "./Burner.sol";
 import {IBaseToken} from "./ZKOSContractHelper.sol";
 import {L1MessengerSendFailed} from "./errors/ZKOSContractErrors.sol";
+import {InvalidCaller} from "contracts/common/L1ContractErrors.sol";
 
 /**
  * @author Matter Labs
@@ -16,10 +17,30 @@ import {L1MessengerSendFailed} from "./errors/ZKOSContractErrors.sol";
  * Instead, this contract is used only as an entrypoint for native token withdrawals.
  */
 contract L2BaseToken is IBaseToken {
+
+    /// @dev Only allows calls from the complex upgrader contract on L2.
+    modifier onlyUpgrader() {
+        if (msg.sender != L2_COMPLEX_UPGRADER_ADDR) {
+            revert InvalidCaller(msg.sender);
+        }
+        _;
+    }
+
+    /// @notice Initializes the contract.
+    /// @dev This function is used to initialize the contract with the initial values.
+    function initL2() public onlyUpgrader {
+        _initialize();
+    }
+
+    /// @dev Internal initialization function. Can be overridden by derived contracts.
+    function _initialize() internal virtual {
+
+    }
+
     /// @notice Initiate the withdrawal of the base token, funds will be available to claim on L1 `finalizeEthWithdrawal` method.
     /// @param _l1Receiver The address on L1 to receive the funds.
     function withdraw(address _l1Receiver) external payable {
-        uint256 amount = _burnMsgValue();
+        uint256 amount = msg.value;
 
         // Send the L2 log, a user could use it as proof of the withdrawal
         bytes memory message = _getL1WithdrawMessage(_l1Receiver, amount);
@@ -33,7 +54,7 @@ contract L2BaseToken is IBaseToken {
     /// @param _l1Receiver The address on L1 to receive the funds.
     /// @param _additionalData Additional data to be sent to L1 with the withdrawal.
     function withdrawWithMessage(address _l1Receiver, bytes calldata _additionalData) external payable {
-        uint256 amount = _burnMsgValue();
+        uint256 amount = msg.value;
 
         // Send the L2 log, a user could use it as proof of the withdrawal
         bytes memory message = _getExtendedWithdrawMessage(_l1Receiver, amount, msg.sender, _additionalData);
@@ -41,17 +62,6 @@ contract L2BaseToken is IBaseToken {
         if (msgHash == bytes32(0)) revert L1MessengerSendFailed();
 
         emit WithdrawalWithMessage(msg.sender, _l1Receiver, amount, _additionalData);
-    }
-
-    /// @dev The function burn the sent `msg.value`.
-    /// NOTE: Since this contract holds the mapping of all ether balances of the system,
-    /// the sent `msg.value` is added to the `this` balance before the call.
-    /// So the balance of `address(this)` is always bigger or equal to the `msg.value`!
-    function _burnMsgValue() internal returns (uint256 amount) {
-        amount = msg.value;
-
-        if (amount == 0) return 0;
-        new Burner{value: amount}();
     }
 
     /// @dev Get the message to be sent to L1 to initiate a withdrawal.
