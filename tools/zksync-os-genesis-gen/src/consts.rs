132ae4b440092253cc30f75b95c1d783d865eb6e
Danil <deniallugo@gmail.com>
2025-12-19 18:58:48 +0000

    Fix constants
    Signed-off-by: Danil <deniallugo@gmail.com>

--- tools/zksync-os-genesis-gen/src/consts.rs
+++ tools/zksync-os-genesis-gen/src/consts.rs
@@ -10,7 +10,7 @@ pub const L2_WRAPPED_BASE_TOKEN: Address = Address(FixedBytes::<20>(hex_literal:
     "0000000000000000000000000000000000010007"
 )));
 pub const SYSTEM_CONTRACT_PROXY_ADMIN: Address = Address(FixedBytes::<20>(hex_literal::hex!(
-    "000000000000000000000000000000000001000c"
+    "0000000000000000000000000000000000010010"
 )));
 // keccak256("L2_COMPLEX_UPGRADER_IMPL_ADDR") - 1.
 // We need it predeployed to make the genesis upgrade work at all.
