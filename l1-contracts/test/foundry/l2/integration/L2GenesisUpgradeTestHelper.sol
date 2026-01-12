c97d8a363cec5a8dfcf2e4b781afd10e6bf9049a
kelemeno <kl@matterlabs.dev>
2026-01-12 21:13:18 +0000

    linting

--- l1-contracts/test/foundry/l2/integration/L2GenesisUpgradeTestHelper.sol
+++ l1-contracts/test/foundry/l2/integration/L2GenesisUpgradeTestHelper.sol
@@ -69,46 +69,53 @@ library L2GenesisUpgradeTestHelper {
     }
 
     function getAdditionalForceDeploymentsData() public pure returns (bytes memory) {
-        return abi.encode(
-            ZKChainSpecificForceDeploymentsData({
-                baseTokenBridgingData: TokenBridgingData({
-                    assetId: bytes32(0x0100056f53fd9e940906d998a80ed53392e5c50a8eb198baf9f78fd84ce7ec70),
-                    originChainId: 1,
-                    originToken: address(1)
-                }),
-                l2LegacySharedBridge: address(0),
-                predeployedL2WethAddress: address(1),
-                baseTokenL1Address: address(1),
-                baseTokenMetadata: TokenMetadata({name: "Ether", symbol: "ETH", decimals: 18})
-            })
-        );
+        return
+            abi.encode(
+                ZKChainSpecificForceDeploymentsData({
+                    baseTokenBridgingData: TokenBridgingData({
+                        assetId: bytes32(0x0100056f53fd9e940906d998a80ed53392e5c50a8eb198baf9f78fd84ce7ec70),
+                        originChainId: 1,
+                        originToken: address(1)
+                    }),
+                    l2LegacySharedBridge: address(0),
+                    predeployedL2WethAddress: address(1),
+                    baseTokenL1Address: address(1),
+                    baseTokenMetadata: TokenMetadata({name: "Ether", symbol: "ETH", decimals: 18})
+                })
+            );
     }
 
-    function getFixedForceDeploymentsData(uint256 _chainId, BytecodeInfo memory _bytecodeInfo) public pure returns (bytes memory) {
-        return abi.encode(
-            FixedForceDeploymentsData({
-                l1ChainId: 1,
-                gatewayChainId: 1,
-                eraChainId: _chainId,
-                l1AssetRouter: address(1),
-                l2TokenProxyBytecodeHash: bytes32(0x0100056f53fd9e940906d998a80ed53392e5c50a8eb198baf9f78fd84ce7ec70),
-                aliasedL1Governance: address(1),
-                maxNumberOfZKChains: 100,
-                bridgehubBytecodeInfo: _bytecodeInfo.bridgehubBytecodeInfo,
-                l2AssetRouterBytecodeInfo: _bytecodeInfo.l2AssetRouterBytecodeInfo,
-                l2NtvBytecodeInfo: _bytecodeInfo.l2NtvBytecodeInfo,
-                messageRootBytecodeInfo: _bytecodeInfo.messageRootBytecodeInfo,
-                chainAssetHandlerBytecodeInfo: _bytecodeInfo.chainAssetHandlerBytecodeInfo,
-                interopCenterBytecodeInfo: _bytecodeInfo.interopCenterBytecodeInfo,
-                interopHandlerBytecodeInfo: _bytecodeInfo.interopHandlerBytecodeInfo,
-                assetTrackerBytecodeInfo: _bytecodeInfo.assetTrackerBytecodeInfo,
-                beaconDeployerInfo: _bytecodeInfo.beaconDeployerBytecodeInfo,
-                l2SharedBridgeLegacyImpl: address(0),
-                l2BridgedStandardERC20Impl: address(0),
-                aliasedChainRegistrationSender: address(1),
-                dangerousTestOnlyForcedBeacon: address(0)
-            })
-        );
+    function getFixedForceDeploymentsData(
+        uint256 _chainId,
+        BytecodeInfo memory _bytecodeInfo
+    ) public pure returns (bytes memory) {
+        return
+            abi.encode(
+                FixedForceDeploymentsData({
+                    l1ChainId: 1,
+                    gatewayChainId: 1,
+                    eraChainId: _chainId,
+                    l1AssetRouter: address(1),
+                    l2TokenProxyBytecodeHash: bytes32(
+                        0x0100056f53fd9e940906d998a80ed53392e5c50a8eb198baf9f78fd84ce7ec70
+                    ),
+                    aliasedL1Governance: address(1),
+                    maxNumberOfZKChains: 100,
+                    bridgehubBytecodeInfo: _bytecodeInfo.bridgehubBytecodeInfo,
+                    l2AssetRouterBytecodeInfo: _bytecodeInfo.l2AssetRouterBytecodeInfo,
+                    l2NtvBytecodeInfo: _bytecodeInfo.l2NtvBytecodeInfo,
+                    messageRootBytecodeInfo: _bytecodeInfo.messageRootBytecodeInfo,
+                    chainAssetHandlerBytecodeInfo: _bytecodeInfo.chainAssetHandlerBytecodeInfo,
+                    interopCenterBytecodeInfo: _bytecodeInfo.interopCenterBytecodeInfo,
+                    interopHandlerBytecodeInfo: _bytecodeInfo.interopHandlerBytecodeInfo,
+                    assetTrackerBytecodeInfo: _bytecodeInfo.assetTrackerBytecodeInfo,
+                    beaconDeployerInfo: _bytecodeInfo.beaconDeployerBytecodeInfo,
+                    l2SharedBridgeLegacyImpl: address(0),
+                    l2BridgedStandardERC20Impl: address(0),
+                    aliasedChainRegistrationSender: address(1),
+                    dangerousTestOnlyForcedBeacon: address(0)
+                })
+            );
     }
 
     function setupMockCalls(
@@ -122,24 +129,12 @@ library L2GenesisUpgradeTestHelper {
         address _proxyAdmin,
         address _complexUpgrader
     ) public {
-        _vm.mockCall(
-            _systemContext,
-            abi.encodeWithSelector(ISystemContext.setChainId.selector),
-            ""
-        );
+        _vm.mockCall(_systemContext, abi.encodeWithSelector(ISystemContext.setChainId.selector), "");
         _vm.mockCall(_bridgehub, abi.encodeWithSelector(L2Bridgehub.initL2.selector), "");
         _vm.mockCall(_assetRouter, abi.encodeWithSelector(L2AssetRouter.initL2.selector), "");
         _vm.mockCall(_chainAssetHandler, abi.encodeWithSelector(L2ChainAssetHandler.initL2.selector), "");
         _vm.mockCall(_interopCenter, abi.encodeWithSelector(bytes4(keccak256("initL2(uint256,address)"))), "");
-        _vm.mockCall(
-            _knownCodeStorage,
-            abi.encodeWithSelector(bytes4(keccak256("getMarker(bytes32)"))),
-            abi.encode(1)
-        );
-        _vm.mockCall(
-            _proxyAdmin,
-            abi.encodeWithSignature("owner()"),
-            abi.encode(_complexUpgrader)
-        );
+        _vm.mockCall(_knownCodeStorage, abi.encodeWithSelector(bytes4(keccak256("getMarker(bytes32)"))), abi.encode(1));
+        _vm.mockCall(_proxyAdmin, abi.encodeWithSignature("owner()"), abi.encode(_complexUpgrader));
     }
 }
