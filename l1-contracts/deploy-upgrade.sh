9e765905c9efb4c75c962d33f5b314452d307d48
Stanislav Breadless <stanislavbezkor@gmail.com>
2026-01-21 10:34:51 +0100

    new dummy data

--- l1-contracts/deploy-upgrade.sh
+++ l1-contracts/deploy-upgrade.sh
@@ -1,5 +1,4 @@
 UPGRADE_ECOSYSTEM_INPUT=/upgrade-envs/v0.29.3-vk-update/stage.toml UPGRADE_ECOSYSTEM_OUTPUT=/script-out/v29-3-ecosystem.toml forge script --sig "run()" ./deploy-scripts/upgrade/VerifierOnlyUpgrade.s.sol:VerifierOnlyUpgrade --ffi --rpc-url $TENDERLY_SEPOLIA --gas-limit 20000000000 --private-key $TEST_PK --broadcast
 
 
-
 UPGRADE_ECOSYSTEM_OUTPUT=script-out/v29-3-ecosystem.toml UPGRADE_ECOSYSTEM_OUTPUT_TRANSACTIONS=broadcast/VerifierOnlyUpgrade.s.sol/11155111/run-latest.json YAML_OUTPUT_FILE=script-out/yaml-output.yaml yarn upgrade-yaml-output-generator
