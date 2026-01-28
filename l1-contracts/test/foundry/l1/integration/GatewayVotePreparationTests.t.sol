d291e7cde29c96e93aff4b4f0e0f1fa9f62e0ab2
kelemeno <kl@matterlabs.dev>
2026-01-28 12:26:39 +0000

    more dead code

--- l1-contracts/test/foundry/l1/integration/GatewayVotePreparationTests.t.sol
+++ l1-contracts/test/foundry/l1/integration/GatewayVotePreparationTests.t.sol
@@ -2,35 +2,8 @@
 pragma solidity 0.8.28;
 
 import {Test} from "forge-std/Test.sol";
-import {Vm} from "forge-std/Vm.sol";
-import {console2 as console} from "forge-std/console2.sol";
 
 import {GatewayVotePreparation} from "deploy-scripts/gateway/GatewayVotePreparation.s.sol";
-import {Call} from "contracts/governance/Common.sol";
-
-/// @title GatewayVotePreparationForTests
-/// @notice Test version of GatewayVotePreparation that exposes internal state for testing
-contract GatewayVotePreparationForTests is GatewayVotePreparation {
-    /// @notice Get the gateway chain ID from config
-    function getGatewayChainId() public view returns (uint256) {
-        return gatewayChainId;
-    }
-
-    /// @notice Get the CTM address
-    function getCTM() public view returns (address) {
-        return ctm;
-    }
-
-    /// @notice Get the refund recipient
-    function getRefundRecipient() public view returns (address) {
-        return refundRecipient;
-    }
-
-    /// @notice Get the era chain ID
-    function getEraChainId() public view returns (uint256) {
-        return eraChainId;
-    }
-}
 
 /// @title GatewayVotePreparationTests
 /// @notice Integration tests for GatewayVotePreparation script
@@ -40,7 +13,7 @@ contract GatewayVotePreparationTests is Test {
     /// @dev This verifies that the GatewayVotePreparation import compiles and the contract structure is valid
     function test_gatewayVotePreparationCanBeInstantiated() public {
         // Create the GatewayVotePreparation test script - this verifies the contract compiles and can be deployed
-        GatewayVotePreparationForTests votePreparationScript = new GatewayVotePreparationForTests();
+        GatewayVotePreparation votePreparationScript = new GatewayVotePreparation();
 
         // Verify the contract was created (address is non-zero)
         assertTrue(address(votePreparationScript) != address(0), "GatewayVotePreparation should be instantiated");
