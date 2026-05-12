7882688172ba3713fce8e15f85b21e16cda0a273
zkzoomer <sergio@zkzoomer.io>
2026-05-12 21:11:35 +0200

    add CoreUpgrade_v31

--- l1-contracts/scripts/copy-to-zkstack-out.ts
+++ l1-contracts/scripts/copy-to-zkstack-out.ts
@@ -37,7 +37,7 @@ const REQUIRED_CONTRACTS = [
   "IFinalizeChainInit.sol",
   "IUpgradeV31.sol",
   "IFinalizeUpgrade.sol",
-  "EcosystemUpgrade_v31.s.sol",
+  "CoreUpgrade_v31.s.sol",
   "IL1NativeTokenVault.sol",
   "IL2NativeTokenVault.sol",
   "IL1AssetRouter.sol",
