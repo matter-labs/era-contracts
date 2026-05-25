3ddba65fcb69d57466d1de5301e11e6abd60b853
zkzoomer <sergio@zkzoomer.io>
2026-05-25 15:56:56 +0200

    missing info

--- l1-contracts/deploy-scripts/gateway/GatewayVotePreparation.s.sol
+++ l1-contracts/deploy-scripts/gateway/GatewayVotePreparation.s.sol
@@ -386,8 +386,23 @@ contract GatewayVotePreparation is DeployCTMUtils, GatewayGovernanceUtils {
         );
         vm.serializeAddress(
             "gateway_state_transition",
+            "migrator_facet_addr",
+            output.gatewayStateTransition.facets.migratorFacet
+        );
+        vm.serializeAddress(
+            "gateway_state_transition",
+            "committer_facet_addr",
+            output.gatewayStateTransition.facets.committerFacet
+        );
+        vm.serializeAddress(
+            "gateway_state_transition",
             "diamond_init_addr",
             output.gatewayStateTransition.facets.diamondInit
+        );
+        vm.serializeBytes(
+            "gateway_state_transition",
+            "force_deployments_data",
+            forceDeploymentsData
         );
         vm.serializeAddress(
             "gateway_state_transition",
